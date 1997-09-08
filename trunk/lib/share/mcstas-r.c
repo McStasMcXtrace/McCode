675d00f151aed589cd39137e8b79f922df760792
kn <kn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
1997-09-08 10:40:03 +0000

    Initial revision

---
+++ trunk/lib/share/mcstas-r.c
@@ -0,0 +1,209 @@
+/*******************************************************************************
+* Runtime system for McStas.
+*
+* 	Project: Monte Carlo Simulation of Tripple Axis Spectrometers
+* 	File name: mcstas-r.c
+*
+* 	Author: K.N.			Aug 27, 1997
+*
+* 	$Id: mcstas-r.c,v 1.1 1997-09-08 10:40:03 kn Exp $
+*
+* 	$Log: not supported by cvs2svn $
+*
+* Copyright (C) Risoe National Laboratory, 1991-1997, All rights reserved
+*******************************************************************************/
+
+#include <math.h>
+#include "mcstas-r.h"
+
+/* Assign coordinates. */
+Coords
+coords_set(MCNUM x, MCNUM y, MCNUM z)
+{
+  Coords a;
+
+  a.x = x;
+  a.y = y;
+  a.z = z;
+  return a;
+}
+
+/* Add two coordinates. */
+Coords
+coords_add(Coords a, Coords b)
+{
+  Coords c;
+
+  c.x = a.x + b.x;
+  c.y = a.y + b.y;
+  c.z = a.z + b.z;
+  return c;
+}
+
+/* Subtract two coordinates. */
+Coords
+coords_sub(Coords a, Coords b)
+{
+  Coords c;
+
+  c.x = a.x - b.x;
+  c.y = a.y - b.y;
+  c.z = a.z - b.z;
+  return c;
+}
+
+/* Negate coordinates. */
+Coords
+coords_neg(Coords a)
+{
+  Coords b;
+
+  b.x = -a.x;
+  b.y = -a.y;
+  b.z = -a.z;
+  return b;
+}
+
+/*******************************************************************************
+* Get transformation for rotation first phx around x axis, then phy around y,
+* then phz around z.
+*******************************************************************************/
+void
+rot_set_rotation(Rotation t, double phx, double phy, double phz)
+{
+  double cx = cos(phx);
+  double sx = sin(phx);
+  double cy = cos(phy);
+  double sy = sin(phy);
+  double cz = cos(phz);
+  double sz = sin(phz);
+
+  t[0][0] = cy*cz;
+  t[0][1] = sx*sy*cz + cx*sz;
+  t[0][2] = sx*sz - cx*sy*cz;
+  t[1][0] = -cy*sz;
+  t[1][1] = cx*cz - sx*sy*sz;
+  t[1][2] = sx*cz + cx*sy*sz;
+  t[2][0] = sy;
+  t[2][1] = -sx*cy;
+  t[2][2] = cx*cy;
+}
+
+/*******************************************************************************
+* Matrix multiplication of transformations (this corresponds to combining
+* transformations). After rot_mul(T1, T2, T3), doing T3 is equal to doing
+* first T2, then T1.
+* Note that T3 must not alias (use the same array as) T1 or T2.
+*******************************************************************************/
+void
+rot_mul(Rotation t1, Rotation t2, Rotation t3)
+{
+  int i,j, k;
+
+  for(i = 0; i < 3; i++)
+    for(j = 0; j < 3; j++)
+      t3[i][j] = t1[i][0]*t2[0][j] + t1[i][1]*t2[1][j] + t1[i][2]*t2[2][j];
+}
+
+/*******************************************************************************
+* Copy a rotation transformation (needed since arrays cannot be assigned in C).
+*******************************************************************************/
+void
+rot_copy(Rotation dest, Rotation src)
+{
+  dest[0][0] = src[0][0];
+  dest[0][1] = src[0][1];
+  dest[0][2] = src[0][2];
+  dest[1][0] = src[1][0];
+  dest[1][1] = src[1][1];
+  dest[1][2] = src[1][2];
+  dest[2][0] = src[2][0];
+  dest[2][1] = src[2][1];
+  dest[2][2] = src[2][2];
+}
+
+void
+rot_transpose(Rotation src, Rotation dst)
+{
+  dst[0][0] = src[0][0];
+  dst[0][1] = src[1][0];
+  dst[0][2] = src[2][0];
+  dst[1][0] = src[0][1];
+  dst[1][1] = src[1][1];
+  dst[1][2] = src[2][1];
+  dst[2][0] = src[0][2];
+  dst[2][1] = src[1][2];
+  dst[2][2] = src[2][2];
+}
+
+Coords
+rot_apply(Rotation t, Coords a)
+{
+  Coords b;
+
+  b.x = t[0][0]*a.x + t[0][1]*a.y + t[0][2]*a.z;
+  b.y = t[1][0]*a.x + t[1][1]*a.y + t[1][2]*a.z;
+  b.z = t[2][0]*a.x + t[2][1]*a.y + t[2][2]*a.z;
+  return b;
+}
+
+void
+mccoordschange(Coords a, Rotation t, double *x, double *y, double *z,
+	       double *vx, double *vy, double *vz, double *time,
+	       double *s1, double *s2)
+{
+  Coords b, c;
+
+  b.x = *x;
+  b.y = *y;
+  b.z = *z;
+  c = rot_apply(t, b);
+  b = coords_add(c, a);
+  *x = b.x;
+  *y = b.y;
+  *z = b.z;
+
+  b.x = *vx;
+  b.y = *vy;
+  b.z = *vz;
+  c = rot_apply(t, b);
+  *vx = b.x;
+  *vy = b.y;
+  *vz = b.z;
+  /* ToDo: What to do about the spin? */
+}
+
+
+void
+mcreadparams(void)
+{
+  extern struct {
+    char *name;
+    MCNUM *par;
+  } mcinputtable[];
+  int i;
+
+  for(i = 0; mcinputtable[i].name != 0; i++)
+  {
+    printf("Set value of instrument parameter %s: ", mcinputtable[i].name);
+    scanf("%lf", mcinputtable[i].par);
+  }
+}
+
+
+void
+mcgenstate(void)
+{
+  extern double mcnx, mcny, mcnz, mcnvx, mcnvy, mcnvz, mcnt, mcns1, mcns2, mcnp;
+  
+  mcnx = 0;
+  mcny = 0;
+  mcnz = 0;
+  mcnvx = 0;
+  mcnvy = 0;
+  mcnvz = 1;
+  mcnt = 0;
+  mcns1 = 0;
+  mcns2 = 0;
+  mcnp = 1;
+}
---
+++ trunk/lib/share/mcstas-r.h
@@ -0,0 +1,37 @@
+/*******************************************************************************
+* Runtime system for McStas.
+*
+*	Project: Monte Carlo Simulation of Tripple Axis Spectrometers
+*	File name: mcstas-r.h
+*
+*	Author: K.N.			Aug 29, 1997
+*
+*	$Id: mcstas-r.h,v 1.1 1997-09-08 10:39:44 kn Exp $
+*
+*	$Log: not supported by cvs2svn $
+*
+* Copyright (C) Risoe National Laboratory, 1991-1997, All rights reserved
+*******************************************************************************/
+
+typedef double MCNUM;
+typedef struct {MCNUM x, y, z;} Coords;
+typedef MCNUM Rotation[3][3];
+
+#ifdef DEBUG
+#define mcDEBUG_STATE(x,y,z,vx,vy,vz,t,s1,s2,p) \
+  printf("STATE: %g, %g, %g, %g, %g, %g, %g, %g, %g, %g\n", \
+	 x,y,z,vx,vy,vz,t,s1,s2,p)
+#else
+#define mcDEBUG_STATE(x,y,z,vx,vy,vz,t,s1,s2,p)
+#endif
+
+Coords coords_set(MCNUM x, MCNUM y, MCNUM z);
+Coords coords_add(Coords a, Coords b);
+Coords coords_sub(Coords a, Coords b);
+Coords coords_neg(Coords a);
+
+void rot_set_rotation(Rotation t, double phx, double phy, double phz);
+void rot_mul(Rotation t1, Rotation t2, Rotation t3);
+void rot_copy(Rotation dest, Rotation src);
+void rot_transpose(Rotation src, Rotation dst);
+Coords rot_apply(Rotation t, Coords a);
