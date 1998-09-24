5512411b5368ddb43329362104cb7fa9d98d2a83
kn <kn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
1998-09-24 12:15:30 +0000

    Added print_warn() function.

--- trunk/debug.c
+++ trunk/debug.c
@@ -6,9 +6,12 @@
 *
 *	Author: K.N.			Jul  1, 1997
 *
-*	$Id: debug.c,v 1.2 1997-07-02 07:22:53 kn Exp $
+*	$Id: debug.c,v 1.3 1998-09-24 12:15:30 kn Exp $
 *
 *	$Log: not supported by cvs2svn $
+*	Revision 1.2  1997/07/02 07:22:53  kn
+*	Error reporting functions.
+*
 *	Revision 1.1  1997/07/01 08:17:57  kn
 *	Initial revision
 *
@@ -31,6 +34,27 @@ print_error(char *format, ...)
 
   va_start(ap, format);
   vfprintf(stderr, format, ap);
+  va_end(ap);
+}
+
+
+/* Print a warning message. May optionally take a pointer to a flag of type
+ * int; this should be NULL or point to a variable that is initialized to
+ * zero. It the flag is given, the warning will only be displayed the first
+ * time this function is called.
+ */
+void
+print_warn(int *flag, char *format, ...)
+{
+  va_list ap;
+
+  va_start(ap, format);
+  if(flag == NULL || *flag == 0)
+  {
+    fprintf(stderr, "Warning: ");
+    vfprintf(stderr, format, ap);
+    *flag = 1;
+  }
   va_end(ap);
 }
 
