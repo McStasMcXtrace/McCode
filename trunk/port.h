75ec0ebc095b72553e46455e93f237ac8d493714
kn <kn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
1999-04-16 07:40:36 +0000

    Added Mac OS-specific defines.

--- trunk/port.h
+++ trunk/port.h
@@ -9,24 +9,41 @@
 * Copyright (C) Risoe National Laboratory, 1998, All rights reserved
 *******************************************************************************/
 
+/* Machintosh specific compiler defines. */
+#ifdef __dest_os
+#if (__dest_os == __mac_os)
+#define MAC
+#endif
+#endif
+
 /* File system details. */
 #ifdef WIN32
 #define PATHSEP_S "\\"
 #define PATHSEP_C '\\'
 #define CURRENT_DIR_S "."
-#else
+#else  /* !WIN32 */
+#ifdef MAC
+#define PATHSEP_S ":"
+#define PATHSEP_C ':'
+#define CURRENT_DIR_S ""	/* Apparently no Mac equivalent for this. */
+#else  /* !WIN32 && !MAC */
 #define PATHSEP_S "/"
 #define PATHSEP_C '/'
 #define CURRENT_DIR_S "."
-#endif
+#endif /* !MAC */
+#endif /* !WIN32 */
 
 #ifndef MC_SYS_DIR
 #ifdef WIN32
 #define MC_SYS_DIR "C:\\mcstas\\lib"
-#else
+#else  /* !WIN32 */
+#ifdef MAC
+#define MC_SYS_DIR ":mcstas:lib" /* ToDo: What to put here? */
+#else  /* !MAC */
 #define MC_SYS_DIR "/usr/local/lib/mcstas"
-#endif
-#endif
+#endif /* !MAC */
+#endif /* !WIN32 */
+#endif /* MC_SYS_DIR */
 
 #ifndef HAVE_STRCASECMP
 int strcasecmp(char *, char *);
