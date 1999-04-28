9b0970f98a9bf8b2462fbf897f102d28cddad871
kn <kn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
1999-04-28 09:13:14 +0000

    Improved initial help message. Default for postscript output is now black&white; the `C' jey produces the old color postscript.

--- trunk/mcplot.pl
+++ trunk/mcplot.pl
@@ -19,27 +19,27 @@ my ($instr_inf, $sim_inf, $datalist) = read_sim_file($file);
 die "No data in simulation file '$file'"
     unless @$datalist;
 
-print <<END;
-Click plot for full-screen view.
-Type 'P' (in graphics window) for hardcopy, 'Q' to quit.
-END
+print "Click on a plot for full-window view.\n" if @$datalist > 1;
+print "Type 'P' (in graphics window) for hardcopy, 'Q' to quit.\n";
 
 for(;;) {
     my ($cc,$cx,$cy,$idx);
     # Do overview plot, letting user select a plot for full-screen zoom.
     ($cc,$idx) = overview_plot("/xserv", $datalist, 1);
     last if $cc =~ /[xq]/i;	# Quit?
-    if($cc =~ /p/i) {		# Hardcopy?
-	overview_plot("mcstas.ps/cps", $datalist, 0);
+    if($cc =~ /[pc]/i) {	# Hardcopy?
+	my $dev = ($cc =~ /c/i) ? "cps" : "ps";
+	overview_plot("mcstas.ps/$dev", $datalist, 0);
 	print "Wrote postscript file 'mcstas.ps'\n";
 	next;
     }
     # now do a full-screen version of the plot selected by the user.
     ($cc, $cx, $cy) = single_plot("/xserv", $datalist->[$idx], 1);
     last if $cc =~ /[xq]/i;	# Quit?
-    if($cc =~ /p/i) {		# Hardcopy?
+    if($cc =~ /[pc]/i) {	# Hardcopy?
+	my $dev = ($cc =~ /c/i) ? "cps" : "ps";
 	my $filename = "$datalist->[$idx]{'Component'}.ps";
-	single_plot("$filename/cps", $datalist->[$idx], 0);
+	single_plot("$filename/$dev", $datalist->[$idx], 0);
 	print "Wrote postscript file '$filename'\n";
     }	
 }
