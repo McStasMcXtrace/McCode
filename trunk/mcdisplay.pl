e0f4392c1198e251c04f9784f55008cee50950ea
farhi <farhi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2003-04-16 14:52:46 +0000

    corrected help

--- trunk/mcdisplay.pl
+++ trunk/mcdisplay.pl
@@ -838,7 +838,7 @@ die "Usage: mcdisplay [-mzipf][-gif|-ps|-psc] Instr.out [instr_options] params
  -gif|-ps|-psc               Export figure as gif/b&w ps/color ps and exit
  When using -ps -psc -gif, the program writes the hardcopy file
  and then exits (plotter PGPLOT only).
- SEE ALSO: mcstas, mcplot, mcrun, mcresplot, mcstas2vitess, mcgui"
+ SEE ALSO: mcstas, mcplot, mcrun, mcresplot, mcstas2vitess, mcgui\n"
  unless $sim_cmd;
 
 if ($sim_cmd =~ m'\.instr$') # recompile .instr if needed
