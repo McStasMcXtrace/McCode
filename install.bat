36d8872a302616caefe7ebe0d59e77bef3462a25
pkwi <pkwi@fysik.dtu.dk>
2003-05-16 14:43:01 +0000

    1) Cure for .eps saving problem in mcplot.sci. 2) Enhanced batch files for windows 2000, now query needed variables using set /p. NOTE: This is incompatible with earlier versions of windows!!! - Problem is: choice command only on windows 2000 including the resource kit... Joys of the M$ World... :(

--- install.bat
+++ install.bat
@@ -4,21 +4,26 @@
 @REM 
 @REM Please modify the path below for installing mcstas in non-standard 
 @REM location
-@SET MCSTAS=c:\mcstas
+@if "%MCSTAS_SITE%"=="" set MCSTAS_SITE=c:\mcstas
+@SET /P MCSTAS_SITE=Set McStas base directory (default is %MCSTAS_SITE%): 
 @echo McStas install.bat for Win32...
-@echo Installing in MCSTAS=%MCSTAS%
-@if exist %MCSTAS% goto bin
-@mkdir %MCSTAS%
+@echo Installing in MCSTAS_SITE=%MCSTAS_SITE%
+@if exist %MCSTAS_SITE% goto bin
+@echo Creating directory %MCSTAS_SITE%
+@mkdir %MCSTAS_SITE%
 :bin
-@if exist %MCSTAS%\bin goto lib
-@mkdir %MCSTAS%\bin
+@if exist %MCSTAS_SITE%\bin goto lib
+@echo Creating directory %MCSTAS_SITE%\bin
+@mkdir %MCSTAS_SITE%\bin
 :lib
-@if exist %MCSTAS%\lib goto inst
-@mkdir %MCSTAS%\lib
+@if exist %MCSTAS_SITE%\lib goto inst
+@echo Creating directory %MCSTAS_SITE%\lib
+@mkdir %MCSTAS_SITE%\lib
 :inst
-@copy mcstas.exe %MCSTAS%\bin
-@copy *.pl %MCSTAS%\bin
-@xcopy /e /y /q /i lib %MCSTAS%\lib
+@echo Copying in the files...
+@copy mcstas.exe %MCSTAS_SITE%\bin
+@copy *.pl %MCSTAS_SITE%\bin
+@xcopy /e /y /q /i lib %MCSTAS_SITE%\lib
 @echo Done
-@echo Please remember to add %MCSTAS%\bin to your path!
-@echo Also, set the MCSTAS environment variable to %MCSTAS%\lib
+@echo Please remember to add %MCSTAS_SITE%\bin to your path!
+@echo Also, set the MCSTAS environment variable to %MCSTAS_SITE%\lib
