caf46f9d68719afa47e06485623cb9521cf4f1dd
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2003-11-28 13:20:35 +0000

    We are now going GPL (GNU General Public License). Existing McStas type headers have not been touched, e.g. the .c files have not been edited. The main README file should probably get an update to point to the new GPL style COPYING file, and the LICENSE should be retired. More work is needed in the lib directory, where LGPL and README should be edited / removed.

--- trunk/install.bat
+++ trunk/install.bat
@@ -1,9 +1,28 @@
+@rem  
+@rem   This file is part of the McStas neutron ray-trace simulation package
+@rem   Copyright (C) 1997-2003, All rights reserved
+@rem   Risoe National Laborartory, Roskilde, Denmark
+@rem   Institut Laue Langevin, Grenoble, France
+@rem
+@rem   This program is free software; you can redistribute it and/or modify
+@rem   it under the terms of the GNU General Public License as published by
+@rem   the Free Software Foundation; either version 2 of the License, or
+@rem   (at your option) any later version.
+@rem
+@rem   This program is distributed in the hope that it will be useful,
+@rem   but WITHOUT ANY WARRANTY; without even the implied warranty of
+@rem   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@rem   GNU General Public License for more details.
+@rem
+@rem   You should have received a copy of the GNU General Public License
+@rem   along with this program; if not, write to the Free Software
+@rem   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 @echo off
-@REM Simple batch script for installation of McStas on Win32 systems,
-@REM after build using Bloodshed Dev-Cpp. 
-@REM 
-@REM Please modify the path below for installing mcstas in non-standard 
-@REM location
+@rem Simple batch script for installation of McStas on Win32 systems,
+@rem after build using Bloodshed Dev-Cpp. 
+@rem 
+@rem Please modify the path below for installing mcstas in non-standard 
+@rem location
 @if "%MCSTAS_SITE%"=="" set MCSTAS_SITE=c:\mcstas
 @SET /P MCSTAS_SITE=Set McStas base directory (default is %MCSTAS_SITE%): 
 @echo McStas install.bat for Win32...
