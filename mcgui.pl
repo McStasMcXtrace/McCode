d64dde44c533d5cfdc759f5df915e325105aa980
farhi <farhi@ill.fr>
2005-06-15 12:32:19 +0000

    Added citations to use in mcgui:about dialog

--- mcgui.pl
+++ mcgui.pl
@@ -237,14 +237,16 @@ sub mcdoc_test {
 sub mcdoc_about {
   my ($w) = @_;
   my $version = `mcstas --version`;
-  my $ret = $w->messageBox(-message => "This is the McStas Graphical User Interface\n
-  McStas is a tool for Monte Carlo neutron scattering simulations\n
-  It provides a complete set of tools, components, and example instruments\n
-  $version\n
+  my $ret = $w->messageBox(-message => "This is the McStas Graphical User Interface. McStas is a tool for Monte Carlo neutron scattering simulations. It provides a complete set of tools, components, and example instruments.\n
+  This software required a significant effort to be brought to you. If you enjoy it, please use following references in your work:\n
+  P. Willendrup, E. Farhi and K. Lefmann, Physica B, 350 (2004) 735.\n
+  K. Lefmann and K. Nielsen, Neutron News 10, 20, (1999).\n
+  $version
   Please visit <http://neutron.risoe.dk/mcstas/>",
                                  -title => "McGUI: About McStas",
                                  -type => 'OK',
                                  -icon => 'info',
+                                 -width => 40,
                                  -default => 'cancel');
 }
 
