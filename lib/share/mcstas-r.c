1dd564f1cbf1df76b2799f82c3deb113bb8b1615
pkwi <pkwi@fysik.dtu.dk>
2006-03-02 12:39:33 +0000

    Corrected typo in last commit:
    tout should have been t_out - resulted in:
    
    lp-07151:~> mcrun -c vanadium_example.instr
    Translating instrument definition 'vanadium_example.instr' into C ...
    mcstas -t -o vanadium_example.c vanadium_example.instr
    Warning: 'Source_flat' is an obsolete component (not maintained).
    Compiling C source 'vanadium_example.c' ...
    gcc -g -O2 -o vanadium_example.out vanadium_example.c -lm
    mcstas-r.c: In function `cylinder_intersect':
    mcstas-r.c:3713: error: `tout' undeclared (first use in this function)
    mcstas-r.c:3713: error: (Each undeclared identifier is reported only once
    mcstas-r.c:3713: error: for each function it appears in.)
    ** Error exit **
    lp-07151:~>
    
    Please make simple tests of compilation etc. before committing...

--- lib/share/mcstas-r.c
+++ lib/share/mcstas-r.c
@@ -11,16 +11,19 @@
 * Written by: KN
 * Date:    Aug 29, 1997
 * Release: McStas 1.6
-* Version: $Revision: 1.125 $
+* Version: $Revision: 1.126 $
 *
 * Runtime system for McStas.
 * Embedded within instrument in runtime mode.
 *
 * Usage: Automatically embbeded in the c code whenever required.
 *
-* $Id: mcstas-r.c,v 1.125 2006-03-01 16:06:25 farhi Exp $
+* $Id: mcstas-r.c,v 1.126 2006-03-02 12:39:33 pkwi Exp $
 *
 * $Log: not supported by cvs2svn $
+* Revision 1.125  2006/03/01 16:06:25  farhi
+* Fixed error in cylinder_intersect when trajectory is parallel to the cylinder axis (raised by T. Vanvuure).
+*
 * Revision 1.124  2005/12/12 13:43:14  farhi
 * remove gridding on Matlab in-line plots
 *
@@ -3707,7 +3710,7 @@ cylinder_intersect(double *t0, double *t1, double x, double y, double z,
       t_out = (-(2*vz*z + 2*vx*x) + sqrt(D))/(2*(vz*vz + vx*vx));
     } else if (vy) { /* trajectory parallel to cylinder axis */
       t_in = (y + h/2)/vy;
-      tout = (y - h/2)/vy;
+      t_out = (y - h/2)/vy;
     } else return 0;
     y_in = vy*t_in + y;
     y_out =vy*t_out + y;
