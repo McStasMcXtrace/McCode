78b8fa44029d9b2d36f52a9a772ecab234f6c283
pchr <nobody@mccode.org>
2006-07-31 13:17:10 +0000

    Made a library with some polarisation routines.

---
+++ lib/share/pol-lib.c
@@ -0,0 +1,139 @@
+/****************************************************************************
+*
+* McStas, neutron ray-tracing package
+*         Copyright 1997-2006, All rights reserved
+*         Risoe National Laboratory, Roskilde, Denmark
+*         Institut Laue Langevin, Grenoble, France
+*
+* Library: share/pol-lib.c
+*
+* %Identification
+* Written by: Peter Christiansen
+* Date: August, 2006
+* Origin: RISOE
+* Release: McStas 1.10
+* Version: $Revision: 1.1 $
+*
+* This file is to be imported by polarisation components. 
+* It handles some shared functions. 
+* Embedded within instrument in runtime mode.
+* Variable names have prefix 'mc_pol_' for 'McStas Polarisation' 
+* to avoid conflicts
+*
+* Usage: within SHARE
+* %include "pol-lib"
+*
+* $Id: pol-lib.c,v 1.1 2006-07-31 13:17:10 pchr Exp $
+*
+* $Log: not supported by cvs2svn $
+*
+****************************************************************************/
+
+#ifndef POL_LIB_H
+#include "pol-lib.h"
+#endif
+
+/****************************************************************************
+* void GetMonoPolFNFM(double Rup, double Rdown, double *FN, double *FM)
+* 
+* ACTION: Calculate FN and FM from reflectivities Rup and Rdown
+*
+* For a monochromator (nuclear and magnetic scattering), the
+* polarisation is done by defining the reflectivity for spin up (Rup)
+* and spin down (Rdown) (which can be negative, see now!) and based on
+* this the nuclear and magnetic structure factors are calculated:
+* FM = sign(Rup)*sqrt(|Rup|) + sign(Rdown)*sqrt(|Rdown|)
+* FN = sign(Rup)*sqrt(|Rup|) - sign(Rdown)*sqrt(|Rdown|)
+*****************************************************************************/
+void GetMonoPolFNFM(double mc_pol_Rup, double mc_pol_Rdown, 
+		    double *mc_pol_FN, double *mc_pol_FM){
+  if (mc_pol_Rup>0)
+    mc_pol_Rup   = sqrt(fabs(mc_pol_Rup));
+  else
+    mc_pol_Rup   = -sqrt(fabs(mc_pol_Rup));
+  
+  if (mc_pol_Rdown>0)
+    mc_pol_Rdown = sqrt(fabs(mc_pol_Rdown));
+  else
+    mc_pol_Rdown = -sqrt(fabs(mc_pol_Rdown));
+  
+  *mc_pol_FN = 0.5*(mc_pol_Rup + mc_pol_Rdown);
+  *mc_pol_FM = 0.5*(mc_pol_Rup - mc_pol_Rdown);
+  return;
+}
+
+/****************************************************************************
+* void GetMonoPolRefProb(double FN, double FM, double sy, double *prob)
+* 
+* ACTION: Calculate reflection probability from sy, FN and FM
+*
+* For a monochromator with up direction along y the reflection
+* probability is given as:
+* prob = FN*FN + 2*FN*FM*sy_in + FM*FM 
+*     (= |Rup| + |Rdown| (for sy_in=0))
+* where FN and FM are calculated from Rup and Rdown by GetMonoPolFNFM
+*****************************************************************************/
+void GetMonoPolRefProb(double mc_pol_FN, double mc_pol_FM, 
+		       double mc_pol_sy, double *mc_pol_prob) {
+  *mc_pol_prob = mc_pol_FN*mc_pol_FN + mc_pol_FM*mc_pol_FM
+    + 2*mc_pol_FN*mc_pol_FM*mc_pol_sy;
+  return;
+}
+
+/****************************************************************************
+* void SetMonoPolRefOut(double FN, double FM, double refProb,
+*		     double* sx, double* sy, double* sz) {
+* 
+* ACTION: Set the outgoing polarisation vector of the reflected neutrons
+* given FN, FM and the reflection probability.
+*
+* For a monochromator with up direction along y the outgoing polarisation
+* is given as:
+*	sx = (FN*FN - FM*FM)*sx_in/R0;
+*	sy = ((FN*FN - FM*FM)*sy_in + 2*FN*FM + FM*FM*sy_in)/R0;
+*	sz = (FN*FN - FM*FM)*sz_in/R0;
+* where sx_in, sy_in, and sz_in is the incoming polarisation, and
+* FN and FM are calculated from Rup and Rdown by GetMonoPolFNFM
+*****************************************************************************/
+void SetMonoPolRefOut(double mc_pol_FN, double mc_pol_FM, 
+		      double mc_pol_refProb, double* mc_pol_sx, 
+		      double* mc_pol_sy, double* mc_pol_sz) {
+  *mc_pol_sx = (mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sx)
+    /mc_pol_refProb;
+  *mc_pol_sy = ((mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sy) 
+		+ 2*mc_pol_FN*mc_pol_FM + 2*mc_pol_FM*mc_pol_FM*(*mc_pol_sy))
+    /mc_pol_refProb;
+  *mc_pol_sz = (mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sz)
+    /mc_pol_refProb;
+  return;
+}
+
+/****************************************************************************
+* void SetMonoPolTransOut(double FN, double FM, double refProb, 
+*			  double* sx, double* sy, double* sz) {
+* 
+* ACTION: Set the outgoing polarisation vector of the transmitted neutrons
+* given FN, FM and the REFLECTION probability.
+*
+* We use that the polarization is conserved so:
+* s_in = refProb*s_ref+(1-refProb)*s_trans, and then
+* s_trans = (s_in-refProb*s_ref)/(1-refProb)
+* where refProb is calculated using the routine GetMonoPolRefProb
+* and s_ref is calculated by SetMonoPolRefOut
+*****************************************************************************/
+void SetMonoPolTransOut(double mc_pol_FN, double mc_pol_FM, 
+			double mc_pol_refProb, double* mc_pol_sx, 
+			double* mc_pol_sy, double* mc_pol_sz) {
+  double mc_pol_sx_ref = *mc_pol_sx, mc_pol_sy_ref = *mc_pol_sy;
+  double mc_pol_sz_ref = *mc_pol_sz;
+  
+  // By passing 1 as probability we get mc_pol_refProb*s_out_ref
+  SetMonoPolRefOut(mc_pol_FN, mc_pol_FM, 1, 
+		   &mc_pol_sx_ref, &mc_pol_sy_ref, &mc_pol_sz_ref);
+  *mc_pol_sx = (*mc_pol_sx - mc_pol_sx_ref)/(1 - mc_pol_refProb);
+  *mc_pol_sy = (*mc_pol_sy - mc_pol_sy_ref)/(1 - mc_pol_refProb);
+  *mc_pol_sz = (*mc_pol_sz - mc_pol_sz_ref)/(1 - mc_pol_refProb);
+  return;
+}
+
+/* end of pol-lib.c */
