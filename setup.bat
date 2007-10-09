0b558a25cbfa0128b560b195204380cdb0e44dd0
pkwi <pkwi@fysik.dtu.dk>
2007-10-09 10:08:15 +0000

    From next release, McStas is GPL 2 only.
    The debate on the internet about the future GPL 3 suggests that this license might have implications on the 'derived work', hence have implications on what and how our users use their McStas simulations.

--- setup.bat
+++ setup.bat
@@ -6,8 +6,7 @@
 @rem
 @rem   This program is free software; you can redistribute it and/or modify
 @rem   it under the terms of the GNU General Public License as published by
-@rem   the Free Software Foundation; either version 2 of the License, or
-@rem   (at your option) any later version.
+@rem   the Free Software Foundation; version 2 of the License.
 @rem
 @rem   This program is distributed in the hope that it will be useful,
 @rem   but WITHOUT ANY WARRANTY; without even the implied warranty of
