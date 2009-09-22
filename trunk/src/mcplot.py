cc5a8b7d894517b1f9f1b33d8f94df385a404d67
willend <willend@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2009-09-22 21:31:28 +0000

    Scan files hacked in there...

--- trunk/src/mcplot.py
+++ trunk/src/mcplot.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 import sys;
+import os;
 
 import matplotlib
 if sys.platform == 'darwin':
@@ -24,46 +26,65 @@ def mcplot_single(File):
     # Read the header and determine type of dataset
     Header = filter(isHeader, open(File).readlines())
     Type = filter(isType,Header); Type = Type[0].split(': '); Type = Type[1].strip(); Type = Type.split('('); Type = Type[0];
-    
-    # Common stuff for 1 and 2 d:
+
     Xlabel = filter(isXlabel,Header); Xlabel = Xlabel[0].split(': '); Xlabel = Xlabel[1].strip();
-    Ylabel = filter(isYlabel,Header); Ylabel = Ylabel[0].split(': '); Ylabel = Ylabel[1].strip();
-    Component = filter(isComp,Header); Component = Component[0].split(': '); Component = Component[1].strip();
-    Filename = filter(isFilename,Header); Filename = Filename[0].split(': '); Filename = Filename[1].strip();
-    Title = filter(isTitle,Header); Title = Title[0].split(': '); Title = Title[1].strip();
-    Values = filter(isValues,Header); Values = Values[0].split(': '); Values = Values[1].split();
-    Statistics = filter(isStatistics,Header); Statistics = Statistics[0].split(': '); Statistics = Statistics[1].strip();
-    
-    data=loadtxt(File);
-    
-    if Type == 'array_1d':
-        Xlimits = filter(isXlimits,Header); Xlimits = Xlimits[0].split(': '); Xlimits = Xlimits[1].split(); 
-        Xmin = eval(Xlimits[0]); Xmax = eval(Xlimits[1]);
-        
+
+    if Type == 'multiarray_1d':
+        Variables = filter(isVariables,Header); Variables = Variables[0].split(': '); Variables = Variables[1].split();
+        L=len(Variables);
+        dims = calc_panel_size((L-1)/2);    
+        data=loadtxt(File);
         x=data[:,0];
-        y=data[:,1];
-        dy=data[:,2];
-        errorbar(x,y,dy);
-        xlim(Xmin,Xmax);
-    elif Type == 'array_2d':
-        mysize=data.shape;
-        I=data[0:mysize[0]/3,...];
-        mysize=I.shape;
-        XYlimits = filter(isXYlimits,Header); XYlimits = XYlimits[0].split(': '); XYlimits = XYlimits[1].split(); 
-        Xmin = eval(XYlimits[0]); Xmax = eval(XYlimits[1]);
-        Ymin = eval(XYlimits[2]); Ymax = eval(XYlimits[3]);
-        x = linspace(Xmin,Xmax,mysize[1]);
-        y = linspace(Ymin,Ymax,mysize[0]);       
-        pcolor(x,y,I);
-        ylim(Ymin,Ymax);
+        for j in range(1, 1+(L-1)/2):
+            subplot(dims[0],dims[1],j);
+            I=data[:,2*j-1];
+            E=data[:,2*j];
+            errorbar(x,I,E);
+            xlabel(Variables[0], fontsize=4);
+            ylabel("Intensity " + Variables[2*j-1], fontsize=4);
+            title(Variables[2*j-1] + " " + File + "\n Scan of " + Variables[0], fontsize=4)
+    else:
+        # Common stuff for 1 and 2 d:
+        Ylabel = filter(isYlabel,Header); Ylabel = Ylabel[0].split(': '); Ylabel = Ylabel[1].strip();
+        Component = filter(isComp,Header); Component = Component[0].split(': '); Component = Component[1].strip();
+        Filename = filter(isFilename,Header); Filename = Filename[0].split(': '); Filename = Filename[1].strip();
+        Title = filter(isTitle,Header); Title = Title[0].split(': '); Title = Title[1].strip();
+        Values = filter(isValues,Header); Values = Values[0].split(': '); Values = Values[1].split();
+        Statistics = filter(isStatistics,Header); Statistics = Statistics[0].split(': '); Statistics = Statistics[1].strip();
         
+        data=loadtxt(File);
+    
+    
+
+        if Type == 'array_1d':
+            Xlimits = filter(isXlimits,Header); Xlimits = Xlimits[0].split(': '); Xlimits = Xlimits[1].split(); 
+            Xmin = eval(Xlimits[0]); Xmax = eval(Xlimits[1]);
+        
+            x=data[:,0];
+            y=data[:,1];
+            dy=data[:,2];
+            errorbar(x,y,dy);
+            xlim(Xmin,Xmax);
+        elif Type == 'array_2d':
+            mysize=data.shape;
+            I=data[0:mysize[0]/3,...];
+            mysize=I.shape;
+            XYlimits = filter(isXYlimits,Header); XYlimits = XYlimits[0].split(': '); XYlimits = XYlimits[1].split(); 
+            Xmin = eval(XYlimits[0]); Xmax = eval(XYlimits[1]);
+            Ymin = eval(XYlimits[2]); Ymax = eval(XYlimits[3]);
+            x = linspace(Xmin,Xmax,mysize[1]);
+            y = linspace(Ymin,Ymax,mysize[0]);       
+            pcolor(x,y,I);
+            ylim(Ymin,Ymax);
+            
         xlim(Xmin,Xmax);
         xlabel(Xlabel);
         ylabel(Ylabel);
-        
+    
         Title = Component + ' [' + Filename + '], ' + Title + '\n' + 'I=' + Values[0] + ' E=' + Values[1] + ' N=' + Values[2] + '; ' + Statistics;
+    
         title(Title, fontsize=8)
-        return 0;
+    return 0;
 
 def calc_panel_size(num):
     Panels = ( [1,1], [2,1], [2,2], [3,2], [3,3], [4,3], [5,3], [4,4],
@@ -98,19 +119,28 @@ isCompFilename = lambda line: line.startswith('    filename:');
 SimFile = filter(isBegin, open(File).readlines());
 
 if SimFile == []:
-    print "File " + File + " is a single monitor file - plotting\n";
     mcplot_single(File);
     show();
 else:
     # Get filenames from the sim file
     MonFiles = filter(isCompFilename, open(File).readlines());
     L = len(MonFiles);
-    dims = calc_panel_size(L);
+    
+    # Scan or oveview?
+    if L==0:
+        isFilename = lambda line: line.startswith('filename');
+        Scanfile = filter(isFilename, open(File).readlines()); Scanfile = Scanfile[0].split(': '); 
+        Scanfile = os.path.join(os.path.dirname(File),Scanfile[1].strip()); 
+        # Proceed to scan datafile
+        mcplot_single(Scanfile);
 
-    for j in range(0, L):
-        subplot(dims[0],dims[1],j+1);
-        MonFile = MonFiles[j].split(':'); MonFile = MonFile[1].strip();
-        mcplot_single(MonFile);
+    else:
+        dims = calc_panel_size(L);
+
+        for j in range(0, L):
+            subplot(dims[0],dims[1],j+1);
+            MonFile = MonFiles[j].split(':'); MonFile = MonFile[1].strip();
+            mcplot_single(MonFile);
     show();
     
 
