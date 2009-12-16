1b74ce72b2776d41a87fe25aa3ed103a6f6c270d
msiebuhr <nobody@mccode.org>
2009-12-16 13:14:18 +0000

    Fixed up un-pythonic and broken things in mcrun.py.

--- src/mcrun.py
+++ src/mcrun.py
@@ -12,59 +12,124 @@ def get_binary(opts):
     
 
 def run_mcstas(opts):
-    print "Running mcstas -t " + opts.instrfile
-    os.system("mcstas -t " + opts.instrfile)
+    command = "%s -t %s" % (opts.mcstas, opts.instrfile)
+    if opts.verbose:
+        print "Running '%s'" % command
+    os.system(command)
     
 def run_cc(opts):
-    print "Running gcc -lm -O2 " + opts.cfile + " -o " +  opts.binfile
-    os.system("gcc -lm -O2 " + opts.cfile + " -o " +  opts.binfile)
+    command = "%s -lm %s -o %s" % (opts.cc, opts.cfile, opts.binfile)
+    if opts.verbose:
+        print "Running '%s'" % command
+    os.system(command)
     
 def singlesim(opts, args):
     get_binary(opts)
     cmd = opts.binfile
     for j in range(0,len(opts.parms)):
         cmd = cmd + " " + opts.parms[j]
-    print "Performing single simulation: " + cmd 
+    if opts.verbose:
+        print "Performing single simulation: " + cmd 
     os.system(cmd)
     
 def scan(opts, args):
-    print "Scanning"
+    if opts.verbose:
+        print "Scanning"
 
 def optim(opts,args):
-    print "Optimising"
+    if opts.verbose:
+        print "Optimising"
 
 
 if __name__ == "__main__":
-    from optparse import OptionParser
-    usage =         "Usage: %prog [-cpnN] Instr [-sndftgahi] params={val|min,max|min,guess,max}\n\n"
-    usage = usage + "  This program both runs mcstas with Instr and the C compiler to build an\n"
-    usage = usage + "  independent simulation program. The following environment variables may be\n"
-    usage = usage + "  specified for building the instrument:\n"
-    usage = usage + "  MCSTAS        Location of the McStas and component library\n"
-    usage = usage + "  MCSTAS_CC     Name of the C compiler               (gcc)\n"
-    usage = usage + "  MCSTAS_CFLAGS Options for compilation              (-g -O2)\n"
-    usage = usage + "  MCSTAS_FORMAT Default FORMAT to use for data files (PGPLOT)\n"
-    parser = OptionParser(usage=usage)
-    parser.add_option("-c", "--force-compile", action="store_true", dest="compile", help="Force rebuilding of instrument.")
-    parser.add_option("-p", "--param", dest="param", help="Read parameters from FILE.", metavar="FILE")
-    parser.add_option("-n","--ncount", help="Set number of neutrons to simulate.", metavar="NCOUNT", dest="NCOUNT")
-    parser.add_option("-N","--numpoints", dest="NP", metavar="NP", help="Set number of scan points.")
-    parser.add_option("--grid", action="store_true",dest="NB_GRID",help="Spawn simulations to multiple machine/cores grid.")
-    parser.add_option("--mpi",dest="NB_MPI",action="store_true",help="Spread simulation over NB_CPU machines using MPI.")
-    parser.add_option("--machines", dest="MACHINES", help="Read machine names from file MACHINES (MPI/grid).", metavar="MACHINES")
-    parser.add_option("--optim", dest="COMP",help="Add COMP to the list of monitors to maximize.\nIf COMP not given will maximise all monitors")
-    parser.add_option("--optim-prec", dest="PREC", help="Relative requested accuracy of criteria (1e-3)")    
-    parser.add_option("--optim-file", dest="OPTFILE", help="Defines filename for storing optim results.\n(Defaults to \"mcoptim_XXXX.dat\")")
-    parser.add_option("--test", help="Execute McStas selftest and generate report", action="store_true", dest="test")
-    parser.add_option("-s","--seed",dest="SEED", help="Set random seed (must be != 0)")
-    parser.add_option("-d", "--dir", dest="DIR", help="Put all data files in directory DIR.")
-    parser.add_option("-f","--file", dest="FILE", help="Put all data in a single file.")
-    parser.add_option("-t","--trace", action="store_true", help="Enable trace of neutron through instrument.")
-    parser.add_option("-g","--gravitation", action="store_true", help="Enable gravitation for all trajectories.")
-    parser.add_option("-a","--data-only", action="store_true", help="Do not put any headers in the data files.")
-    parser.add_option("--no-output-files", action="store_true", help="Do not write any data files.")
-    parser.add_option("-i","--info", action="store_true", help="Detailed instrument information.")
-    parser.add_option("--format", dest="FORMAT", help="Output data files using format FORMAT. (format list obtained from <instr>.out -h)")
+    import optparse
+    usage =  "Usage: %prog [-cpnN] Instr [-sndftgahi] params={val|min,max|min,guess,max}\n\n"
+    usage += "  This program both runs mcstas with Instr and the C compiler to build an\n"
+    usage += "  independent simulation program. The following environment variables may be\n"
+    usage += "  specified for building the instrument:\n"
+    usage += "  MCSTAS        Location of the McStas and component library\n"
+    #usage += "  MCSTAS_CC     Name of the C compiler               (gcc)\n"
+    #usage += "  MCSTAS_CFLAGS Options for compilation              (-g -O2)\n"
+    usage += "  MCSTAS_FORMAT Default FORMAT to use for data files (PGPLOT)\n"
+
+    parser = optparse.OptionParser(usage=usage)
+
+    # Set defaults
+    parser.set_defaults(
+            cc = 'gcc',
+            cflags = '-g -O2',
+            verbose = False,
+            compile = True,
+            mcstas = "mcstas"
+            )
+
+    # Find default mcstas to use.
+    if os.path.exists("./mcstas") and os.path.isfile("./mcstas"):
+        parser.set_defaults(mcstas = "./mcstas")
+    elif os.path.exists("./src/mcstas") and os.path.isfile("./src/mcstas"):
+        parser.set_defaults(mcstas = "./src/mcstas")
+
+    # Add options
+    parser.add_option("--verbose", "-v",
+            help = "Show what we're doing.",
+            action = "store_true")
+    #parser.add_option("-c", "--force-compile",
+    #        action="store_true", dest="compile", help="Force rebuilding of instrument.")
+    parser.add_option("-p", "--param",
+            dest="param", help="Read parameters from FILE.", metavar="FILE")
+    parser.add_option("-n", "--ncount",
+            help="Set number of neutrons to simulate.", metavar="NCOUNT", dest="NCOUNT")
+    parser.add_option("-N", "--numpoints",
+            dest="NP", metavar="NP", help="Set number of scan points.")
+    parser.add_option("--grid",
+            action="store_true",dest="NB_GRID",help="Spawn simulations to multiple machine/cores grid.")
+    parser.add_option("--mpi",
+            dest="NB_MPI",action="store_true",help="Spread simulation over NB_CPU machines using MPI.")
+    parser.add_option("--machines",
+            dest="MACHINES", help="Read machine names from file MACHINES (MPI/grid).", metavar="MACHINES")
+    parser.add_option("--optim",
+            dest="COMP",help="Add COMP to the list of monitors to maximize.\nIf COMP not given will maximise all monitors")
+    parser.add_option("--optim-prec",
+            dest="PREC", help="Relative requested accuracy of criteria (1e-3)")    
+    parser.add_option("--optim-file",
+            dest="OPTFILE", help="Defines filename for storing optim results.\n(Defaults to \"mcoptim_XXXX.dat\")")
+    parser.add_option("--test",
+            help="Execute McStas selftest and generate report", action="store_true", dest="test")
+    parser.add_option("-s","--seed",
+            dest="SEED", help="Set random seed (must be != 0)")
+    parser.add_option("-d", "--dir",
+            dest="DIR", help="Put all data files in directory DIR.")
+    parser.add_option("-f","--file",
+            dest="FILE", help="Put all data in a single file.")
+    parser.add_option("-t","--trace",
+            action="store_true", help="Enable trace of neutron through instrument.")
+    parser.add_option("-g","--gravitation",
+            action="store_true", help="Enable gravitation for all trajectories.")
+    parser.add_option("-a","--data-only",
+            action="store_true", help="Do not put any headers in the data files.")
+    parser.add_option("--no-output-files",
+            action="store_true", help="Do not write any data files.")
+    parser.add_option("-i","--info",
+            action="store_true", help="Detailed instrument information.")
+    parser.add_option("--format",
+            dest="FORMAT", help="Output data files using format FORMAT. (format list obtained from <instr>.out -h)")
+
+
+    optcc = optparse.OptionGroup(parser, "Controlling the C compilation.")
+    #optcc.add_option("--nocflags",
+    #        dest = "use_cflags",
+    #        help = "Do not use CFLAGS (slightly faster compilation)",
+    #        action = "store_false", default=True)
+    #optcc.add_option("--keep-c", "-k",
+    #        help = "Keep the intermediate C file.",
+    #        action = "store_true", default=False)
+    optcc.add_option("--cc",
+            help = "C compiler to use.")
+    optcc.add_option("--cflags",
+            help = "Flags passed to the C compiler.")
+    parser.add_option_group(optcc)
+
+
 
     (options, args) = parser.parse_args()
 
@@ -72,25 +137,24 @@ if __name__ == "__main__":
     options.parms = list()
     # Determine Instrument filename from args list
     for arg in args:
-        print arg
         equ = string.find(arg, "=")
         if string.find(arg, "=") >= 0:
              options.parms.append(arg);
         else:
              filenames.append(arg)
     
+    if len(filenames) == 0:
+        parser.error("No instrument file given.")
+
     if len(filenames) > 1:
-        print "Only a single instrument definition may be given - you gave:\n"
-        for j in range(0,len(filenames)):
-            print str(j+1) + "): " + filenames[j]
-            
-        exit()
+        parser.error("Too many instruments given. (%s)" % \
+                ", ".join(["'%s'" % fn for fn in filenames]))
     
     options.instrfile=filenames[0]
 
-    if (not(os.path.isfile(options.instrfile))):
-         print "Sorry, instrumentfile " + options.instrfile + " does not exist. Exiting"
-         exit()
+    if not os.path.isfile(options.instrfile):
+        parser.error("Sorry, instrumentfile '%s' does not exist." %
+                options.instrfile)
 
     # Add . if no path is in the filename on posix
     if os.name == 'posix' and os.path.dirname(options.instrfile) == '':
@@ -122,21 +186,3 @@ if __name__ == "__main__":
     else:
         singlesim(options,args)
 
-#    print options
-#    print args
-    
-    
-    
-    
-# Further stuff - read config (before parsing input args?)
-# mcrun_single for performing a single sim / step / opt..
-# Checking if run is scan or optim - functions
-
-
-# The following have been left out for now...
-#              --multi=NB_CPU     see the documentation for more info.
-#    --slave=HOST               Execute simulation on distant HOST (SSH grid)
-
-    
-
-# End of __main__
