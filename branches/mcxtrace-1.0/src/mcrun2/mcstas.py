5c33c1ad178e779c9dbacaee2da001933decbac2
erkn <erkn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-01-26 20:06:54 +0000

    Fix MPI cflags

--- branches/mcxtrace-1.0/src/mcrun2/mcstas.py
+++ branches/mcxtrace-1.0/src/mcrun2/mcstas.py
@@ -99,7 +99,7 @@ class McStas:
         cflags = [
             '-lm',  # math library
             options.no_cflags and '-O0' or '-O2',  # optimizing flags
-            mpi and '-DUSE_MPI' or '-D',  # MPI
+            mpi and '-DUSE_MPI' or '-UUSE_MPI',  # MPI
         ]
 
         # Compiler optimisation
