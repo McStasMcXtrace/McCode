8727ed41217fb7ca4d0bb5c7b1bec66fb924fcd0
erkn <erkn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-02-02 16:36:03 +0000

    split monitor_nD libs into xray and neutron parts
    This change to avoid overly complicated structure with lots of
    compiler conditionals

---
+++ branches/mcxtrace-1.0/xlib/share/monitor_nd-lib.h
@@ -0,0 +1,184 @@
+/*******************************************************************************
+*
+* McStas, neutron ray-tracing package
+*         Copyright 1997-2002, All rights reserved
+*         Risoe National Laboratory, Roskilde, Denmark
+*         Institut Laue Langevin, Grenoble, France
+*
+* Library: share/monitor_nd-lib.h
+*
+* %Identification
+* Written by: EF
+* Date: Aug 28, 2002
+* Origin: ILL
+* Release: McStas 1.6
+* Version: $Revision: 1.19 $
+*
+* This file is to be imported by the monitor_nd related components
+* It handles some shared functions.
+*
+* Usage: within SHARE
+* %include "monitor_nd-lib"
+*
+*******************************************************************************/
+
+#ifndef MONITOR_ND_LIB_H
+
+#define MONITOR_ND_LIB_H "$Revision: 1.19 $"
+#define MONnD_COORD_NMAX  30  /* max number of variables to record */
+
+  typedef struct MonitornD_Defines
+  {
+    int COORD_NONE  ;
+    int COORD_X     ;
+    int COORD_Y     ;
+    int COORD_Z     ;
+    int COORD_RADIUS; 
+    int COORD_VX    ;
+    int COORD_VY    ;
+    int COORD_VZ    ;
+    int COORD_V     ;
+    int COORD_T     ;
+    int COORD_P     ;
+    int COORD_SX    ;
+    int COORD_SY    ;
+    int COORD_SZ    ;
+    int COORD_KX    ;
+    int COORD_KY    ;
+    int COORD_KZ    ;
+    int COORD_K     ;
+    int COORD_ENERGY;
+    int COORD_LAMBDA;
+    int COORD_KXY   ;
+    int COORD_KYZ   ;
+    int COORD_KXZ   ;
+    int COORD_VXY   ;
+    int COORD_VYZ   ;
+    int COORD_VXZ   ;
+    int COORD_HDIV  ;
+    int COORD_VDIV  ;
+    int COORD_ANGLE ;
+    int COORD_NCOUNT;
+    int COORD_THETA ;
+    int COORD_PHI   ;
+    int COORD_USER1 ;
+    int COORD_USER2 ;
+    int COORD_USER3 ;
+    int COORD_XY    ;
+    int COORD_XZ    ;
+    int COORD_YZ    ;
+    int COORD_PHASE ;
+ 
+    /* token modifiers */
+    int COORD_VAR   ; /* next token should be a variable or normal option */
+    int COORD_MIN   ; /* next token is a min value */
+    int COORD_MAX   ; /* next token is a max value */
+    int COORD_DIM   ; /* next token is a bin value */
+    int COORD_FIL   ; /* next token is a filename */
+    int COORD_EVNT  ; /* next token is a buffer size value */
+    //int COORD_3HE   ; /* next token is a 3He pressure value */
+    int COORD_LOG   ; /* next variable will be in log scale */
+    int COORD_ABS   ; /* next variable will be in abs scale */
+    int COORD_SIGNAL; /* next variable will be the signal var */
+    int COORD_AUTO  ; /* set auto limits */
+
+    char TOKEN_DEL[32]; /* token separators */
+
+    char SHAPE_SQUARE; /* shape of the monitor */
+    char SHAPE_DISK  ;
+    char SHAPE_SPHERE;
+    char SHAPE_CYLIND;
+    char SHAPE_BANANA; /* cylinder without top/bottom, on restricted angular area */
+    char SHAPE_BOX   ;
+    char SHAPE_PREVIOUS;
+
+  } MonitornD_Defines_type;
+
+  typedef struct MonitornD_Variables
+  {
+    double area;
+    double Sphere_Radius     ;
+    double Cylinder_Height   ;
+    char   Flag_With_Borders ;   /* 2 means xy borders too */
+    char   Flag_List         ;   /* 1 store 1 buffer, 2 is list all, 3 list all+append */
+    char   Flag_Multiple     ;   /* 1 when n1D, 0 for 2D */
+    char   Flag_Verbose      ;
+    int    Flag_Shape        ;
+    char   Flag_Auto_Limits  ;   /* get limits from first Buffer */
+    char   Flag_Absorb       ;   /* monitor is also a slit */
+    char   Flag_Exclusive    ;   /* absorb particles out of monitor limits */
+    char   Flag_per_cm2      ;   /* flux is per cm2 */
+    char   Flag_log          ;   /* log10 of the flux */
+    char   Flag_parallel     ;   /* set neutron state back after detection (parallel components) */
+    char   Flag_Binary_List  ;
+    char   Flag_capture      ;   /* lambda monitor with lambda/lambda(2200m/s = 1.7985 Angs) weightening */
+    int    Flag_signal       ;   /* 0:monitor p, else monitor a mean value */
+
+    long   Coord_Number      ;   /* total number of variables to monitor, plus intensity (0) */
+    long   Buffer_Block      ;   /* Buffer size for list or auto limits */
+    long   Photon_Counter    ;   /* event counter, simulation total counts is mcget_ncount() */
+    long   Buffer_Counter    ;   /* index in Buffer size (for realloc) */
+    long   Buffer_Size       ;
+    int    Coord_Type[MONnD_COORD_NMAX];    /* type of variable */
+    char   Coord_Label[MONnD_COORD_NMAX][30];       /* label of variable */
+    char   Coord_Var[MONnD_COORD_NMAX][30]; /* short id of variable */
+    long   Coord_Bin[MONnD_COORD_NMAX];             /* bins of variable array */
+    double Coord_Min[MONnD_COORD_NMAX];
+    double Coord_Max[MONnD_COORD_NMAX];
+    char   Monitor_Label[MONnD_COORD_NMAX*30];      /* Label for monitor */
+    char   Mon_File[128];    /* output file name */
+
+    double cx,cy,cz;
+    double cvx, cvy, cvz;
+    double ckx, cky, ckz;
+    double cEx, cEy, cEz;
+    double ct, cphi, cp;
+    double He3_pressure;
+    char   Flag_UsePreMonitor    ;   /* use a previously stored parameter set */
+    char   UserName1[128];
+    char   UserName2[128];
+    char   UserName3[128];
+    double UserVariable1;
+    double UserVariable2;
+    double UserVariable3;
+    char   option[CHAR_BUF_LENGTH];
+
+    double Nsum;
+    double psum, p2sum;
+    double **Mon2D_N;
+    double **Mon2D_p;
+    double **Mon2D_p2;
+    double *Mon2D_Buffer;
+
+    double mxmin,mxmax,mymin,mymax,mzmin,mzmax;
+    double mean_dx, mean_dy, min_x, min_y, max_x, max_y, mean_p;
+
+    char   compcurname[128];
+    Coords compcurpos;
+
+  } MonitornD_Variables_type;
+
+/* monitor_nd-lib function prototypes */
+/* ========================================================================= */
+
+void Monitor_nD_Init(MonitornD_Defines_type *, MonitornD_Variables_type *, MCNUM, MCNUM, MCNUM, MCNUM, MCNUM, MCNUM, MCNUM, MCNUM, MCNUM);
+double Monitor_nD_Trace(MonitornD_Defines_type *, MonitornD_Variables_type *);
+MCDETECTOR Monitor_nD_Save(MonitornD_Defines_type *, MonitornD_Variables_type *);
+void Monitor_nD_Finally(MonitornD_Defines_type *, MonitornD_Variables_type *);
+void Monitor_nD_McDisplay(MonitornD_Defines_type *,
+ MonitornD_Variables_type *);
+
+#define MONND_DECLARE(monname) \
+  struct MonitornD_Variables *mcmonnd ## monname;
+#define MONND_USER_TITLE(monname, num, title) \
+  { mcmonnd ## monname = &(MC_GETPAR(monname, Vars)); \
+    strcpy(mcmonnd ## monname->UserName ## num, title); }
+#define MONND_USER_VALUE(monname, num, value) \
+  { mcmonnd ## monname = &(MC_GETPAR(monname, Vars)); \
+    mcmonnd ## monname->UserVariable ## num = (value); }
+
+#endif
+
+
+
+/* end of monitor_nd-lib.h */
