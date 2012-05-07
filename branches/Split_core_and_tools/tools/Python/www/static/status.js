5f2828524df593cc20a1be328019d0a63d950b1e
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-05-07 08:44:03 +0000

    Quick clean up

--- branches/Split_core_and_tools/tools/Python/www/static/status.js
+++ branches/Split_core_and_tools/tools/Python/www/static/status.js
@@ -1,15 +1,15 @@
 
-var baseUrl = "/out/" + runid + "/";
+var baseUrl = '/out/' + runid + '/';
 
 
 function loadData() {
     // load text files
-    loadUrl("out", "out.txt");
-    loadUrl("err", "err.txt");
-    loadUrl("mcstas", "mcstas/mcstas.sim");
+    loadUrl('out', 'out.txt');
+    loadUrl('err', 'err.txt');
+    loadUrl('mcstas', 'mcstas/mcstas.sim');
 
     // load list of components
-    path = baseUrl + "comps.json";
+    path = baseUrl + 'comps.json';
     waitContent(
         path,
         function() {
@@ -17,50 +17,53 @@ function loadData() {
             $.getJSON(
                 path,
                 createCompImgs
-            )}
-    );
+            );
+        });
 }
 
 function createCompImgs(comps) {
     $.each(comps.sort(), function(i, comp) {
         // create img tag
-        $.each(['lin', 'log'],
-               function(_, mode) {
-                   var plotid = 'plot'+mode+i;
-                   // create a tag
-                   var taga = $('<a>').attr('id', plotid + 'a')
-                   // create img tag
-                   taga.append($('<img>').attr('id', plotid + 'i').
-                               css('width', '280px'));
-                   // add to list of plots
-                   $('#'+mode+'Plots').append(taga);
+        $.each(
+            ['lin', 'log'],
+            function(_, mode) {
+                var plotid = 'plot'+mode+i;
+                // create a tag
+                var taga = $('<a>').attr('id', plotid + 'a');
+                // create img tag
+                taga.append($('<img>').attr('id', plotid + 'i').
+                            css('width', '280px'));
+                // add to list of plots
+                $('#'+mode+'Plots').append(taga);
 
-                   // wait till content is ready
-                   var linUrl = baseUrl+'plot'+'-'+comp+'-'+mode+'.gif';
-                   waitContent(linUrl,
-                               function () {
-                                   $('#'+plotid+'a').attr('href', linUrl);
-                                   $('#'+plotid+'i').attr('src', linUrl);
-                               });
-               });
-    });
+                // wait till content is ready
+                var linUrl = baseUrl+'plot'+'-'+comp+'-'+mode+'.gif';
+                waitContent(linUrl,
+                            function () {
+                                $('#'+plotid+'a').attr('href', linUrl);
+                                $('#'+plotid+'i').attr('src', linUrl);
+                            });
+            });
+           });
 }
 
 
 function loadUrl(id, file) {
     var myUrl = baseUrl + file;
-    waitContent(myUrl,
-                function() {
-                    var tag = $("#"+id);
-                    tag.html("<a href='" + myUrl + "'>" + tag.html() + "</a>");
-                });
+    waitContent(
+        myUrl,
+        function() {
+            var tag = $('#'+id);
+            tag.html('<a href="' + myUrl + '">' + tag.html() + '</a>');
+        }
+    );
 }
 
 
 function loadImg(id, file) {
     waitContent(baseUrl + file,
                 function () {
-                    $("#"+id).attr("src", baseUrl + file);
+                    $('#'+id).attr('src', baseUrl + file);
                 });
 }
 
@@ -68,8 +71,8 @@ function loadImg(id, file) {
 function waitContent(url, cb) {
     // wait until content (url) is ready, then call cb
     $.ajax({
-        url: url + "?" + Date.now(), // by-pass browser caching
-        type: "HEAD",
+        url: url + '?' + Date.now(), // by-pass browser caching
+        type: 'HEAD',
         success: cb,
         error: function() {
             setTimeout(function() {
