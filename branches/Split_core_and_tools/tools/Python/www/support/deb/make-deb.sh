e0742d2d1f72302ccf3d9447cc982f9daf69a3ea
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-07-02 09:57:37 +0000

    Clean up

--- branches/Split_core_and_tools/tools/Python/www/support/deb/make-deb.sh
+++ branches/Split_core_and_tools/tools/Python/www/support/deb/make-deb.sh
@@ -4,25 +4,29 @@ NAME=mcstas-web.deb
 
 echo '2.0' > debian-binary
 
-# clean up
-rm -f ${NAME}
-DIRS="control data"
-for d in ${DIRS}; do
-    rm -f ${d}/${d}.tar.gz;
-done
+echo '. Clean old archives..'
+rm -vf ${NAME}
+rm -vf {control,data}/*.tar.gz
+
+# copy files
+# TODO
 
 # initialise new debian package
-rm -f ${NAME};
+echo ''
+echo ". Goal: ${NAME}"
 ar -r ${NAME} debian-binary;
 
 # generate md5 listing
+echo '. Generate: control/md5sums..'
 cd data;
 md5sum `find . -type f` > ../control/md5sums;
 cd ..;
 
 # pack 'control' and 'data'
+echo '. Pack files:'
 DIRS="control data"
 for d in ${DIRS}; do
+    echo "> ${d}"
     cd ${d};
     tar czf ${d}.tar.gz *;
     cd ..;
