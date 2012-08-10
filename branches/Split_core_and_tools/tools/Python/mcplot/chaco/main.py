0cce6ee2cbdafe6d6e89d9d55c7017011109ddb3
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-08-10 13:02:21 +0000

    Use program argument for mcstas.sim path

--- branches/Split_core_and_tools/tools/Python/mcplot/chaco/main.py
+++ branches/Split_core_and_tools/tools/Python/mcplot/chaco/main.py
@@ -1,9 +1,15 @@
-from mcdata import mcstas_to_plotdescs
-from plotting import PlotDesc, McLayout
+import sys
 
 
 def main():
-    plotdatas = mcstas_to_plotdescs('mcstas')
+    if len(sys.argv) < 2:
+        print 'usage: %s mcstas.sim' % sys.argv[0]
+        sys.exit(1)
+
+    from mcdata import mcstas_to_plotdescs
+    from plotting import PlotDesc, McLayout
+
+    plotdatas = mcstas_to_plotdescs(sys.argv[1])
     McLayout(list(plotdatas)).configure_traits(scrollable=True)
 
 
