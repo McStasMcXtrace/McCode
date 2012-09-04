279db4ff3273d9161188556fa0479a02ed17ba67
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-09-04 12:31:06 +0000

    Remove trailing whitespace

--- trunk/nlib/share/pol-lib.c
+++ trunk/nlib/share/pol-lib.c
@@ -14,10 +14,10 @@
 * Release: McStas 1.12
 * Version: $Revision: 1.3 $
 *
-* This file is to be imported by polarisation components. 
-* It handles some shared functions. 
+* This file is to be imported by polarisation components.
+* It handles some shared functions.
 * Embedded within instrument in runtime mode.
-* Variable names have prefix 'mc_pol_' for 'McStas Polarisation' 
+* Variable names have prefix 'mc_pol_' for 'McStas Polarisation'
 * to avoid conflicts
 *
 * Usage: within SHARE
@@ -80,7 +80,7 @@ int mcmagnet_get_field(double x, double y, double z, double t, double *bx,double
 
   /*PROP_MAGNET takes care of transforming local "PROP" coordinates to lab system*/
   in.x=x;in.y=y;in.z=z;
-  
+
   int i=0,stat=1;
   p=stack[i];
   *bx=0;*by=0;*bz=0;
@@ -103,7 +103,7 @@ int mcmagnet_get_field(double x, double y, double z, double t, double *bx,double
     if (p->stop) break;
     p=stack[++i];
   }
-  /*we now have the magnetic field in lab coords in loc., transfer it back to caller*/ 
+  /*we now have the magnetic field in lab coords in loc., transfer it back to caller*/
   *bx=loc.x;
   *by=loc.y;
   *bz=loc.z;
@@ -232,14 +232,14 @@ int rot_magnetic_field(double x, double y, double z, double t,
   *by =  Bmagnitude * cos(PI/2*z/magnetLength);
   *bz =  0;
   //printf("mag field at (x,y,z)=( %g %g %g ) t=%g is B=( %g %g %g )\n",x,y,z,t,*bx,*by,*bz);
-  return 1;  
+  return 1;
 }
-  
+
 int majorana_magnetic_field(double x, double y, double z, double t,
     double *bx, double *by, double *bz, void *data) {
-  /* Large linearly decreasing (from +Bx to -Bx in magnetLength) component along x axis, 
+  /* Large linearly decreasing (from +Bx to -Bx in magnetLength) component along x axis,
    * small constant component along y axis
-   */ 
+   */
   double Blarge       = ((double *)data)[0];
   double Bsmall       = ((double *)data)[1];
   double magnetLength = ((double *)data)[2];
@@ -252,7 +252,7 @@ int majorana_magnetic_field(double x, double y, double z, double t,
 
 /****************************************************************************
 * void GetMonoPolFNFM(double Rup, double Rdown, double *FN, double *FM)
-* 
+*
 * ACTION: Calculate FN and FM from reflectivities Rup and Rdown
 *
 * For a monochromator (nuclear and magnetic scattering), the
@@ -262,18 +262,18 @@ int majorana_magnetic_field(double x, double y, double z, double t,
 * FM = sign(Rup)*sqrt(|Rup|) + sign(Rdown)*sqrt(|Rdown|)
 * FN = sign(Rup)*sqrt(|Rup|) - sign(Rdown)*sqrt(|Rdown|)
 *****************************************************************************/
-void GetMonoPolFNFM(double mc_pol_Rup, double mc_pol_Rdown, 
+void GetMonoPolFNFM(double mc_pol_Rup, double mc_pol_Rdown,
 		    double *mc_pol_FN, double *mc_pol_FM){
   if (mc_pol_Rup>0)
     mc_pol_Rup   = sqrt(fabs(mc_pol_Rup));
   else
     mc_pol_Rup   = -sqrt(fabs(mc_pol_Rup));
-  
+
   if (mc_pol_Rdown>0)
     mc_pol_Rdown = sqrt(fabs(mc_pol_Rdown));
   else
     mc_pol_Rdown = -sqrt(fabs(mc_pol_Rdown));
-  
+
   *mc_pol_FN = 0.5*(mc_pol_Rup + mc_pol_Rdown);
   *mc_pol_FM = 0.5*(mc_pol_Rup - mc_pol_Rdown);
   return;
@@ -281,16 +281,16 @@ void GetMonoPolFNFM(double mc_pol_Rup, double mc_pol_Rdown,
 
 /****************************************************************************
 * void GetMonoPolRefProb(double FN, double FM, double sy, double *prob)
-* 
+*
 * ACTION: Calculate reflection probability from sy, FN and FM
 *
 * For a monochromator with up direction along y the reflection
 * probability is given as:
-* prob = FN*FN + 2*FN*FM*sy_in + FM*FM 
+* prob = FN*FN + 2*FN*FM*sy_in + FM*FM
 *     (= |Rup| + |Rdown| (for sy_in=0))
 * where FN and FM are calculated from Rup and Rdown by GetMonoPolFNFM
 *****************************************************************************/
-void GetMonoPolRefProb(double mc_pol_FN, double mc_pol_FM, 
+void GetMonoPolRefProb(double mc_pol_FN, double mc_pol_FM,
 		       double mc_pol_sy, double *mc_pol_prob) {
   *mc_pol_prob = mc_pol_FN*mc_pol_FN + mc_pol_FM*mc_pol_FM
     + 2*mc_pol_FN*mc_pol_FM*mc_pol_sy;
@@ -300,7 +300,7 @@ void GetMonoPolRefProb(double mc_pol_FN, double mc_pol_FM,
 /****************************************************************************
 * void SetMonoPolRefOut(double FN, double FM, double refProb,
 *		     double* sx, double* sy, double* sz) {
-* 
+*
 * ACTION: Set the outgoing polarisation vector of the reflected neutrons
 * given FN, FM and the reflection probability.
 *
@@ -312,12 +312,12 @@ void GetMonoPolRefProb(double mc_pol_FN, double mc_pol_FM,
 * where sx_in, sy_in, and sz_in is the incoming polarisation, and
 * FN and FM are calculated from Rup and Rdown by GetMonoPolFNFM
 *****************************************************************************/
-void SetMonoPolRefOut(double mc_pol_FN, double mc_pol_FM, 
-		      double mc_pol_refProb, double* mc_pol_sx, 
+void SetMonoPolRefOut(double mc_pol_FN, double mc_pol_FM,
+		      double mc_pol_refProb, double* mc_pol_sx,
 		      double* mc_pol_sy, double* mc_pol_sz) {
   *mc_pol_sx = (mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sx)
     /mc_pol_refProb;
-  *mc_pol_sy = ((mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sy) 
+  *mc_pol_sy = ((mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sy)
 		+ 2*mc_pol_FN*mc_pol_FM + 2*mc_pol_FM*mc_pol_FM*(*mc_pol_sy))
     /mc_pol_refProb;
   *mc_pol_sz = (mc_pol_FN*mc_pol_FN - mc_pol_FM*mc_pol_FM)*(*mc_pol_sz)
@@ -326,9 +326,9 @@ void SetMonoPolRefOut(double mc_pol_FN, double mc_pol_FM,
 }
 
 /****************************************************************************
-* void SetMonoPolTransOut(double FN, double FM, double refProb, 
+* void SetMonoPolTransOut(double FN, double FM, double refProb,
 *			  double* sx, double* sy, double* sz) {
-* 
+*
 * ACTION: Set the outgoing polarisation vector of the transmitted neutrons
 * given FN, FM and the REFLECTION probability.
 *
@@ -338,14 +338,14 @@ void SetMonoPolRefOut(double mc_pol_FN, double mc_pol_FM,
 * where refProb is calculated using the routine GetMonoPolRefProb
 * and s_ref is calculated by SetMonoPolRefOut
 *****************************************************************************/
-void SetMonoPolTransOut(double mc_pol_FN, double mc_pol_FM, 
-			double mc_pol_refProb, double* mc_pol_sx, 
+void SetMonoPolTransOut(double mc_pol_FN, double mc_pol_FM,
+			double mc_pol_refProb, double* mc_pol_sx,
 			double* mc_pol_sy, double* mc_pol_sz) {
   double mc_pol_sx_ref = *mc_pol_sx, mc_pol_sy_ref = *mc_pol_sy;
   double mc_pol_sz_ref = *mc_pol_sz;
-  
+
   // By passing 1 as probability we get mc_pol_refProb*s_out_ref
-  SetMonoPolRefOut(mc_pol_FN, mc_pol_FM, 1, 
+  SetMonoPolRefOut(mc_pol_FN, mc_pol_FM, 1,
 		   &mc_pol_sx_ref, &mc_pol_sy_ref, &mc_pol_sz_ref);
   *mc_pol_sx = (*mc_pol_sx - mc_pol_sx_ref)/(1 - mc_pol_refProb);
   *mc_pol_sy = (*mc_pol_sy - mc_pol_sy_ref)/(1 - mc_pol_refProb);
@@ -357,16 +357,16 @@ void SetMonoPolTransOut(double mc_pol_FN, double mc_pol_FM,
 * void SimpleNumMagnetPrecession(double x, double y, double z, double t,
 *			         double vx, double vy, double vz,
 *			         double* sx, double* sy, double* sz, double dt)
-* 
+*
 *****************************************************************************/
-void SimpleNumMagnetPrecession(double mc_pol_x, double mc_pol_y, 
+void SimpleNumMagnetPrecession(double mc_pol_x, double mc_pol_y,
 			       double mc_pol_z, double mc_pol_time,
-			       double mc_pol_vx, double mc_pol_vy, 
+			       double mc_pol_vx, double mc_pol_vy,
 			       double mc_pol_vz,
-			       double* mc_pol_sx, double* mc_pol_sy, 
+			       double* mc_pol_sx, double* mc_pol_sy,
 			       double* mc_pol_sz, double mc_pol_deltaT,
 			       Coords mc_pol_posLM, Rotation mc_pol_rotLM) {
-  
+
   double mc_pol_Bx, mc_pol_By, mc_pol_Bz, mc_pol_phiz;
   double mc_pol_BxStart, mc_pol_ByStart, mc_pol_BzStart, mc_pol_Bstart;
   double mc_pol_BxTemp, mc_pol_ByTemp, mc_pol_BzTemp, mc_pol_Btemp;
@@ -381,58 +381,58 @@ void SimpleNumMagnetPrecession(double mc_pol_x, double mc_pol_y,
   //printf("pos_at_caller(xyz)( %g %g %g )\n", mc_pol_x,mc_pol_y,mc_pol_z);
   // change coordinates from current local system to lab system
   mccoordschange(mc_pol_posLM, mc_pol_rotLM,
-		 &mc_pol_x, &mc_pol_y, &mc_pol_z, 
+		 &mc_pol_x, &mc_pol_y, &mc_pol_z,
 		 &mc_pol_vx, &mc_pol_vy, &mc_pol_vz, mc_pol_sx, mc_pol_sy, mc_pol_sz);
   //printf("pos_at_labaftertranformation(xyz)( %g %g %g )\n", mc_pol_x,mc_pol_y,mc_pol_z);
-  
+
   // get initial B-field value
-  mcMagneticField(mc_pol_x, mc_pol_y, mc_pol_z, mc_pol_time, 
+  mcMagneticField(mc_pol_x, mc_pol_y, mc_pol_z, mc_pol_time,
 		  &mc_pol_BxTemp, &mc_pol_ByTemp, &mc_pol_BzTemp,NULL);
-  
+
   do {
-    
-    mc_pol_Bx = 0; mc_pol_By = 0; mc_pol_Bz = 0; mc_pol_phiz = 0; 
-    mc_pol_BxStart = mc_pol_BxTemp; mc_pol_ByStart = mc_pol_ByTemp; 
+
+    mc_pol_Bx = 0; mc_pol_By = 0; mc_pol_Bz = 0; mc_pol_phiz = 0;
+    mc_pol_BxStart = mc_pol_BxTemp; mc_pol_ByStart = mc_pol_ByTemp;
     mc_pol_BzStart = mc_pol_BzTemp;
-    mc_pol_Bstart = 
-      sqrt(mc_pol_BxStart*mc_pol_BxStart + mc_pol_ByStart*mc_pol_ByStart 
+    mc_pol_Bstart =
+      sqrt(mc_pol_BxStart*mc_pol_BxStart + mc_pol_ByStart*mc_pol_ByStart
 	   + mc_pol_BzStart*mc_pol_BzStart);
     mc_pol_timeStep = mc_pol_startTimeStep;
-    
+
     if(mc_pol_deltaT<mc_pol_timeStep)
       mc_pol_timeStep = mc_pol_deltaT;
-    
+
     do {
-      
-      mcMagneticField(mc_pol_x+mc_pol_vx*mc_pol_timeStep, 
-		      mc_pol_y+mc_pol_vy*mc_pol_timeStep, 
-		      mc_pol_z+mc_pol_vz*mc_pol_timeStep, 
+
+      mcMagneticField(mc_pol_x+mc_pol_vx*mc_pol_timeStep,
+		      mc_pol_y+mc_pol_vy*mc_pol_timeStep,
+		      mc_pol_z+mc_pol_vz*mc_pol_timeStep,
 		      mc_pol_time+mc_pol_timeStep,
 		      &mc_pol_BxTemp, &mc_pol_ByTemp, &mc_pol_BzTemp,NULL);
       // not so elegant, but this is how we make sure that the steps decrease
       // when the WHILE condition is not met
       mc_pol_timeStep *= 0.5;
-      
-      mc_pol_Btemp = 
-	sqrt(mc_pol_BxTemp*mc_pol_BxTemp + mc_pol_ByTemp*mc_pol_ByTemp 
+
+      mc_pol_Btemp =
+	sqrt(mc_pol_BxTemp*mc_pol_BxTemp + mc_pol_ByTemp*mc_pol_ByTemp
 	     + mc_pol_BzTemp*mc_pol_BzTemp);
-      
+
       mc_pol_sp =
-	scalar_prod(mc_pol_BxStart, mc_pol_ByStart, mc_pol_BzStart, 
+	scalar_prod(mc_pol_BxStart, mc_pol_ByStart, mc_pol_BzStart,
 		    mc_pol_BxTemp, mc_pol_ByTemp, mc_pol_BzTemp);
       mc_pol_sp /= mc_pol_Bstart*mc_pol_Btemp;
-      
+
     } while (mc_pol_sp<mc_pol_spThreshold);
-    
+
     mc_pol_timeStep*=2;
-    
+
     // update coordinate values
     mc_pol_x += mc_pol_vx*mc_pol_timeStep;
-    mc_pol_y += mc_pol_vy*mc_pol_timeStep; 
-    mc_pol_z += mc_pol_vz*mc_pol_timeStep; 
+    mc_pol_y += mc_pol_vy*mc_pol_timeStep;
+    mc_pol_z += mc_pol_vz*mc_pol_timeStep;
     mc_pol_time += mc_pol_timeStep;
     mc_pol_deltaT -= mc_pol_timeStep;
-    
+
     mc_pol_Bx = 0.5 * (mc_pol_BxStart + mc_pol_BxTemp);
     mc_pol_By = 0.5 * (mc_pol_ByStart + mc_pol_ByTemp);
     mc_pol_Bz = 0.5 * (mc_pol_BzStart + mc_pol_BzTemp);
@@ -442,44 +442,42 @@ void SimpleNumMagnetPrecession(double mc_pol_x, double mc_pol_y,
 		       *mc_pol_timeStep*mc_pol_omegaL, 2*PI);
 
     // Do the neutron spin precession
-    
+
     if(!(mc_pol_Bx==0 && mc_pol_By==0 && mc_pol_Bz==0)) {
-      
+
       double mc_pol_sx_in = *mc_pol_sx;
       double mc_pol_sy_in = *mc_pol_sy;
       double mc_pol_sz_in = *mc_pol_sz;
-      
-      rotate(*mc_pol_sx, *mc_pol_sy, *mc_pol_sz, 
-	     mc_pol_sx_in, mc_pol_sy_in, mc_pol_sz_in, 
+
+      rotate(*mc_pol_sx, *mc_pol_sy, *mc_pol_sz,
+	     mc_pol_sx_in, mc_pol_sy_in, mc_pol_sz_in,
 	     mc_pol_phiz, mc_pol_Bx, mc_pol_By, mc_pol_Bz);
     }
-    
+
   } while (mc_pol_deltaT>0);
-  
+
   // change back spin coordinates from lab system to local system
-  rot_transpose(mc_pol_rotLM, mc_pol_rotBack); 
+  rot_transpose(mc_pol_rotLM, mc_pol_rotBack);
   mccoordschange_polarisation(mc_pol_rotBack, mc_pol_sx, mc_pol_sy, mc_pol_sz);
-  
+
 }
 
 /****************************************************************************
 * double GetConstantField(double length, double lambda, double angle)
-* 
+*
 * Return the magnetic field in Tesla required to flip a neutron with
-* wavelength lambda(1/velocity), angle degrees, over the specified 
+* wavelength lambda(1/velocity), angle degrees, over the specified
 * length(=time*velocity).
-* 
+*
 *****************************************************************************/
-double GetConstantField(double mc_pol_length, double mc_pol_lambda, 
+double GetConstantField(double mc_pol_length, double mc_pol_lambda,
 			double mc_pol_angle)
 {
   const double mc_pol_velocity = K2V*2*PI/mc_pol_lambda;
   const double mc_pol_time = mc_pol_length/mc_pol_velocity;
-  
+
   // B*omegaL*time = angle
   return mc_pol_angle*DEG2RAD/mc_pol_omegaL/mc_pol_time; // T
 }
 
 /* end of regular pol-lib.c */
-
-
