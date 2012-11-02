768c5a6fd2d1c6db85ad52473521ade780c8141f
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-11-02 10:24:57 +0000

    Introduce helper-macros is_mkdist and setup_mccode_mkdist

--- trunk/cmake/Modules/MCUtil.cmake
+++ trunk/cmake/Modules/MCUtil.cmake
@@ -1,5 +1,4 @@
-# Macro for installing library files into lib/${FLAVOR},
-# while skipping unneeded files
+# Install library files into lib/${FLAVOR}, while skipping unneeded files
 macro(install_lib path)
   install (
     DIRECTORY "${path}"
@@ -11,3 +10,33 @@ macro(install_lib path)
   )
 endmacro()
 
+
+# Check whether we are being run through mkdist
+macro(is_mkdist outvar)
+  string(REPLACE "@" "_" TMP "@MCCODE_NAME@")
+  string(COMPARE NOTEQUAL "${TMP}" "_MCCODE_NAME_" ${outvar})
+endmacro()
+
+
+# Setup McCode constants using either mkdist or SVN-defaults
+macro(setup_mccode_mkdist)
+
+  # Check for mkdist values
+  is_mkdist(MKDIST)
+
+  if(MKDIST)
+    ## Set mkdist-provided version
+    set(MCCODE_VERSION "@MCCODE_VERSION@")
+    set(MCCODE_NAME "${MKDIST} ${MKDIST_S} @__MCCODE_NAME@")
+    set(MCCODE_DATE "@MCCODE_DATE@")
+    set(MCCODE_STRING "@MCCODE_STRING@")
+    set(MCCODE_TARNAME "@MCCODE_TARNAME@")
+  else()
+    ## Set SVN-specific version
+    set(MCCODE_VERSION "2.9999-svn")
+    set(MCCODE_NAME "${NAME}")
+    set(MCCODE_DATE "svn")
+    set(MCCODE_STRING "${NAME} ${MCCODE_VERSION}, ${MCCODE_DATE}")
+    set(MCCODE_TARNAME "${FLAVOR}")
+  endif()
+endmacro()
