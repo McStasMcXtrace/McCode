63a559a7ea8a7f899463e10f7cdcdf21700176a1
pkwi <pkwi@fysik.dtu.dk>
2012-11-07 07:44:57 +0000

    Locating Tk installation to default Mac OS /Library/Perl location.
    NOTA BENE: Unfortunate we need to build for each version independently... :-(

--- support/MacOSX/Perl-Tk/Tk-804.030/Build.sh
+++ support/MacOSX/Perl-Tk/Tk-804.030/Build.sh
@@ -1,16 +1,19 @@
-# Clear out build dir
+#!/bin/sh
+# Clear out build and src dirs
 rm -rf ../build/*
+make clean
 
 # What version of Tk are we building?
 TKVER=`basename $PWD`
 
-# On which OS X?
+# On which OS X and which PERL
 OSXVER=`sw_vers -productVersion|cut -f -2 -d.`
+PERLVER=`perl -v |grep v5|cut -f2 -d\(|cut -f1 -d\)|cut -b2-|cut -f1-2 -d.`
 
 # Assemble build-dir path
 BUILDDIR=${TKVER}_MacOSX_${OSXVER}
 # Configure with system default perl
-perl Makefile.PL PREFIX=../build/$BUILDDIR/usr/local
+perl Makefile.PL PREFIX=../build/$BUILDDIR/Library/Perl/$PERLVER LIB=../build/$BUILDDIR/Library/Perl/$PERLVER
 
 # Build
 make
