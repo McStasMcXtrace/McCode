dd5173ba91b5aa601abc96e7ec2c650464510cc2
pkwi <pkwi@fysik.dtu.dk>
2012-11-07 10:28:42 +0000

    Document for quick guidelines in generating packages.
    Includes instructions for building an OS X .mpkg meta-package

--- support/MacOSX/Perl-Tk/Tk-804.030/Build.sh
+++ support/MacOSX/Perl-Tk/Tk-804.030/Build.sh
@@ -4,14 +4,15 @@ rm -rf ../build/*
 make clean
 
 # What version of Tk are we building?
-TKVER=`basename $PWD`
+TKVER=`basename $PWD|sed -e 's/\./\_/g'`
 
 # On which OS X and which PERL
-OSXVER=`sw_vers -productVersion|cut -f -2 -d.`
+OSXVER=`sw_vers -productVersion|cut -f -2 -d.|sed -e 's/\./\_/g'`
 PERLVER=`perl -v |grep v5|cut -f2 -d\(|cut -f1 -d\)|cut -b2-|cut -f1-2 -d.`
+PERLVER_=`echo $PERLVER|sed -e 's/\./\_/g'`
 
 # Assemble build-dir path
-BUILDDIR=${TKVER}_MacOSX_${OSXVER}
+BUILDDIR=${TKVER}_MacOSX_${OSXVER}_Perl_${PERLVER_}
 # Configure with system default perl
 perl Makefile.PL PREFIX=../build/$BUILDDIR/Library/Perl/$PERLVER LIB=../build/$BUILDDIR/Library/Perl/$PERLVER
 
