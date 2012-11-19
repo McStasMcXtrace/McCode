8864905dbda308db63a433b7c6de13a8b64a89d1
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-11-19 15:14:34 +0000

    Factor out common MinGW configuration to MinGW module

---
+++ trunk/cmake/Modules/MinGW.cmake
@@ -0,0 +1,14 @@
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+
+
+# Set executable extension
+set(EXE_SUFFIX   "exe")
+
+# OUT_SUFFIX defaults to EXE_SUFFIX
--- trunk/cmake/toolchains/mingw64.cmake
+++ trunk/cmake/toolchains/mingw64.cmake
@@ -11,15 +11,6 @@ SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
 
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-
-# Set executable extension
-set(EXE_SUFFIX   "exe")
-
-# OUT_SUFFIX defaults to EXE_SUFFIX
+# include general MinGW module
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
+include(MinGW)
