6baa55ce86e196957431cafa70d72ad0b28e8fbe
pkwi <pkwi@fysik.dtu.dk>
2012-11-29 17:33:27 +0000

    Typo fix

--- cmake/support/scripts/postsetup.bat
+++ cmake/support/scripts/postsetup.bat
@@ -3,8 +3,8 @@
 @ppm install Astro-FITS-Header
 @ppm install Convert-UU
 @ppm install Tk
-@ppm install http://www.sisyphusion.tk/ppm/Tk-CodeText.ppd
-@ppm install http://ppds.mccode.org/ppm/Math-Amoeba.ppd
-@ppm install http://ppds.mccode.org/ppm/OpenGL.ppd
-@ppm install http://ppds.mccode.org/ppm/PDL.ppd
-@ppm install http://ppds.mccode.org/ppm/PGPLOT.ppd
+@ppm install http://ppds.mccode.org/Tk-CodeText.ppd
+@ppm install http://ppds.mccode.org/Math-Amoeba.ppd
+@ppm install http://ppds.mccode.org/OpenGL.ppd
+@ppm install http://ppds.mccode.org/PDL.ppd
+@ppm install http://ppds.mccode.org/PGPLOT.ppd
