c3ec0189b34ee8f9a364364705dbb456cfb0b327
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-11-30 17:49:49 +0000

    Use environment variables for McStas configuration if available

--- trunk/tools/Python/mcrun/config.py
+++ trunk/tools/Python/mcrun/config.py
@@ -1,8 +1,17 @@
 
-VERSION    = "mcrun @MCCODE_VERSION@"
+import os
+
+VERSION = "mcrun @MCCODE_VERSION@"
+
 
 MCCODE_BIN = "@MCCODE_BIN@"
-MCCODE_LIB = "@MCCODE_LIB@"
 
-CC         = "@CC@"
-CFLAGS     = "@CFLAGS@"
+
+MCCODE_LIB = os.environ.get('MCSTAS', None)
+MCCODE_LIB = MCCODE_LIB or "@MCCODE_LIB@"
+
+CC = os.environ.get('MCSTAS_CC', None)
+CC = CC or "@CC@"
+
+CFLAGS = os.environ.get('MCSTAS_CFLAGS', None)
+CFLAGS = CFLAGS or "@CFLAGS@"
