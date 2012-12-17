acfe950d5370276a6e3b0dad0bcb1a965d29773e
pkwi <pkwi@fysik.dtu.dk>
2012-12-17 14:58:56 +0000

    Let's better get all ppd's from our own repository.
    Important: The Tk.ppd is for Perl 5.16 ONLY

--- cmake/support/scripts/postsetup.bat
+++ cmake/support/scripts/postsetup.bat
@@ -1,8 +1,8 @@
 @REM Script for final installation of McStas-related Perl modules
 @REM
-@call ppm install Astro-FITS-Header
-@call ppm install Convert-UU
-@call ppm install Tk
+@call ppm install http://ppds.mccode.org/Astro-FITS-Header.ppd
+@call ppm install http://ppds.mccode.org/Convert-UU.ppd
+@call ppm install http://ppds.mccode.org/Tk.ppd
 @call ppm install http://ppds.mccode.org/Tk-CodeText.ppd
 @call ppm install http://ppds.mccode.org/Math-Amoeba.ppd
 @call ppm install http://ppds.mccode.org/OpenGL.ppd
