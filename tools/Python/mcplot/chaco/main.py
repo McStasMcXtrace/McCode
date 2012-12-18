e18141a96b2780bb089a84a6dbedc7c9b82b2ab5
jos <nobody@mccode.org>
2012-12-18 15:53:35 +0000

    Respect '--help' flag

--- tools/Python/mcplot/chaco/main.py
+++ tools/Python/mcplot/chaco/main.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python2.7
-import sys
+import sys, os
+
+
+def usage():
+    print 'usage: %s mcstas.sim' % sys.argv[0]
 
 
 def main():
+    # Check for --help flag
+    if '--help' in sys.argv:
+        usage()
+        sys.exit(0)
+
+    # Sanity check
     if len(sys.argv) < 2:
-        print 'usage: %s mcstas.sim' % sys.argv[0]
+        usage()
         sys.exit(1)
 
     from mcdata import mcstas_to_plotdescs
