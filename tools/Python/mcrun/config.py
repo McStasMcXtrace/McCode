250c37b97b3d14eece83c03291fe297577bca646
jos <nobody@mccode.org>
2012-12-18 16:54:51 +0000

    Minor: Clean up

--- tools/Python/mcrun/config.py
+++ tools/Python/mcrun/config.py
@@ -1,18 +1,13 @@
 
 import os
 
+
 VERSION = "mcrun @MCCODE_VERSION@"
 
 MCCODE_BIN = "@MCCODE_BIN@"
-
 OUT_SUFFIX = "@OUT_SUFFIX@"
 
 
-MCCODE_LIB = os.environ.get('MCSTAS', None)
-MCCODE_LIB = MCCODE_LIB or "@MCCODE_LIB@"
-
-CC = os.environ.get('MCSTAS_CC', None)
-CC = CC or "@CC@"
-
-CFLAGS = os.environ.get('MCSTAS_CFLAGS', None)
-CFLAGS = CFLAGS or "@CFLAGS@"
+MCCODE_LIB = os.environ.get('MCSTAS',        '@MCCODE_LIB@')
+CC         = os.environ.get('MCSTAS_CC',     '@CC@')
+CFLAGS     = os.environ.get('MCSTAS_CFLAGS', '@CFLAGS@')
