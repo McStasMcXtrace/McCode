bd4a56202a3824a57b1a806c3b341d70ea692083
jos <jos@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2012-12-20 13:42:07 +0000

    Use '%*' for argument expansion in batch scripts (fixes mcrun+MPI on Windows)

--- trunk/cmake/support/scripts/mpicc.bat
+++ trunk/cmake/support/scripts/mpicc.bat
@@ -1,3 +1,3 @@
 @REM mpicc.bat batch file for running gcc and MPICH2 under windows
 
-gcc %1 %2 %3 %4 %5 %6 %7 %8 %9 %10 -g -lm -O2 -lmpi -I"c:\Program Files\MPICH2\include" -L"c:\Program Files\MPICH2\lib"
+gcc %* -lmpi -I"c:\Program Files\MPICH2\include" -L"c:\Program Files\MPICH2\lib"
