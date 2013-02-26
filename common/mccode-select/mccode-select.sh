26407940c7c47fc54ce4ee628aa65b288547e1d0
jos <nobody@mccode.org>
2013-02-26 17:21:27 +0000

    Improve MCINSTALL_PREFIX support and introduce MCNO_ALTERNATIVES (mkdist environment definitions)

--- common/mccode-select/mccode-select.sh
+++ common/mccode-select/mccode-select.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+function fullpath_dir() {
+    (
+        cd $(dirname $0);
+        echo $(pwd)
+    )
+}
 
-PREFIX=/usr/local
+# Locate bin-folder and prefix from current script-location
+BIN="$(fullpath_dir)"
+PREFIX=$(dirname "${BIN}")
 
-
-if [ "x$1" = "x" ] || [ "x$1" = "x--help" ] ; then
+function usage() {
     echo "usage: $0 [OPTION]... package [VERSION]";
     cat <<EOF
 Select a specific version of McStas/McXtrace as default.
@@ -17,8 +24,29 @@ Select a specific version of McStas/McXtrace as default.
 
 Report bugs to jsbn@fysik.dtu.dk
 EOF
+}
+
+if [ "x$1" = "x" ] || [ "x$1" = "x--help" ] ; then
+    usage;
     exit 0;
 fi
+
+
+
+# Check to see whether we have update-alternatives (Debian)
+ALTERNATIVES=$(command -v update-alternatives)
+if [ $? -eq 0 ] && [ -x ${ALTERNATIVES} ]; then
+    HAS_ALTERNATIVES=true;
+else
+    HAS_ALTERNATIVES=false;
+fi
+
+
+if [ "x$1" = "x--no-update-alternatives" ]; then
+    HAS_ALTERNATIVES=false;
+    shift;
+fi
+
 
 
 if [ "x$1" = "x--bootstrap" ]; then
@@ -29,12 +57,21 @@ if [ "x$1" = "x--bootstrap" ]; then
         exit 1;
     fi
 
-    # Install mccode-select version
-    $0 --install mccode-select        ${VERSION};
-    $0 --install mccode-select-bundle ${VERSION};
-    # Choose mccode-select version
-    $0 mccode-select        ${VERSION};
-    $0 mccode-select-bundle ${VERSION};
+    if ${HAS_ALTERNATIVES}; then
+        # Install mccode-select version
+        $0 --install mccode-select        ${VERSION};
+        $0 --install mccode-select-bundle ${VERSION};
+        # Choose mccode-select version
+        $0 mccode-select        ${VERSION};
+        $0 mccode-select-bundle ${VERSION};
+    else
+        # Install mccode-select version
+        $0 --no-update-alternatives --install mccode-select        ${VERSION};
+        $0 --no-update-alternatives --install mccode-select-bundle ${VERSION};
+        # Choose mccode-select version
+        $0 --no-update-alternatives mccode-select        ${VERSION};
+        $0 --no-update-alternatives mccode-select-bundle ${VERSION};
+    fi
     exit 0;
 fi
 
@@ -63,9 +100,16 @@ while true; do
     shift;
 done
 
-if ${LIST} && ${INSTALL}; then
+# Sanity check
+if ${LIST} && [ "x${INSTALL}" = "x--install" ] ; then
     echo "Error: list and install cannot be combined. Pick one.";
     exit 1;
+fi
+
+# Check for help or missing package name after parsing arguments
+if [ "x$1" = "x" ] || [ "x$1" = "x--help" ] ; then
+    usage;
+    exit 0;
 fi
 
 # Set name and version
@@ -74,15 +118,6 @@ VERSION="$2"
 
 # Define dirname where existing non-overwritable binaries are moved
 BACKUPDIR=$PREFIX/bin/MCCODE_BACKUP_`date +%Y%m%d_%H.%M`
-
-# Check to see whether we have update-alternatives (Debian)
-ALTERNATIVES=$(command -v update-alternatives)
-if [ $? -eq 0 ] && [ -x ${ALTERNATIVES} ]; then
-    HAS_ALTERNATIVES=true;
-else
-    HAS_ALTERNATIVES=false;
-fi
-
 
 linksTo() {
     (
