06bec3637174b05c7c9225faa7d85c50c99f95e4
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2013-06-20 07:27:27 +0000

    A small patch to the mcdisplay/R backend

--- trunk/tools/Python/mcdisplay/R/run.sh
+++ trunk/tools/Python/mcdisplay/R/run.sh
@@ -3,6 +3,6 @@
 CSVCOMPS="comps.csv"
 CSVLINES="lines.csv"
 
-cat $1 | ./rewrite.py ${CSVCOMPS} ${CSVLINES}
+mcrun --trace -n1e2 $1 | ./rewrite.py ${CSVCOMPS} ${CSVLINES}
 
 R_PROFILE_USER=display.r R ${CSVCOMPS} ${CSVLINES} --no-save
