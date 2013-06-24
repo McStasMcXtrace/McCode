9d1809bb9652a0a287eba558dbbe233c0fedae2a
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2013-06-24 09:24:05 +0000

    Adding django user group defintion to createuser script

--- trunk/tools/Python/www/www-django/mcwww/mcwww/management/commands/createuser.py
+++ trunk/tools/Python/www/www-django/mcwww/mcwww/management/commands/createuser.py
@@ -3,8 +3,7 @@ from django.core.management.base import BaseCommand, make_option
 import sys
 from getpass import getpass
 
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import User, Group
 
 def main(args):
     if len(args) < 1:
@@ -18,14 +17,23 @@ def main(args):
         print 'Abort: User already exists!'
         sys.exit(1)
 
-    if len(args) == 2:
+    if len(args) > 1:
         print 'Password taken from argument'
         password = args[1]
     else:
         password = getpass('Enter password: ')
-
+    
     user = User.objects.create_user(username, password=password)
     user.save()
+    
+    if len(args) > 2:
+        for idx in range(2,len(args)):
+            print '- adding user to group '+args[idx]
+            try:
+                g = Group.objects.get(name=args[idx]) 
+                g.user_set.add(user)
+            except:
+                print 'Error: Group '+args[idx]+' does not exist!'
 
     print 'User created.'
 
