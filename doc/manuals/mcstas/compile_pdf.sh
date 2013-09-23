63d8c48c66eed575d7538d8ca9400129b8b4c0b0
pkwi <pkwi@fysik.dtu.dk>
2013-09-23 09:22:47 +0000

    Edits to allow building the manual via cmake and pdflatex.
    Component_manual needs similar edits (ps figures -> pdf etc.) as the manual already recieved, so disabled for now

--- doc/manuals/mcstas/compile_pdf.sh
+++ doc/manuals/mcstas/compile_pdf.sh
@@ -4,7 +4,7 @@ NAME="$1"
 
 echo "Building ${NAME}"
 
-for prog in latex bibtex makeindex latex latex; do
+for prog in pdflatex bibtex makeindex pdflatex pdflatex; do
     echo "> ${prog}";
     ${prog} "${NAME}" > /dev/null || exit 1;
 done
@@ -12,16 +12,16 @@ done
 echo ""
 
 
-DVIPDF="`command -v dvipdf`"
+#DVIPDF="`command -v dvipdf`"
 
-DVIPS="`command -v dvips`"
-PSPDF="`command -v ps2pdf`"
+#DVIPS="`command -v dvips`"
+#PSPDF="`command -v ps2pdf`"
 
-if [ -x "${DVIPDF}" ]; then
-    echo "Generate PDF file using dvipdf (better quality than ps2pdf)";
-    ${DVIPDF} ${NAME}.dvi ${NAME}.pdf;
-else
-    echo "Generate PDF file using dvips and ps2pdf";
-    dvips -o ${NAME}.ps ${NAME}.dvi;
-    ps2pdf ${NAME}.ps;
-fi
+#if [ -x "${DVIPDF}" ]; then
+#    echo "Generate PDF file using dvipdf (better quality than ps2pdf)";
+#    ${DVIPDF} ${NAME}.dvi ${NAME}.pdf;
+#else
+#    echo "Generate PDF file using dvips and ps2pdf";
+#    dvips -o ${NAME}.ps ${NAME}.dvi;
+#    ps2pdf ${NAME}.ps;
+#fi
