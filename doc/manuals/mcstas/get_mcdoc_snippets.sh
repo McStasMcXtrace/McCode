b794293dea0461256d9277499f855a7ee03e1f5c
pkwi <pkwi@fysik.dtu.dk>
2014-02-08 16:54:00 +0000

    HTML in the snippets is causing trouble, therefore opt for simpler lstlisting for now.
    Implemented with all source and sample comps so far

--- doc/manuals/mcstas/get_mcdoc_snippets.sh
+++ doc/manuals/mcstas/get_mcdoc_snippets.sh
@@ -15,7 +15,7 @@ do
     cd $DIRNAME/
     sed s/@COMP@/$COMP/g $PREFIX | sed s/@CAT@/$DIRNAME/g > $COMP.parms
     cat $HEADER >> $COMP.parms
-    mcdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -v \# | sed "s/_/\\\\_/g" | sed "s/\]/\]\$\&\ /g" | sed "s/\[/\$\[/g" | sed "s/</\\\\\\\\\\\hline\ /g" | sed "s/>\:/\\ \&\\ /g"  >> $COMP.parms
+    mcdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -v \#  >> $COMP.parms
     cat $FOOTER >> $COMP.parms
     cd $PW
 done
