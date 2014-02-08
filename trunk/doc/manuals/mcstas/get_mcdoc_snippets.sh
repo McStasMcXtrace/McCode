0775c71871b861936a3bda7d2a73af1a6224b50b
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-02-08 11:21:26 +0000

    Experimental TeX table from comp parms

--- trunk/doc/manuals/mcstas/get_mcdoc_snippets.sh
+++ trunk/doc/manuals/mcstas/get_mcdoc_snippets.sh
@@ -7,9 +7,13 @@
 BASEDIR=../../../mcstas-comps/
 DIRNAME=`basename $1`
 PW=$PWD
+HEADER=$PWD/COMPheader
+FOOTER=$PWD/COMPfooter
 for COMP in `cat $DIRNAME/mcdoc_index`
 do
     cd $DIRNAME/
-    mcdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -v \# > $COMP.parms
+    cp $HEADER $COMP.parms
+    mcdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -v \# | sed "s/_/\\\\_/g" | sed "s/\</\\\\\\\\\\\hline\ /g" | sed "s/\>\:/\\ \&\\ /g" | sed "s/\]/\]\$\&\ /g" | sed "s/\[/\$\[/g" >> $COMP.parms
+    cat $FOOTER >> $COMP.parms
     cd $PW
 done
