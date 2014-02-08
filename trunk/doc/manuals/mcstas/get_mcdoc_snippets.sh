5e565457f15b33449b6bb5e694a58ed3eda28386
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-02-08 12:48:05 +0000

    Different quoting issues Mac os vs Linux... Let's see if sed will do it

--- trunk/doc/manuals/mcstas/get_mcdoc_snippets.sh
+++ trunk/doc/manuals/mcstas/get_mcdoc_snippets.sh
@@ -7,12 +7,13 @@
 BASEDIR=$2/../../../mcstas-comps/
 DIRNAME=`basename $1`
 PW=$PWD
+PREFIX=$PWD/COMPprefix
 HEADER=$PWD/COMPheader
 FOOTER=$PWD/COMPfooter
 for COMP in `cat $DIRNAME/mcdoc_index`
 do
     cd $DIRNAME/
-    echo "{\\bf Input Parameters for component \\verb+$COMP+ from $DIRNAME}" > $COMP.parms
+    sed s/@COMP@/$COMP/g $PREFIX | sed s/@CAT@/$DIRNAME/g > $COMP.parms
     cat $HEADER >> $COMP.parms
     mcdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -v \# | sed "s/_/\\\\_/g" | sed "s/\]/\]\$\&\ /g" | sed "s/\[/\$\[/g" | sed "s/</\\\\\\\\\\\hline\ /g" | sed "s/>\:/\\ \&\\ /g"  >> $COMP.parms
     cat $FOOTER >> $COMP.parms
