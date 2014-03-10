eccbca920831d7cca90288cd91004152f1612f59
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-03-10 09:17:55 +0000

    Phenomenological fits of time-distribution and moderator "flux assymmetry" from Troels Schoenfeldt. Consider cleanups

--- trunk/mcstas-comps/share/ess_source-lib.c
+++ trunk/mcstas-comps/share/ess_source-lib.c
@@ -440,18 +440,41 @@ double ESS_2014_Schoenfeldt_cold(double *t, double *p, double lambda, double tfo
   double alpha_decay=3277.8;
   /* Normalization constant to achieve that int(pulse)=1. */
   /* NOTE: Assumption is that ~ 20% of intensity comes after the main pulse */
-  double gamma = 1.2*ESS_SOURCE_DURATION;
+  double gamma = ESS_SOURCE_DURATION/(0.8);
 
   /* Assign 90% of statistics within the pulse duration */
-  if (rand01()>0.1) {
+  if (rand01()>0.2) {
     *t = ESS_SOURCE_DURATION*(rand01());
   } else {
     *t =  ESS_SOURCE_DURATION -1e-3*log(1e-12+rand01());
   }
   /* Troels Schoenfeldt function for timestructure */
-  *p *= TSC_Time_Model(*t,lambda,ESS_SOURCE_DURATION,gamma);
+  *p *= TSC_TimeDist_cold(*t, lambda, 100*extras.height_c, ESS_SOURCE_DURATION,gamma);
+  *p *= TSC_y0_cold(100*extras.Y, 100*extras.height_c);
 } /* end of ESS_2014_Schoenfeldt_cold */
 
+
+//<<<<<y0 Distribution Cold>>>>>>>>>>>>>>>>>>>>>>>>>
+double TSC_y0_cold(double y0,double height){
+  double one_over_integral_y0_of_height= height/((0.36434*height*height+2.53796*height-0.107774));
+  if(y0 < -height/2. || y0 > height/2. )return 0;
+  double cosh_ish=(exp(-7e-1/sqrt(height)*(y0-height/2.))+exp(-7e-1/20.*height+7e-1/sqrt(height)*(y0+height/2.)));
+  double sinh_ish=(exp(50/sqrt(height)*(y0-height/2.))-1)*(exp(-50/sqrt(height)*(y0+height/2.))-1);
+  double tmp=one_over_integral_y0_of_height*cosh_ish*sinh_ish;
+  return tmp;
+}
+//<<<<<Alpha the Time Distribution Cold>>>>>>>>>
+double TSC_alpha_of_lambda_for_t_cold(double lambda,double height){
+  if(lambda<0.43)return 1e-13;
+  return (-4.12609e-006*height+0.000214586)*pow(lambda-0.43,0.33333);
+}
+//<<<<<Time Distribution Cold>>>>>>>>>>>>>>>>>>>>>>>
+double TSC_TimeDist_cold(double t,double lambda,double height, double pulselength, double gamma){
+  double normalizer=1;//gamma/pulselength;
+  if(t<0)return 0;
+  if(t<pulselength)return (1-exp(-(t)/TSC_alpha_of_lambda_for_t_cold(lambda,height)));
+  return (2-exp(-pulselength/TSC_alpha_of_lambda_for_t_cold(lambda,height))-1)*exp(-(t-pulselength)/TSC_alpha_of_lambda_for_t_cold(lambda,height));
+}
 
 /* This is the thermal moderator with 2013 updates, fits from Troels Schoenfeldt */
 double ESS_2014_Schoenfeldt_thermal(double *t, double *p, double lambda, double tfocus_w, double tfocus_t, double tfocus_dt, ess_moderator_struct extras)
@@ -526,7 +549,7 @@ double TSC_Time_Model(double time, double lambda, double pulselength, double gam
     return isnan(tmp) ? 0 : tmp;
 }
 
-double TSC_y0_Model(double y,double height, double center){
+double TSC_y0_Model_Cold(double y,double height, double center){
     double integral=1; //insert integral from c-h/2 to c+h/2
     if(y<center-height/2.||y>center+height/2.)return 0;
     return 1./integral*(exp(-2.2e-001*(y-center-height/2.))+exp(-4.5e-002*height+2.2e-001*(y-center+height/2.)))*(exp(50./sqrt(height)*(y-center-height/2.))-1)*(exp(-50./sqrt(height)*(y-center+height/2.))-1);
