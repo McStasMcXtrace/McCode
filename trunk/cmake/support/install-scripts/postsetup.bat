525d84c76d3ee1709f8b55dabfdaf64e2871eef8
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-04-06 15:23:42 +0000

    For stability reasons, put Strawberry Perl bin PATH in front in install and run-scripts

--- trunk/cmake/support/install-scripts/postsetup.bat
+++ trunk/cmake/support/install-scripts/postsetup.bat
@@ -1,10 +1,11 @@
 @REM Script for final installation of McStas-related Perl modules
 @REM
-@call ppm install http://ppds.mccode.org/Astro-FITS-Header.ppd
-@call ppm install http://ppds.mccode.org/Convert-UU.ppd
-@call ppm install http://ppds.mccode.org/Tk.ppd
-@call ppm install http://ppds.mccode.org/Tk-CodeText.ppd
-@call ppm install http://ppds.mccode.org/Math-Amoeba.ppd
-@call ppm install http://ppds.mccode.org/OpenGL.ppd
-@call ppm install http://ppds.mccode.org/PDL.ppd
-@call ppm install http://ppds.mccode.org/PGPLOT.ppd
+@set PATH=c:\\strawberry\\perl\\bin;%PATH%
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/Astro-FITS-Header.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/Convert-UU.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/Tk.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/Tk-CodeText.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/Math-Amoeba.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/OpenGL.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/PDL.ppd
+@call c:\\strawberry\\perl\\bin\\ppm.exe install http://ppds.mccode.org/PGPLOT.ppd
