0ed4881a73d4ee5f83a376522da4bcc1d6b33a34
erkn <erkn@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-05-15 09:59:15 +0000

    now uses the build-tree components
    instead of whatever happens to be installed

--- trunk/doc/manuals/mcxtrace/get_mxdoc_snippets.sh
+++ trunk/doc/manuals/mcxtrace/get_mxdoc_snippets.sh
@@ -4,19 +4,26 @@
 #
 #
 
-BASEDIR=$2/../../../mcxtrace-comps/
 DIRNAME=`basename $1`
 PW=$PWD
 PREFIX=$PWD/COMPprefix
 HEADER=$PWD/COMPheader
 FOOTER=$PWD/COMPfooter
+
+#search upwards for comps directory
+COMP_SOURCE_DIR=$PW
+while [ ! -d $COMP_SOURCE_DIR/mcxtrace-comps ] && [ "x$COMP_SOURCE_DIR" != "x" ]
+do
+  COMP_SOURCE_DIR=${COMP_SOURCE_DIR%/*}
+done
+
 for COMP in `cat $DIRNAME/mxdoc_index`
 do
-    cd $DIRNAME/
-    sed s/@COMP@/$COMP/g $PREFIX | sed s/@CAT@/$DIRNAME/g > $COMP.parms
-    cat $HEADER >> $COMP.parms
-    mxdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -v \#  >> $COMP.parms
-    cat $FOOTER >> $COMP.parms
+    sed s/@COMP@/$COMP/g $PREFIX | sed s/@CAT@/$DIRNAME/g > $DIRNAME/$COMP.parms
+    cat $HEADER >> $DIRNAME/$COMP.parms
+    cd $COMP_SOURCE_DIR/mcxtrace-comps
+    mxdoc -t $COMP.comp | grep -A1000 \#\ Input | grep -B1000 \#\ Output | grep -ve ^\ *$ | grep -v \# >> $PW/$DIRNAME/$COMP.parms
     cd $PW
+    cat $FOOTER >> $DIRNAME/$COMP.parms
 done
 touch $DIRNAME.done
