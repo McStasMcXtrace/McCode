dba5e74b559410b8521e60d2e4f1b26ead3b5c38
lewis <lewis@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-05-23 14:31:24 +0000

    Some more admin styling, jujst trying to get the whole thing so that the permissions are set up nicely with no possibility of overwriting Jobs or moving simulations to the wrong parameter set is possible. I am pretty sure this is still breakable so no surprises if you get a layer deep and you find an error.
    tl;dr Django admin fiddling still.

--- trunk/tools/Python/www/www-django/mcwww/mcsimulator/models.py
+++ trunk/tools/Python/www/www-django/mcwww/mcsimulator/models.py
@@ -17,34 +17,32 @@ from datetime import datetime
 
 class Job(models.Model):
     ''' A job specifying the simuation to run and its status '''
-
     # Unique permanent reference (for use in links)
     ref = models.CharField(max_length=64, db_index=True)
-
     # Parameters
     seed    = models.IntegerField()
     samples = models.IntegerField()
     npoints = models.IntegerField()
-
     # Simulation to run
-    sim     = models.ForeignKey('Simulation')
-    
+#    sim     = models.ForeignKey('Simulation')
+    sim = models.OneToOneField('Simulation')
     # Date and time created
     created = models.DateTimeField(db_index=True, editable=False)
-
     @staticmethod
     def new(ref, sim, seed=0, samples=1000, npoints=1):
         return Job(None, ref, seed, samples, npoints, sim.id, created=now())
+    def sim_details(self):
+        return ("Simulation [name, id]: [%s, %s]") % (self.sim.name, self.sim.id)
+    def __unicode__(self):
+        return "Job Details [ref, sim name]: [%s, %s]" % (self.ref, self.sim.name)
 
 
 class Simulation(models.Model):
     ''' A simulation '''
-
     # Simulation name (see Param/sim.param_set() for parameters)
     name = models.CharField(max_length=64, unique=True, db_index=True)
     simgroup = models.CharField(max_length=64, unique=False, db_index=True)
     displayname = models.CharField(max_length=64, unique=False, db_index=True)
-    
     # param_set()  gives the parameters
     # job_set()    gives the related jobs
     # simrun_set() gives the related runs
@@ -56,33 +54,25 @@ class Simulation(models.Model):
     def new(*args, **kwargs):
         return Simulation(None, *args, **kwargs)
     def __unicode__(self):
-        return self.name # u'<Sim %s>' % self.name # Changed this to make it look good on the admin page, change back if it breaks something (no reason why it should)
-    
+        return u'<Sim %s>' % self.name 
 
 class SimRun(models.Model):
     ''' A particular run of a simulation (configured by Job) '''
-
     # Unique permanent reference (for use in links)
     ref = models.CharField(max_length=64, db_index=True)
-
     # Creator / owner
     user = models.ForeignKey(User)
-    
     # Job to run
     job = models.ForeignKey('Job')
     sim = models.ForeignKey('Simulation')
-    
     # Current status (waiting, running, completed)
     status = models.CharField(max_length=32, db_index=True)
-
     # Parameters and result as JSON
     str_params = models.TextField()
     str_result = models.TextField()
-
     # Dates for creation and completion
     created   = models.DateTimeField(db_index=True, editable=False)
     completed = models.DateTimeField(db_index=True, editable=False, null=True)
-
     @staticmethod
     def new(user, job, sim, params):
         ref = job.ref + "__" + str(datetime.now()).replace(' ', '_')
@@ -91,40 +81,34 @@ class SimRun(models.Model):
         run.params = params
         run.result = None
         return run
-
     @property
     def params(self):
         return loads(self.str_params)
     @params.setter
     def params(self, p):
         self.str_params = dumps(p)
-
     @property
     def result(self):
         return loads(self.str_result)
     @result.setter
     def result(self, r):
         self.str_result = dumps(r)
+    def __unicode__(self):
+        return "SimRun Details [ref, job ref, sim name]: [%s, %s, %s]" % (self.ref, self.job.ref, self.sim.name)
 
 class Param(models.Model):
     ''' A parameter '''
-
     # Where to show the parameter in a listing (e.g. on configure page)
     priority = models.IntegerField()
-
     # Name, unit and help message
     name = models.CharField(max_length=128)
     unit = models.CharField(max_length=128)
     msg  = models.CharField(max_length=128)
-    
     # JSON-encoded default value
     str_default = models.CharField(max_length=256)
-
     # The simulation this parameter belongs to
     sim = models.ForeignKey('Simulation')
-
     # value_set() gives the ParamValue's that use this parameter
-
     @staticmethod
     def new(sim, name, unit, value=None, msg=''):
         param = Param(None, sim.id, name, unit, msg, 'null')
@@ -139,27 +123,24 @@ class Param(models.Model):
     def default_value(self, val):
         self.str_default = dumps(val)
     def __unicode__(self):
-        return '<Param: %s, %s>' % (self.id, self.name) # self.default_value, self.unit) # May have to change back if this breaks soimething else (though I can't see how it would. 
+        return '<Param: %s, %s, %s>' % (self.name, self.default_value, self.unit) # May have to change back if this breaks soimething else (though I can't see how it would. 
                                                   # Added self.id and commented out the rest of the line. Remember to put in the right number of %s's back in!
     
 class ParamValue(models.Model):
     ''' A parameter value tied to a specific job '''
-    param     = models.ForeignKey('Param') # I think a 1-to-1 relationship would suit better, one parameter, one value. Maybe not, maybe it is the scan functionality.
+    param     = models.ForeignKey('Param')
     job       = models.ForeignKey('Job')
     str_value = models.TextField()
-
     @staticmethod
     def new(param, job, value):
         pv = ParamValue(None, param.id, job.id)
         pv.value = value
         return pv
-
     @property
     def value(self):
         return loads(self.str_value)
     @value.setter
     def value(self, val):
         self.str_value = dumps(val)
-
-    def __unicode__():
+    def __unicode__(self):
         return self.param.name
