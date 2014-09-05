bb6d0831f499cdfae1baedf0cd8bfb4866175734
lewis <lewis@fysik.dtu.dk>
2014-09-05 15:19:56 +0000

    reinserting conditional

--- tools/Python/www/www-django/mcwww/mcwww/management/LDAP/LDAPData.py
+++ tools/Python/www/www-django/mcwww/mcwww/management/LDAP/LDAPData.py
@@ -6,36 +6,37 @@ class LDAPData:
         self.UID = None
         self.CN = None
         self.SN = None
-        self.displayName = None
         self.MAIL = None
+        self.displayName = None
 
         self.ldif_file = None
         self.PASSWORD = None
 
     def setcn(self, cn):
-        self.CN = cn
+        if not self.CN: self.CN = cn
     def cn(self):
         return self.CN
 
     def setsn(self, sn):
-        self.SN = sn
+        if not self.SN: self.SN = sn
     def sn(self):
         return self.SN
+
+
+    def setmail(self, M):
+        if not self.MAIL: self.MAIL = M
+    def mail(self):
+        return self.MAIL
+
+    def setuid(self, un):
+        if not self.UID: self.UID = un
+    def uid(self):
+        return self.UID
 
     def setdisplayname(self, name):
         self.displayname = name
     def displayname(self):
         return self.displayName
-
-    def setmail(self, M):
-        self.MAIL = M
-    def mail(self):
-        return self.MAIL
-
-    def setuid(self, un):
-        self.UID = un
-    def uid(self):
-        return self.UID
 
     def setldif_file(self, filepath):
         self.ldif_file = filepath        
@@ -57,7 +58,7 @@ class LDAPDataBuilder:
         uid = None
         splitname = split(" ", full_name)
         splitlen = len(splitname)-1
-        if splitlen = 2:
+        if splitlen == 2:
             uid = splitname[0][0]+splitname[1][0,3]
         else: 
             s = 0
@@ -69,8 +70,8 @@ class LDAPDataBuilder:
         self.data.setuid(uid)
         self.data.setcn("%s %s" % splitname[0], splitname[splitlen])
         self.data.setsn(splitname[splitlen])
-        self.data.setdisplayname(splitname[0])
         self.data.setmail(mail)
+        self.data.setdisplayname(splitname[0])
         
     def getData(self):
         return self.data
@@ -80,26 +81,22 @@ class LDAPDataBuilder:
 #===========================#
 class LDAPDataPopulator:
     def __init__(self, cn, pw):
-        data = LDAPData()
+        self.data = LDAPData()
+        conn = LDAPComm()
         query = "cn=\"%s\"" % cn
         dn = "dn=\"cn=%s,ou=person,dc=fysik,dc=dtu,dc=dk\"" % cn
         usr_details = conn.ldapQuery(dn, pw, query)
         for line in usr_details:
             if 'uid' in line:
-                data.setuid(split(" ", line)[1])
+                self.data.setuid(split(" ", line)[1])
             if 'cn' in line:
-                data.setcn(split(" ", line)[1])
+                self.data.setcn(split(" ", line)[1])
             if 'sn' in line:
-                data.setsn(split(" ", line)[1])
-            if 'displayname' in line:
-                data.setdisplayname(split(" ", line)[1])
+                self.data.setsn(split(" ", line)[1])
             if 'email' in line:
-                data.setmail(split()" ", line)[1])
+                self.data.setmail(split()" ", line)[1])
+            if 'displayname' in line:
+                self.data.setdisplayname(split(" ", line)[1])
 
     def getData(self):
-        return self.data
-
-def LDAPDict(key):
-    return {
-        
-    
+        return self.data        
