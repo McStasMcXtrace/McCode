78d9441347e2dba3d2161f806eef940ee00fb2f6
pkwi <pkwi@fysik.dtu.dk>
2014-09-08 09:49:39 +0000

    Small improvements making script cron'able...

--- tools/Python/www/www-django/mcwww/scripts/compile.sh
+++ tools/Python/www/www-django/mcwww/scripts/compile.sh
@@ -14,15 +14,25 @@ fi
 
 cd sim
 
-for dir in $( ls -d * | grep -v datafiles ); do
+for dir in $( find . -maxdepth 1 -type d | cut -b3- | grep -v datafiles | grep -v svn); do
   cd $dir
   echo "Instrument group $dir..."
   mcdoc -t .
-    for i in $( ls *.instr ); do
+    # Compile McStas instruments
+    echo "McStas based in instruments..."
+    for i in $( grep -i mcstas *.instr | cut -f1 -d: | uniq ); do
       echo "> $i"
       b=`basename $i .instr`
-      mcrun ${MCRUN_FLAGS} --trace --info $i &>/dev/null;
       
+      mcrun ${MCRUN_FLAGS} --trace --info $i &>/dev/null;      
+    done
+    # Compile McXtrace instruments
+    echo "McXtrace based in instruments..."
+    for i in $( grep -i mcxtrace *.instr | cut -f1 -d: |uniq ); do
+      echo "> $i"
+      b=`basename $i .instr`
+
+      mxrun ${MCRUN_FLAGS} --trace --info $i &>/dev/null;
     done
   mv *.html ..
   cd -
