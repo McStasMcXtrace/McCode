cd5f7b0842b16514fbf396940952a39e43415a7f
lewis <lewis@fysik.dtu.dk>
2014-10-09 12:17:34 +0000

    Nicer layout, comments and improvement instructions.

--- tools/Python/www/www-django/mcwww/mcwww/management/commands/createuser.py
+++ tools/Python/www/www-django/mcwww/mcwww/management/commands/createuser.py
@@ -1,3 +1,16 @@
+#===================================================================#
+# User Creation Script                                              #
+# --------------------                                              #
+# Creates sqlite and LDAP DB entries.                               #
+# Django User and Combined Authentication for Moodle and Mediawiki  #
+# under same pwd.                                                   #
+# ---------------                                                   #
+# - Submitted: 17:00 (ish) 15-7-14                                  #
+# - Completed primary: 9-10-14                                      #
+# - Secondary: Group Membership                                     #
+#              McUser implementation (possibly not necc but nice)   #
+#===================================================================#
+
 import sys
 from sys import stdin
 from django.core.management.base import BaseCommand, make_option
@@ -5,11 +18,6 @@ from mcwww.management.LDAP.LDAPUserCreation import *
 from mcwww.management.LDAP.LDAPComm import *
 from django.contrib.auth.models import User, Group
 from getpass import getpass
-
-#
-# Added functionality to call the LDAP modules for LDAP user entry
-# - Submitted: 17:00 (ish) 15-7-14
-# - Completed: 
 
 def main(args):
     comm = LDAPComm()
@@ -30,26 +38,29 @@ def main(args):
     else:
         password = getpass('Enter password: ')
 
-# Test to see if users can be added to LDAP DB : no point in continuing if not
+    #======================#
+    # LDAP DB Modification #
+    # Check permissions
     LDAP_admin_cn = raw_input('Enter your LDAP authentication cn (not your uid): ')
-
     if(not comm.ldapAdminGroupQuery(LDAP_admin_cn)): 
         print "Insufficient LDAP privs, your cn may not be what you input.\n Try putting in surname or contact admin.\n"
         sys.exit(1)
-# LDAPUserCreation call
-# PUT REQUEST FOR GROUP MEMBERSHIP IN HERE
+    '''
+    REQUEST GROUP MEMBERSHIP HERE : secondary importance, all are students at the moment
+    '''
+    # LDAPUserCreation call
     entity = LDAPUserCreation(username, password)
     LDAP_admin_pw = getpass('Enter your LDAP authentication pwd: ')
     LDAP_admin_dn = "cn=%s,ou=person,dc=fysik,dc=dtu,dc=dk" % LDAP_admin_cn
-
     print "Calling: processLDIF(", LDAP_admin_dn, ",", LDAP_admin_pw, ")"
-
     entity.processLDIF(LDAP_admin_dn, LDAP_admin_pw)
     print "LDAP User Added"
+    # End LDAP Modification #
+    #=======================#
+
 # McCode User creation
     user = User.objects.create_user(username, password=password)
     user.save()
-
 # Add to groups implied in the args
     if len(args) > 2:
         for idx in range(2,len(args)):
@@ -59,7 +70,6 @@ def main(args):
                 g.user_set.add(user)
             except:
                 print 'Error: Group '+args[idx]+' does not exist!'
-
     print 'McCode User created.'
 
 class Command(BaseCommand):
