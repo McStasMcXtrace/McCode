3cb3318317833b0930747680435905ea460bcfa4
lewis <lewis@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2014-11-14 15:05:53 +0000

    Scripting the ldap setup on instatiation of all DBs (the upload and mcUser setup will go here too). Changed the ID for the simulation DB to include the group name. Don't migrate, rebuild the server...sorry :)

--- trunk/tools/Python/www/www-django/mcwww/bin/init-db.sh
+++ trunk/tools/Python/www/www-django/mcwww/bin/init-db.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-python manage.py syncdb
+DN=`sed '2q;d' /etc/hosts`
+NAME=`cat /etc/hostname`
+DN=${DN%$NAME*}     # all before (repeated) NAME
+DN=${DN##*$NAME.}   # get after first NAME on line
+
+echo "Please input your LDAP root password"
+ROOTPW=`slappasswd`
+echo "Please input your moodle bind password"
+BINDPW=`slappasswd`
+echo "Please input your LDAP DB admin password (the one you set on slapd install)"
+read TREEPW
+
+echo ROOTPW cipher: $ROOTPW
+echo BINDPW cipher: $BINDPW
+echo TREEPW : $TREEPW
+echo name: $NAME
+echo dn: $DN
+
+#python ldap-build.py
+#python manage.py syncdb
