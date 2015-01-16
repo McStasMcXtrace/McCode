e9e5b123025aa9c3584c23ec99a64824f8da7de4
lewis <lewis@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2015-01-16 13:57:16 +0000

    mcUser LDAP capability for mcwww-django

--- trunk/tools/Python/www/www-django/mcwww/bin/create-user.sh
+++ trunk/tools/Python/www/www-django/mcwww/bin/create-user.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python create_user.py $*
+python mcUser/management/commands/create_user.py $*
--- trunk/tools/Python/www/www-django/mcwww/bin/init-db.sh
+++ trunk/tools/Python/www/www-django/mcwww/bin/init-db.sh
@@ -8,41 +8,20 @@ NAME=`cat /etc/hostname`
 DN=${DN%$NAME*}                     # all before (repeated) NAME
 DN=${DN##*$NAME.}                   # get after first NAME on line 
 DN=$(echo $DN | sed 's/ *$//')      # trim whitespace at end
-DN="${DN//./,dc=}"               # replacing '.' with ',dc='
+DN="${DN//./,dc=}"                  # replacing '.' with ',dc='
 echo Obtained LDAP database DN: $DN
 echo " "
 #---------------#
 # Get passwords #
 #---------------#
-count=0
-warn="."
-until [[ ${ROOTPW:0:1} == "{" ]]; do
-    echo "Please input your LDAP root password"
-    read -s PROOTPW
-    ROOTPW=`slappasswd -s $PROOTPW`
-    echo " "
-done
-warn="."
-until [[ ${BINDPW:0:1} == "{" ]]; do
-    echo "Please input your moodle bind password"$warn
-    warn=", ensuring passwords match."
-    BINDPW=`slappasswd`
-    echo " "
-done
-warn="."
-until [[ ${LDAPOP:0:1} == "d" ]]; do
-    echo "Please input your LDAP DB admin password"$warn
-    warn=" (the one you set on slapd install)."
-    read -s TREEPW
-    echo " "
-    echo Testing LDAP accesses.
-    LDAPOP=`ldapwhoami -D cn=admin,dc=$DN -w $TREEPW`
-done
+ROOTPW=`slappasswd -s 7r4um4r00t`
+BINDPW=`slappasswd -s spoons`
 echo Access by admin accepted.
 echo " "
 
 #---------------------#
 # Calling DB builders #
 #---------------------#
-python ./bin/ldap-build.py $DN $ROOTPW $BINDPW $TREEPW $PROOTPW
-python manage.py syncdb
+python ./bin/ldap-build.py $DN $ROOTPW $BINDPW $TREEPW 7r4um4r00t 7r4um4r00t
+python server.py syncdb
+# python manage.py syncdb
