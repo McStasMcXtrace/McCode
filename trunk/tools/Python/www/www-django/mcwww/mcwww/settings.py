bed82378ec2d1a3e891b33a0ca7b3e59b548c17d
lewis <lewis@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2015-01-28 13:00:04 +0000

    Chasing bugs made me refit the settings.py, nicer to read now.

--- trunk/tools/Python/www/www-django/mcwww/mcwww/settings.py
+++ trunk/tools/Python/www/www-django/mcwww/mcwww/settings.py
@@ -3,20 +3,15 @@ PROJECT_PATH = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
 #===============#
 # McUser CONFIG #
 #===============#
-# LOGIN_URL = './login',
-LOGIN_URL = '/login'
-AUTHENTICATION_BACKENDS=('models.mcBackend',)
-AUTH_USER_MODEL        = 'models.mcUser'
+LOGIN_URL                       = '/login'
+AUTHENTICATION_BACKENDS         = ('models.mcBackend',)
+AUTH_USER_MODEL                 = ('models.mcUser')
 SESSION_COOKIE_AGE              = 10*60
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST      = True
 LDIF_DIR                        = 'mcUser/management/LDAP/LDIFs/temp/'
-# STATIC_URL                      = root_path('static')
-# ROOT_URLCONF                    = module.__name__
-# TEMPLATE_DIRS                   = [os.path.dirname(module.__file__),
-#                                   root_path('templates')]
-# SESSION_ENGINE                  = 'mcUser.mcSession'
-# SESSION_FILE_PATH               = './sqlite3_DB/session_data'
+SESSION_FILE_PATH               = './sqlite3_DB/session_data' # trace of logged in sessions
+# SESSION_ENGINE                  = 'mcUser.mcSession'        # may make if AbstactBaseUser inherit not work
 #============#
 # McUser END #
 #============#
@@ -24,18 +19,11 @@ LDIF_DIR                        = 'mcUser/management/LDAP/LDIFs/temp/'
 #============================#
 # BEGIN McStas configuration #
 #============================#
-# IMAGE_FORMAT is either 'gif' or 'png' - R-based plotter only supports png
-IMAGE_FORMAT = 'png'
-# Use MPI to utilise N processors (disabled when 0)
-MPI_NP=0
-# Maximum number of neutrons allowed (ray samples / ncount)
-MAX_RAY_SAMPLES = 10000000
-# Maximum number of scan points allowed (npoints)
-MAX_SCAN_POINTS = 1000
-# The line below would create a link in the simulation folder
-# named 'datafiles' to the path in 'sim/datafiles'
-# (relative to the webapp folder)
-DATA_FILES = ('sim/datafiles',)
+IMAGE_FORMAT    = 'png'              # IMAGE_FORMAT is either 'gif' or 'png' - R-based plotter only supports png
+MPI_NP          = 0                  # Use MPI to utilise N processors (disabled when 0)
+MAX_RAY_SAMPLES = 10000000           # Maximum number of neutrons allowed (ray samples / ncount)
+MAX_SCAN_POINTS = 1000               # Maximum number of scan points allowed (npoints)
+DATA_FILES      = ('sim/datafiles',) # performs: ln <simulation folder>/datafiles -> 'sim/datafiles'  (relative to the webapp folder)
 #==========================#
 # END McStas configuration #
 #==========================#
@@ -43,110 +31,86 @@ DATA_FILES = ('sim/datafiles',)
 #===============#
 # Django Config #
 #===============#
-DEBUG =  True # False - this needs to be set to false. css do not load if ture at the mo. 
+SECRET_KEY     = 'gaeh@565h%=7)gw#625*ag82am#*55xnb40xa769yaxq-^ukj*' # THIS SHOULD BE SECRET!
+DEBUG          =  True # False       <----  THIS NEEDS TO BE SET TO FALSE. css broken if false!!!! 
 TEMPLATE_DEBUG = DEBUG
-ROOT_URLCONF = 'mcwww.urls'
-
 INSTALLED_APPS = (
+    # mcApps
     'mcsimulator',
     'mcwww',
     'mcUser',
-
+    # djangApps
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',     # Uncomment for clickjack protection
 )
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mcwww.wsgi.application'
 
 #-----------------#
 # DATABASE CONFIG #
 #-----------------#
-ADMINS = (
+ADMINS     = ( 
+    ('Peter Willendrup', 'pkwi@fysik.dyu.dk'),
     ('Mark Lewis', 'lewis@fysik.dtu.dk'),
-)
-MANAGERS = ADMINS
-DATABASES = {
-    'default': {
-        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': './DB/mcwww.sqlite3',    # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    )
+MANAGERS   = ADMINS
+DATABASES  = {                                            # keys not read by sqlite removed.
+    'default': { 'ENGINE': 'django.db.backends.sqlite3',
+                 'NAME': './DB/mcwww.sqlite3',            # (sqlite3: NAME = path to db file)
+                 }
     }
-}
 #---------------#
 # END DATABASES #
 #---------------#
 
-#-------------#
-# FILE ACCESS #
-#-------------#
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+#--------------#
+# FILE SERVING # 
+#--------------#
+WSGI_APPLICATION = 'mcwww.wsgi.application' # path to WSGI app used by runserver.
+ROOT_URLCONF     = 'mcwww.urls'             # File holding URLs to be served.
+TEMPLATE_DIRS    = (                        # Absolute paths to template.htmls
+    PROJECT_PATH + '/templates/',
+    )
+TEMPLATE_LOADERS = (                        # Callables that import templates.
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    #     'django.template.loaders.eggs.Loader',
+    )
+STATIC_ROOT      = ''                       # Store custom static files in apps/static/ subdirectories and in STATICFILES_DIRS.
+STATIC_URL       = '/static/'               # URL prefix for static files.
+STATICFILES_DIRS = (                        # Absolute paths of static files.
     PROJECT_PATH + '/static/',
     PROJECT_PATH + '/out/'
 )
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
+STATICFILES_FINDERS = (                     # Classes that find static files.
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    PROJECT_PATH + '/templates/',
-)
-
+#------------------#
+# END FILE SERVING # 
+#------------------#
 #============================#
 # END (USEFUL) DJANGO CONFIG #
 #============================#
 
 #=====================#
-# FUTURE UPLOADER USE #
+# FUTURE UPLOADER USE # : Another File Serving Section, essentially.
 #=====================#
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_ROOT = ''       # Absolute path to user-uploaded directory
+MEDIA_URL  = ''        # URL that serves media from MEDIA_ROOT, inc trailing slash.
 #==============#
 # END UPLOADER #
 #==============#
@@ -154,71 +118,36 @@ MEDIA_URL = ''
 #-----------#
 # STD STUFF #
 #-----------#
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Europe/Copenhagen'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-gb'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'gaeh@565h%=7)gw#625*ag82am#*55xnb40xa769yaxq-^ukj*'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
+TIME_ZONE     = 'Europe/Copenhagen'  # Local time zone. See http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+LANGUAGE_CODE = 'en-gb'              # Language code. See http://www.i18nguy.com/unicode/language-identifiers.html
+SITE_ID       = 1
+USE_I18N      = True                 # loading internationalization machinery. (default -> true, optimised -> false... don't change)
+USE_L10N      = True                 # locale date formatting
+USE_TZ        = True                 # timezone-aware datetimes.
+#----------------#
+# logging config #
+#----------------#
+# this config:
+#   filter
+#    DEBUG=False.
+#   handled by
+#    email site admins 
+#   perform on
+#    HTTP 500 error 
+LOGGING = { # See http://docs.djangoproject.com/en/dev/topics/logging 
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
+    'filters': {'require_debug_false':  {'()': 'django.utils.log.RequireDebugFalse' 
+                                         }
+                },
+    'handlers': {'mail_admins': {'level': 'ERROR',
+                                 'filters': ['require_debug_false'],
+                                 'class': 'django.utils.log.AdminEmailHandler'
+                                 }
+                 },
+    'loggers': {'django.request': {'handlers': ['mail_admins'],
+                                   'level': 'ERROR',
+                                   'propagate': True,
+                                   },
+                }
     }
-}
-
-#MIDDLEWARE_CLASSES = (
-#    'django.contrib.sessions.middleware.Session.Middleware',
-#)
-
-
-
