1dde06759a83fa5f58b7f86108f2c5fb99361f7a
jaga <jaga@fysik.dtu.dk>
2015-03-10 15:24:55 +0000

    Added start simulation dialog (non-interactive dummy version)

--- tools/Python/mcgui/ui-pywrap.py
+++ tools/Python/mcgui/ui-pywrap.py
@@ -11,12 +11,17 @@ e.g. using:
 
 pyside-uic main.ui -o main-ui.py
 
+NOTE: ControlMainWindow must subclass the same as the ui, e.g.
+QMainWindow, QWidget, etc.
+
 '''
+
+
 
 class ControlMainWindow(QtGui.QMainWindow):
   def __init__(self, parent=None):
     super(ControlMainWindow, self).__init__(parent)
-    self.ui =  Ui_MainWindow()
+    self.ui =  Ui_dlgStartSim()
     self.ui.setupUi(self)
    
 if __name__ == "__main__":
