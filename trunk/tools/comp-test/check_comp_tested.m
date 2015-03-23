cbcfc486972c2482ec21834a67534fa70f73fc7e
pkwi <pkwi@2466c161-b38f-4609-84fe-1a4728e1f9f7>
2015-03-23 10:52:23 +0000

    Matlab/Octave tool from Emmanuel, checks what comps in a given directory are tested in the corresponding instrument examples
    >> nottested=check_comp_tested('../../mcstas-comps/');
    >> nottested(:)
    
    ans = 
    
        '../../mcstas-comps/contrib/Guide_four_side_2_shells.comp'
        '../../mcstas-comps/contrib/He3_cell.comp'
        '../../mcstas-comps/contrib/Pol_bender_tapering.comp'
        '../../mcstas-comps/contrib/Radial_div.comp'
        '../../mcstas-comps/contrib/SiC.comp'
        '../../mcstas-comps/contrib/Source_multi_surfaces.comp'
        '../../mcstas-comps/contrib/Virtual_mcnp_output.comp'
        '../../mcstas-comps/contrib/Virtual_tripoli4_output.comp'
        '../../mcstas-comps/contrib/multi_pipe.comp'
        '../../mcstas-comps/misc/Beam_spy.comp'
        '../../mcstas-comps/misc/PolAnalyser_ideal.comp'
        '../../mcstas-comps/misc/Vitess_output.comp'
        '../../mcstas-comps/monitors/DivLambda_monitor.comp'
        '../../mcstas-comps/monitors/DivPos_monitor.comp'
        '../../mcstas-comps/monitors/EPSD_monitor.comp'
        '../../mcstas-comps/monitors/Monitor_4PI.comp'
        '../../mcstas-comps/monitors/PSD_TOF_monitor.comp'
        '../../mcstas-comps/monitors/PSDcyl_monitor.comp'
        '../../mcstas-comps/monitors/PSDlin_diff_monitor.comp'
        '../../mcstas-comps/monitors/PreMonitor_nD.comp'
        '../../mcstas-comps/monitors/Res_monitor.comp'
        '../../mcstas-comps/monitors/TOF_PSD_monitor_rad.comp'
        '../../mcstas-comps/monitors/TOF_cylPSD_monitor.comp'
        '../../mcstas-comps/monitors/TOFlog_monitor.comp'
        '../../mcstas-comps/optics/Derotator.comp'
        '../../mcstas-comps/optics/Guide_anyshape.comp'
        '../../mcstas-comps/samples/Res_sample.comp'
        '../../mcstas-comps/samples/Sans_spheresMod1.comp'
        '../../mcstas-comps/samples/TOFRes_sample.comp'
        '../../mcstas-comps/sources/Monitor_Optimizer.comp'
        '../../mcstas-comps/sources/Source_Optimizer.comp'

---
+++ trunk/tools/comp-test/check_comp_tested.m
@@ -0,0 +1,67 @@
+function [not_tested, tested, instr_code, comps, instrs] = check_comp_tested(pw)
+
+if nargin == 0, pw = [];  end
+if isempty(pw), pw = pwd; end
+
+not_tested = {};
+tested     = {};
+
+allfiles = getAllFiles(pw);
+
+comps  = {};
+instrs = {};
+
+% sort into components and instruments
+for index=1:numel(allfiles)
+  [p,f,e] = fileparts(allfiles{index});
+  switch lower(e)
+  case '.comp'
+    comps{end+1} = allfiles{index};
+  case '.instr'
+    instrs{end+1} = allfiles{index};
+  end
+end
+
+% load all instrument source code into memory for fast search
+instr_code = [];
+for index=1:numel(instrs)
+  this = fileread(instrs{index});
+  if ~isempty(this)
+    instr_code{end+1} = this;
+  end
+end
+
+% check if components appear in any instrument
+for index=1:numel(comps)
+  [p,f,e] = fileparts(comps{index});
+  occurences = strfind(instr_code, f);
+  
+  if all(cellfun(@isempty, occurences))
+    not_tested{end+1} = comps{index};
+  else
+    tested{end+1}     = comps{index};
+  end
+end
+
+end % function
+
+% ------------------------------------------------------------------------------
+
+function fileList = getAllFiles(dirName)
+
+  dirData = dir(dirName);      % Get the data for the current directory
+  dirIndex = [dirData.isdir];  % Find the index for directories
+  fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
+  if ~isempty(fileList)
+    fileList = cellfun(@(x) fullfile(dirName,x),...  % Prepend path to files
+                       fileList,'UniformOutput',false);
+  end
+  subDirs = {dirData(dirIndex).name};  % Get a list of the subdirectories
+  validIndex = ~ismember(subDirs,{'.','..'});  % Find index of subdirectories
+                                               %   that are not '.' or '..'
+  for iDir = find(validIndex)                  % Loop over valid subdirectories
+    nextDir = fullfile(dirName,subDirs{iDir});    % Get the subdirectory path
+    fileList = [fileList; getAllFiles(nextDir)];  % Recursively call getAllFiles
+  end
+  
+end % getAllFiles
