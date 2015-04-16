9295d531c102c5ec0fccea62e9e657181a278dfb
Peter Willendrup <pkwi@fysik.dtu.dk>
2015-04-16 16:10:24 +0200

    Further configuration items for later implementation

--- tools/Python/mcgui/config.py
+++ tools/Python/mcgui/config.py
@@ -2,3 +2,20 @@
 mcstas/mcxtrace configuration.
 '''
 MCCODE_LIB_DIR = '/usr/share/mcstas/2.1/'
+MCCODE = 'mcstas'
+MCRUN = 'mcrun'
+MCPLOT = 'mcplot'
+MCDISPLAY = 'mcdisplay'
+TOOL_NAME = 'mcgui'
+PARTICLE = 'neutron'
+
+'''
+Compilation, parallelisation etc.
+'''
+CFLAGS = '-g lm -O2'
+NEXUSFLAGS = '-DUSE_NEXUS -lNeXus'
+CC = 'gcc'
+MPICC = 'mpicc'
+MPIRUN = 'mpirun'
+MPINODES = '4'
+
