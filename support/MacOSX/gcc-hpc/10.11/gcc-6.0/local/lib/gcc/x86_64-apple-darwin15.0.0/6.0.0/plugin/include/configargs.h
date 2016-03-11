2b3b5b0c980e91b5ec94b75f21ea16ffc6efda89
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-03-11 13:37:16 +0100

    gcc-6.0 from hpc.sourceforge.net - primarily for use on El Capitan

---
+++ support/MacOSX/gcc-hpc/10.11/gcc-6.0/local/lib/gcc/x86_64-apple-darwin15.0.0/6.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-6-20151213/configure --enable-languages=c++,fortran";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
