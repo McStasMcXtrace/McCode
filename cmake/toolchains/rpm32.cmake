45395f2614f7d3297d798e7eb357ff264ff5c302
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-04-06 21:23:24 +0200

    Changing back to i386 here...

--- cmake/toolchains/rpm32.cmake
+++ cmake/toolchains/rpm32.cmake
@@ -1,6 +1,6 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME "Linux")
-SET(ARCH   "i686")
+SET(ARCH   "i386")
 
 # Commands for running various external tools
 set(BROWSER "xdg-open")
