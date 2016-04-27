7867ec3678cf7ad0a4cc4830c0b5d607c4ee5a8f
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-04-27 21:12:27 +0200

    Multiple edits: a) Further visualisation-help (show neighbouring beamlines when available) b) Correct width and length of beamline inserts c) Allow to specify moderator height (needs logic to check that this is either 0.03 or 0.06 m)

--- mcstas-comps/share/butterfly-geometry.c
+++ mcstas-comps/share/butterfly-geometry.c
@@ -1,7 +1,7 @@
 /* MCDISPLAY-section for the ESS butterfly moderator */
 
 /* Define the positioning of the buttefly sketch */
-double dy = 0.015;
+double dy = yheight/2.0;
 
 /* Point sets for the butterfly */
 double butterfly_z[] = {-1.9922764e-01, -1.8484553e-01, -2.0252845e-01, -2.0795122e-01, -2.1054471e-01, -2.1030894e-01, -2.0889431e-01, -2.0535772e-01, -2.0134959e-01, -1.9639837e-01, -1.9026829e-01, -1.8390244e-01, -1.7565041e-01, -1.7093496e-01, -1.4617886e-01, -1.2873171e-01, -9.2658537e-02, -4.0552845e-02, -1.7682927e-02, -9.1951221e-03, -2.3577231e-03, 5.1869889e-03, 1.1788619e-02, 1.7918699e-02, 2.3105689e-02, 2.4991869e-02, 2.4756099e-02, 2.2162599e-02, 1.8154469e-02, 1.2731709e-02, 5.6585389e-03, -2.3577214e-04, 1.4146339e-02, -2.9707317e-02, 1.4146339e-02, -4.7154514e-04, 1.5325199e-02, 2.1691059e-02, 2.4991869e-02, 2.4991869e-02, 2.2634149e-02, 1.8154469e-02, 1.1552849e-02, 3.3008089e-03, -6.1300811e-03, -9.1951221e-03, -6.0593496e-02, -9.2658537e-02, -1.7541463e-01, -1.8508130e-01, -1.9309756e-01, -1.9899187e-01, -2.0182114e-01, -2.0582927e-01, -2.0913008e-01, -2.1078049e-01, -2.1007317e-01, -2.0630081e-01, -2.0229268e-01, -1.9828455e-01, -1.8484553e-01, -1.9922764e-01, -1.5584553e-01, -1.9922764e-01};
@@ -30,10 +30,10 @@ for (y0=-dy; y0<2*dy; y0+=2*dy) {
     rBz = r21*(butterfly_z[j+1]-cz) + r22*(butterfly_x[j+1]-cx);
 
     line(rAx, y0, rAz, rBx, y0, rBz);
-    //line(butterfly_x[j]-cx, y0, butterfly_z[j]-cz, butterfly_x[j+1]-cx, y0, butterfly_z[j+1]-cz);
 
   }
 }
+
 /* Arrow indicating proton beam direction */
 double ax,az,bx,bz,bbx,bbz,ccx,ccz;
 az = -0.0925-cz;
@@ -58,6 +58,7 @@ line(rAx, 0, rAz, rBx, 0, rBz);
 rBx = r11*(ccz) + r12*(ccx);
 rBz = r21*(ccz) + r22*(ccx);
 line(rAx, 0, rAz, rBx, 0, rBz);
+
 /* 120 degree "end of sector" lines */
 bbz = 2 * cos(DEG2RAD*60);
 bbx = 2 * sin(DEG2RAD*60);
@@ -84,15 +85,76 @@ dashed_line(rAx, 0, rAz, rBx, 0, rBz,51);
 circle("xz", rAx, 0, rAz, 2.0);
 circle("xz", rAx, -0.1, rAz, 2.0);
 circle("xz", rAx, 0.1, rAz, 2.0);
+
 /* Circles indicating extent of the target monolith */
 circle("xz", rAx, 0, rAz, 5.5);
 circle("xz", rAx, -1, rAz, 5.5);
 circle("xz", rAx, 1, rAz, 5.5);
-/* Beamport "plug" dimensions FIXME: Put the right dimensions in...*/
-dashed_line(0.04, 0, 2+rAz, 0.04, 0, 3+rAz, 11);
-dashed_line(-0.04, 0, 2+rAz, -0.04, 0, 3+rAz, 11);
-dashed_line(0.08, 0, 3+rAz, 0.08, 0, 5.5+rAz, 31);
-dashed_line(-0.08, 0, 3+rAz, -0.08, 0, 5.5+rAz, 31);
+
+/* Beamport "plug" dimensions */
+double w1=0.206/2.0, w2=0.276/2.0, l1=2.0+rAz, l2=2.0+rAz+1.75, l3=2.0+rAz+3.5;
+line(w1, 0, l1, w1, 0, l2);
+line(-w1, 0, l1, -w1, 0, l2);
+
+line(w2, 0, l2, w2, 0, l3);
+line(-w2, 0, l2, -w2, 0, l3);
+
+/* plus/minus 6 degree versions of the same... */
+double xx1, zz1, xx2, zz2;
+/* Is there room for a neighbouring beamline at "higher" angle? */
+r11 = cos(-DEG2RAD*6);
+r12 = -sin(-DEG2RAD*6);
+r21 = sin(-DEG2RAD*6);
+r22 = cos(-DEG2RAD*6);
+if (!((strcasestr(sector,"E") && beamline==1) || (strcasestr(sector,"W") && beamline==1))) {
+  xx1 = r11*(w1) + r12*(l1);
+  zz1 = r21*(w1) + r22*(l1);
+  xx2 = r11*(w1) + r12*(l2);
+  zz2 = r21*(w1) + r22*(l2);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  xx1 = r11*(-w1) + r12*(l1);
+  zz1 = r21*(-w1) + r22*(l1);
+  xx2 = r11*(-w1) + r12*(l2);
+  zz2 = r21*(-w1) + r22*(l2);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  xx1 = r11*(w2) + r12*(l2);
+  zz1 = r21*(w2) + r22*(l2);
+  xx2 = r11*(w2) + r12*(l3);
+  zz2 = r21*(w2) + r22*(l3);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  xx1 = r11*(-w2) + r12*(l2);
+  zz1 = r21*(-w2) + r22*(l2);
+  xx2 = r11*(-w2) + r12*(l3);
+  zz2 = r21*(-w2) + r22*(l3);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+}
+/* Is there room for a neighbouring beamline at "lower" angle? */
+r11 = cos(DEG2RAD*6);
+r12 = -sin(DEG2RAD*6);
+r21 = sin(DEG2RAD*6);
+r22 = cos(DEG2RAD*6);
+if (!((strcasestr(sector,"N") && beamline==1) || (strcasestr(sector,"S") && beamline==1))) {
+  xx1 = r11*(w1) + r12*(l1);
+  zz1 = r21*(w1) + r22*(l1);
+  xx2 = r11*(w1) + r12*(l2);
+  zz2 = r21*(w1) + r22*(l2);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  xx1 = r11*(-w1) + r12*(l1);
+  zz1 = r21*(-w1) + r22*(l1);
+  xx2 = r11*(-w1) + r12*(l2);
+  zz2 = r21*(-w1) + r22*(l2);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  xx1 = r11*(w2) + r12*(l2);
+  zz1 = r21*(w2) + r22*(l2);
+  xx2 = r11*(w2) + r12*(l3);
+  zz2 = r21*(w2) + r22*(l3);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  xx1 = r11*(-w2) + r12*(l2);
+  zz1 = r21*(-w2) + r22*(l2);
+  xx2 = r11*(-w2) + r12*(l3);
+  zz2 = r21*(-w2) + r22*(l3);
+  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+}
 
 /* Show instrument axis... */
 dashed_line(0,0,0,0,0,2+rAz,21);
