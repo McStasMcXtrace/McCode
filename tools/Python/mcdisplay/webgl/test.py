a919dd88aa1067dd7192535c2094be08a9dd62cf
jaga-mcstas <jaga@fysik.dtu.dk>
2016-05-10 15:08:40 +0200

    test script simplifications

--- tools/Python/mcdisplay/webgl/test.py
+++ tools/Python/mcdisplay/webgl/test.py
@@ -14,40 +14,27 @@ def main(args):
     logging.basicConfig(level=logging.INFO)
     
     # build trace parser and parse data
-    instr, display, rays, comments = cleanTrace(open(args.data, 'r').read())
+    instr_txt, display_txt, rays_txt, comments_txt = cleanTrace(open(args.data, 'r').read())
     
-    instrparser = TraceInstrParser(instr + display)
-    rayparser = TraceNeutronRayParser(rays)
+    instrparser = TraceInstrParser(instr_txt + display_txt)
+    rayparser = TraceNeutronRayParser(rays_txt)
     
     # print the parse tree as a test
     #treeprint = NodeTreePrint(instrparser.parsetree)
     #treeprint = NodeTreePrint(rayparser.parsetree)
     
-    # build the instrument object
+    # build the instrument and neutron ray objects
     instrbuilder = InstrObjectConstructor(instrparser.parsetree)
-    instrument = instrbuilder.build_instr()
-    
-    # build the neutron ray tree
     raybuilder = NeutronRayConstructor(rayparser.parsetree)
-    rays = raybuilder.build_rays()
     
-    instrument.rays = rays
+    instrument = instrbuilder.build_instr()
+    instrument.rays = raybuilder.build_rays()
     
     # build html
     writer = TemplateWebGLWrite(instrument, 'template.html')
     writer.build()
     writer.save('mymultilines.html')
-    
-    # step-wise trace parser test
-    if False: 
-        parser = TraceInstrParser()
-        parser.build_lexer()
-        #parser.test_lexer(data)
-        
-        parser.build_parser()
-        parser.parse(instr + display)
-        
-        NodeTreePrint(parser.parsetree, printrays=True)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
