79e3e6c75995157d004064f09626b5d1c0a19b41
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-05-18 13:03:37 +0200

    Corrected angular separation - uneven in W and S sectors

--- mcstas-comps/share/butterfly-geometry.c
+++ mcstas-comps/share/butterfly-geometry.c
@@ -130,12 +130,14 @@ line(-w2, 0, l2, -w2, 0, l3);
 
 /* plus/minus 6 degree versions of the same... */
 double xx1, zz1, xx2, zz2;
-/* Is there room for a neighbouring beamline at "higher" angle? */
-r11 = cos(-DEG2RAD*6);
-r12 = -sin(-DEG2RAD*6);
-r21 = sin(-DEG2RAD*6);
-r22 = cos(-DEG2RAD*6);
-if (!((strcasestr(sector,"E") && beamline==1) || (strcasestr(sector,"W") && beamline==1))) {
+/* One beamline at "lower"/"higher" beamline no. */
+double delta_omega;
+if (!(beamline == 1)) {
+  delta_omega = Beamlines[beamline-1] - Beamlines[beamline-2];
+  r11 = cos(DEG2RAD*delta_omega);
+  r12 = -sin(DEG2RAD*delta_omega);
+  r21 = sin(DEG2RAD*delta_omega);
+  r22 = cos(DEG2RAD*delta_omega);
   xx1 = r11*(w1) + r12*(l1);
   zz1 = r21*(w1) + r22*(l1);
   xx2 = r11*(w1) + r12*(l2);
@@ -157,33 +159,31 @@ if (!((strcasestr(sector,"E") && beamline==1) || (strcasestr(sector,"W") && beam
   zz2 = r21*(-w2) + r22*(l3);
   dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
 }
-/* Is there room for a neighbouring beamline at "lower" angle? */
-r11 = cos(DEG2RAD*6);
-r12 = -sin(DEG2RAD*6);
-r21 = sin(DEG2RAD*6);
-r22 = cos(DEG2RAD*6);
-if (!((strcasestr(sector,"N") && beamline==1) || (strcasestr(sector,"S") && beamline==1))) {
-  xx1 = r11*(w1) + r12*(l1);
-  zz1 = r21*(w1) + r22*(l1);
-  xx2 = r11*(w1) + r12*(l2);
-  zz2 = r21*(w1) + r22*(l2);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-  xx1 = r11*(-w1) + r12*(l1);
-  zz1 = r21*(-w1) + r22*(l1);
-  xx2 = r11*(-w1) + r12*(l2);
-  zz2 = r21*(-w1) + r22*(l2);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-  xx1 = r11*(w2) + r12*(l2);
-  zz1 = r21*(w2) + r22*(l2);
-  xx2 = r11*(w2) + r12*(l3);
-  zz2 = r21*(w2) + r22*(l3);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-  xx1 = r11*(-w2) + r12*(l2);
-  zz1 = r21*(-w2) + r22*(l2);
-  xx2 = r11*(-w2) + r12*(l3);
-  zz2 = r21*(-w2) + r22*(l3);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-}
+delta_omega = Beamlines[beamline-1] - Beamlines[beamline];
+r11 = cos(DEG2RAD*delta_omega);
+r12 = -sin(DEG2RAD*delta_omega);
+r21 = sin(DEG2RAD*delta_omega);
+r22 = cos(DEG2RAD*delta_omega);
+xx1 = r11*(w1) + r12*(l1);
+zz1 = r21*(w1) + r22*(l1);
+xx2 = r11*(w1) + r12*(l2);
+zz2 = r21*(w1) + r22*(l2);
+dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+xx1 = r11*(-w1) + r12*(l1);
+zz1 = r21*(-w1) + r22*(l1);
+xx2 = r11*(-w1) + r12*(l2);
+zz2 = r21*(-w1) + r22*(l2);
+dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+xx1 = r11*(w2) + r12*(l2);
+zz1 = r21*(w2) + r22*(l2);
+xx2 = r11*(w2) + r12*(l3);
+zz2 = r21*(w2) + r22*(l3);
+dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+xx1 = r11*(-w2) + r12*(l2);
+zz1 = r21*(-w2) + r22*(l2);
+xx2 = r11*(-w2) + r12*(l3);
+zz2 = r21*(-w2) + r22*(l3);
+dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
 
 /* Show instrument axis... */
 dashed_line(0,0,0,0,0,2+rAz,21);
