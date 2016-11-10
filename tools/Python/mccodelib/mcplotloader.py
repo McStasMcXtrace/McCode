6fd87bac8df6101112e991a2068c64c76ceaf576
jaga-mcstas <jaga@fysik.dtu.dk>
2016-11-10 18:24:59 +0100

    upgraded to python3

--- tools/Python/mccodelib/mcplotloader.py
+++ tools/Python/mccodelib/mcplotloader.py
@@ -1,18 +1,19 @@
 '''
-functionality for loading mccode data into suitable data types, 
+functionality for loading mccode data into suitable data types,
 and assembling it in a plot-friendly way.
 '''
 import glob
 import re
-from os.path import isfile, isdir, join, dirname, basename, walk, splitext, walk
+from os.path import isfile, isdir, join, dirname, basename, splitext
+from os import walk
 
 from .flowchart import *
 from .mcplotgraph import *
 
-''' 
+'''
 Flowchart decision functions.
 
-NOTE: These functions probe disk contents, but they do not open any files, so 
+NOTE: These functions probe disk contents, but they do not open any files, so
 all decisions are based on file exists, folder exists and filename.
 Any errors are therefore encountered only at the actual load data step.
 
@@ -52,7 +53,7 @@ def is_monitorfile(args):
 
 def is_sweepfolder(args):
     d = args['directory']
-    
+
     if not isdir(d):
         return False
     dotsim = join(d, 'mccode.sim')
@@ -97,7 +98,7 @@ def has_multiple_datfiles(args):
 def test_decfuncs(simfile):
     args = {}
     args['simfile'] = simfile
-    
+
     print('has_filename:              %s' % str(has_filename(args)))
     print('is_mccodesim_or_mccodedat: %s' % str(is_mccodesim_or_mccodedat(args)))
     print('is_monitorfile:            %s' % str(is_monitorfile(args)))
@@ -110,7 +111,7 @@ def test_decfuncs(simfile):
 
 
 '''
-Utility funcitons for loading and parsing mccode output data files 
+Utility funcitons for loading and parsing mccode output data files
 '''
 
 freetext_pat = '[\w \[\]\{\}\(\)\.\+\-\\\/\^]+'
@@ -118,7 +119,7 @@ freetext_pat = '[\w \[\]\{\}\(\)\.\+\-\\\/\^]+'
 def _parse_1D_monitor(text):
     ''' populates data fields of new Data1D object using the text from a mccode data file '''
     data = Data1D()
-    
+
     try:
         # load essential header data
         '''# component: Ldetector'''
@@ -136,14 +137,14 @@ def _parse_1D_monitor(text):
         '''# ylabel: Intensity'''
         m = re.search('\# ylabel: (%s)\n' % freetext_pat, text)
         data.ylabel = m.group(1)
-        
+
         '''# xvar: L'''
         m = re.search('\# xvar: ([\w]+)\n', text)
         data.xvar = m.group(1)
         '''# xlimits: 5.5 6.5'''
         m = re.search('\# xlimits: ([\d\.\-e]+) ([\d\.\-e]+)\n', text)
         data.xlimits = (float(m.group(1)), float(m.group(2)))
-        
+
         '''# yvar: (I,I_err)'''
         m = re.search('\# yvar: \(([\w]+),([\w]+)\)\n', text)
         data.yvar = (m.group(1), m.group(2))
@@ -153,8 +154,8 @@ def _parse_1D_monitor(text):
         '''# statistics: X0=5.99569; dX=0.0266368;'''
         m = re.search('\# statistics: X0=([\d\.\-e]+); dX=([\d\.\-e]+);\n', text)
         data.statistics = 'X0=%f; dX=%f;' % (float(m.group(1)), float(m.group(2)))
-        
-        # load the actual data 
+
+        # load the actual data
         lines = text.splitlines()
         xvals = []
         yvals = []
@@ -163,27 +164,27 @@ def _parse_1D_monitor(text):
         for l in lines:
             if '#' in l:
                 continue
-            
+
             vals = l.split(' ')
             xvals.append(float(vals[0]))
             yvals.append(float(vals[1]))
             y_err_vals.append(float(vals[2]))
             Nvals.append(float(vals[3]))
-        
+
         data.xvals = xvals
         data.yvals = yvals
         data.y_err_vals = y_err_vals
         data.Nvals = Nvals
-        
+
     except Exception as e:
         print('Data1D load error.')
         raise e
-    
+
     return data
 
 def _parse_2D_monitor(text):
     data = Data2D()
-    
+
     ''' populates data fields using the text from a mccode data file '''
     try:
         # load essential header data
@@ -196,14 +197,14 @@ def _parse_2D_monitor(text):
         '''# title: PSD monitor'''
         m = re.search('\# title: (%s)\n' % freetext_pat, text)
         data.title = m.group(1)
-        
+
         '''# xlabel: X position [cm]'''
         m = re.search('\# xlabel: (%s)\n' % freetext_pat, text)
         data.xlabel = m.group(1)
         '''# ylabel: Y position [cm]'''
         m = re.search('\# ylabel: (%s)\n' % freetext_pat, text)
         data.ylabel = m.group(1)
-        
+
         '''# xvar: X'''
         m = re.search('\# xvar: ([\w ]+)\n', text)
         data.xvar = m.group(1)
@@ -216,7 +217,7 @@ def _parse_2D_monitor(text):
         '''# xylimits: -30 30 -30 30'''
         m = re.search('\# xylimits: ([\d\.\-e]+) ([\d\.\-e]+) ([\d\.\-e]+) ([\d\.\-e]+)\n', text)
         data.xlimits = (float(m.group(1)), float(m.group(2)), float(m.group(3)), float(m.group(4)))
-        
+
         '''# values: 6.72365e-17 4.07766e-18 4750'''
         m = re.search('\# values: ([\d\+\-\.e]+) ([\d\+\-\.e]+) ([\d]+)\n', text)
         data.values = (float(m.group(1)), float(m.group(2)), float(m.group(3)))
@@ -226,7 +227,7 @@ def _parse_2D_monitor(text):
         '''# signal: Min=0; Max=1.20439e-18; Mean=4.10394e-21;'''
         m = re.search('\# signal: Min=([\d\.\+\-e]+); Max=([\d\.\+\-e]+); Mean=([\d\.\+\-e]+);\n', text)
         data.signal = 'Min=%f; Max=%f; Mean=%f;' % (float(m.group(1)), float(m.group(2)), float(m.group(3)))
-        
+
         '''# Data [detector/PSD.dat] I:'''
         '''# Events [detector/PSD.dat] N:'''
         lines = text.splitlines()
@@ -236,18 +237,18 @@ def _parse_2D_monitor(text):
             if '# Data ' in l:
                 dat = True
                 continue
-        
+
             if '# Events ' in l:
                 dat = False
                 events = True
                 continue
-        
+
             if '# Errors ' in l:
                 # NOTE: error values are not loaded
                 dat = False
                 events = False
                 continue
-            
+
             if dat:
                 try:
                     vals = [float(item) for item in l.strip().split(' ')]
@@ -260,11 +261,11 @@ def _parse_2D_monitor(text):
                     data.counts.append(vals)
                 except:
                     pass
-    
+
     except Exception as e:
         print('Data2D load error.')
         raise e
-    
+
     return data
 
 def _parse_header(text):
@@ -275,7 +276,7 @@ def _load_monitor(monitorfile):
     f = monitorfile
     text = open(f).read()
     # determine 1D / 2D data
-    
+
     m = re.search('\# type: (array_\wd)', text)
     typ = m.group(1)
     if typ == 'array_1d':
@@ -284,10 +285,10 @@ def _load_monitor(monitorfile):
         data = _parse_2D_monitor(text)
     else:
         raise Exception('load_monitor: unknown data format.')
-    
+
     if False:
         print(data.get_stats_title())
-    
+
     return data
 
 def _load_datfiles(directory):
@@ -311,12 +312,12 @@ def _load_sweep_header(simfile):
 
 def _load_multiplot_1D_lst(f_dat):
     '''
-    loads the one-dimensional 'multiplot' data sets from a mccode.dat scan sweep file, 
+    loads the one-dimensional 'multiplot' data sets from a mccode.dat scan sweep file,
     corresponding to each monitor as a function of the sweep parameter.
     '''
     text = open(f_dat).read()
     data_lst = []
-    
+
     try:
         header = Data1D()
         header.component = ''
@@ -330,7 +331,7 @@ def _load_multiplot_1D_lst(f_dat):
         '''# ylabel: 'Intensity\''''
         m = re.search('\# ylabel: ([\w \[\]\/\^\']+)\n', text)
         header.ylabel = m.group(1)
-        
+
         # NOTE: this only supports a single xvar
         '''# xvars: lambda'''
         m = re.search('\# xvars: ([\w]+)\n', text)
@@ -338,11 +339,11 @@ def _load_multiplot_1D_lst(f_dat):
         '''# xlimits: 6 7'''
         m = re.search('\# xlimits: ([\d\.\-e]+) ([\d\.\-e]+)\n', text)
         header.xlimits = (float(m.group(1)), float(m.group(2)))
-        
+
         '''# variables: lambda Ldetector_I Ldetector_ERR PSDrad_I PSDrad_ERR PSDrad_I PSDrad_ERR detector_I detector_ERR'''
         m = re.search('\# variables: ([\w ]+)\n', text)
         variables = m.group(1).split(' ')
-        
+
         # get x and y values (the latter is a list of a list, the infamous yvals_lst which contains yvals values, which are lists)
         lines = text.splitlines()
         xvals = []
@@ -355,27 +356,27 @@ def _load_multiplot_1D_lst(f_dat):
             if '#' in l:
                 continue
             xvals.append(l.split(' ')[0])
-            
-            for i in range(len(yvariables)/2):
+
+            for i in range(len(yvariables)//2):
                 yvals_lst.append([])
                 yvals_err_lst.append([])
                 yvals_lst[i].append(l.split(' ')[2*i+1])
                 yvals_err_lst[i].append(l.split(' ')[2*i+2])
         header.xvals = xvals
-        
+
         # create a new instance for each y variable
-        for i in range(len(yvariables)/2):
+        for i in range(len(yvariables)//2):
             data = header.clone()
             data.yvals = yvals_lst[i]
             data.yvar = yvariables[2*i]
             data.title = '%s - %s' % (data.yvar, data.title)
             data.y_err_vals = yvals_err_lst[i]
             data_lst.append(data)
-        
+
     except Exception as e:
-        print(e.message)
+        print(e.__str__())
         raise e
-    
+
     return data_lst
 
 def _load_sweep_monitors(rootdir):
@@ -387,25 +388,28 @@ def _load_sweep_monitors(rootdir):
                                        int(item.partition(' ')[0])
                                        if item[0].isdigit() else float('inf'), item)
                )
-    
+
     def walkfunc(arg, dirname, fnames):
         mnames = []
         dirsignature = (dirname, mnames)
         for f in fnames:
             if splitext(f)[1] == '.dat':
-                if f not in mnames: 
+                if f not in mnames:
                     mnames.append(f)
         arg.append(dirsignature)
-    
+
     d = rootdir
     subdirtuple = []
-    walk(top=d, func=walkfunc, arg=subdirtuple)
+    for root, dirs, files in walk(top=d):
+        walkfunc(subdirtuple, root, files)
+
+
     del subdirtuple[0] # remove root dir
-    
+
     subdirs = map(lambda t: t[0], subdirtuple)
     subdirs = sortalpha(subdirs)
-    monitors = map(lambda t: t[1], subdirtuple)[0]
-    
+    monitors = list(map(lambda t: t[1], subdirtuple))[0]
+
     # assemble monitor data
     sweep_monitors = []
     for m in monitors:
@@ -413,17 +417,17 @@ def _load_sweep_monitors(rootdir):
         for s in subdirs:
             mon_lst.append(_load_monitor(join(s, m)))
         sweep_monitors.append(mon_lst)
-    
+
     sweep_headers = []
     for m_lst in sweep_monitors:
         header = DataMultiHeader()
         header.title = m_lst[0].title
         sweep_headers.append(header)
-    
+
     return sweep_monitors, sweep_headers
 
 '''
-Terminal load data functions. 
+Terminal load data functions.
 
 Loads data files, then assembles and returns a data graph, which can be plotted.
 '''
@@ -433,7 +437,7 @@ def load_monitor(args):
 
     # plot graph only has one node in this case
     root = PNSingle(data)
-    
+
     return root
 
 def load_simulation(args):
@@ -444,7 +448,7 @@ def load_simulation(args):
     # load header and monitor data
     header = _load_header(f)
     data_lst = _load_datfiles(d)
-    
+
     # construct two-level plot graph
     root = PNMultiple(header, data_lst)
     primnodes = []
@@ -453,14 +457,14 @@ def load_simulation(args):
         primnodes.append(node)
     root.set_primaries(primnodes)
     root.set_secondaries(primnodes) # there is only one way to click here...could also be None
-    
+
     return root
 
 def load_sweep(args):
     d = args['directory']
     f_sim = join(d, 'mccode.sim')
     f_dat = join(d, 'mccode.dat')
-    
+
     # load primary data, 1D sweep values
     datalst_sweep1D = _load_multiplot_1D_lst(f_dat)
     header = _load_sweep_header(f_sim)
@@ -472,10 +476,10 @@ def load_sweep(args):
         primnode = PNSingle(data)
         primnodes_lst.append(primnode)
     root.set_primaries(primnodes_lst)
-    
+
     # load secondary data, sweep data points organized by monitor
     monitors, headers = _load_sweep_monitors(d)
-    
+
     # secondary modes (individual sweep monitors)
     secnodes_lst = []
     for i in range(len(headers)):
@@ -490,9 +494,9 @@ def load_sweep(args):
         secnode.set_secondaries(children)
         secnodes_lst.append(secnode)
     root.set_secondaries(secnodes_lst)
-    
+
     return root
-    
+
 def load_sweep_b(args):
     raise Exception('load_sweep_b is not implemented.')
 
@@ -502,10 +506,10 @@ def load_sweep_c(args):
 def load_monitor_folder(args):
     # assume simfile is folder with multiple dat files
     d = args['simfile']
-    
-    # load monitor files into a list 
+
+    # load monitor files into a list
     data_lst = _load_datfiles(d)
-    
+
     # construct two-level plot graph
     root = PNMultiple(DataMultiHeader(), data_lst)
     primnodes = []
@@ -514,7 +518,7 @@ def load_monitor_folder(args):
         primnodes.append(node)
     root.set_primaries(primnodes)
     root.set_secondaries(primnodes)
-    
+
     return root
 
 def throw_error(args):
@@ -526,10 +530,10 @@ class McPlotDataLoader():
         '''  '''
         self.simfile = simfile
         self.plot_graph = None
-    
+
     def load(self):
         ''' loads mccode data and assembles the plotable data graph '''
-        
+
         # exit terminals
         exit_error                  = FCNTerminal(key="error",  fct=throw_error)
         exit_case1                  = FCNTerminal(key="case1",  fct=load_monitor)
@@ -550,14 +554,13 @@ class McPlotDataLoader():
         dec_hasfilename             = FCNDecisionBool(fct=has_filename,             node_T=dec_ismccodesimordat,    node_F=dec_issweepfolder)
         # enter terminal node
         enter_simfile               = FCNTerminal(key='enter', node_next=dec_hasfilename)
-        
+
         # get the "args" which is just the simfile, a string, which may correspond to a file or a folder
         args = {}
         args['simfile'] = self.simfile
-        
+
         # traverse the flow chart
         control = FlowChartControl(terminal_enter=enter_simfile)
         exit_node = control.process(args=args)
-        
-        self.plot_graph = exit_node.result
 
+        self.plot_graph = exit_node.result
