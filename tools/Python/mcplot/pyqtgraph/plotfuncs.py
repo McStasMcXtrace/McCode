32a13a0327d2db105ea0581440ee16e86858922e
jaga-mcstas <jaga@fysik.dtu.dk>
2016-11-10 14:01:40 +0100

    labels and titles to 1D and 2D plots

--- tools/Python/mcplot/pyqtgraph/plotfuncs.py
+++ tools/Python/mcplot/pyqtgraph/plotfuncs.py
@@ -6,17 +6,26 @@ import pyqtgraph as pg
 
 def plot_Data1D(data, plt):
     ''' populate plt with data, 1D version '''
+    # data
     x = np.array(data.xvals).astype(np.float)
     y = np.array(data.yvals).astype(np.float)
-    plt.setWindowTitle(data.title)
-    #height = np.array(data1.y_err_vals)
-    #err = pg.ErrorBarItem(x=x, y=y, height=height, beam=0.02)
-    #p1.addItem(err)
+    
+    # title
+    plt.setLabels(title=data.title, bottom=data.xlabel, left=data.ylabel)
+    # error bars
+    beam = 0
+    if len(x) > 1:
+        beam = x[1]-x[0]/0.8
+    height = np.array(data.y_err_vals)
+    err = pg.ErrorBarItem(x=x, y=y, height=height, beam=beam)
+    plt.addItem(err)
+    
     plt.plot(x, y)
 
 def plot_Data2D(data, plt):
     ''' populate plt with data, 2D version '''
     img = pg.ImageItem()
     plt.addItem(img)
+    plt.setLabels(title=data.title, bottom=data.xlabel, left=data.ylabel)
     img.setImage(np.array(data.zvals))
 
