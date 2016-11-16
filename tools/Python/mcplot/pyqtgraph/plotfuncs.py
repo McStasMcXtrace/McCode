5d60ccca5ffd6269fb2a02f2b6eb50c95d1f1800
jaga-mcstas <jaga@fysik.dtu.dk>
2016-11-16 15:21:43 +0100

    initial color map application to image data

--- tools/Python/mcplot/pyqtgraph/plotfuncs.py
+++ tools/Python/mcplot/pyqtgraph/plotfuncs.py
@@ -30,8 +30,19 @@ def plot_Data2D(data, plt):
     img = pg.ImageItem()
     img.setImage(np.array(data.zvals))
     
+    # color map (by lookup table)
+    pos_min = np.min(data.zvals)
+    pos_max = np.max(data.zvals)
+    pos = [pos_min, pos_min + 1/2*(pos_max-pos_min), pos_max]
+    
+    color = np.array([[0, 0, 0, 255], [255, 128, 0, 255], [255, 255, 0, 255]], dtype=np.ubyte)
+    
+    cm = pg.ColorMap(pos, color)
+    lut = cm.getLookupTable(pos_min, pos_max, 256)
+    img.setLookupTable(lut=lut)
+    
     # labels
     plt.setLabels(title=data.title, bottom=data.xlabel, left=data.ylabel)
-
+    
     # commit
     plt.addItem(img)
