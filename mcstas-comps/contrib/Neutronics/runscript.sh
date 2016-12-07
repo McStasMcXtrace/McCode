5035e856c9810d09e1b8f63a93e1d9bf53797a0f
Peter Willendrup <pkwi@fysik.dtu.dk>
2016-12-07 14:28:49 +0100

    Make naming slightly more specific. Should be installed as binary lib in MCCODE/libs/neutronics

--- mcstas-comps/contrib/Neutronics/runscript.sh
+++ mcstas-comps/contrib/Neutronics/runscript.sh
@@ -1,5 +1,5 @@
 rm ptrak
 rm wssa 
-gfortran -c subs.f
+gfortran -c neutronics-subs.f
 mcstas -t test_SSR_SSW.instr &&  
-gcc -o runme.out test_SSR_SSW.c subs.o -lm -lgfortran --trace &&  ./runme.out
+gcc -o runme.out test_SSR_SSW.c neutronics-subs.o -lm -lgfortran --trace &&  ./runme.out
