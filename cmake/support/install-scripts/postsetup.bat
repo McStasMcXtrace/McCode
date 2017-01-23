f35737afab04b69027abb27dbb7e17b8d3f17c92
Peter Willendrup <pkwi@fysik.dtu.dk>
2017-01-23 14:00:25 +0100

    Install conda py requirements

--- cmake/support/install-scripts/postsetup.bat
+++ cmake/support/install-scripts/postsetup.bat
@@ -1,5 +1,7 @@
-@REM Script for final installation of McStas-related Perl modules
+@REM Script for final installation of McStas/McXtrace-related Python and Perl modules
 @REM
+@conda config --add channels conda-forge
+@conda install qscintilla2 pyqtgraph pyaml
 @set PATH=c:\\strawberry\\perl\\bin;%PATH%
 @call c:\\strawberry\\perl\\bin\\ppm.bat install http://ppds.mccode.org/Astro-FITS-Header.ppd
 @call c:\\strawberry\\perl\\bin\\ppm.bat install http://ppds.mccode.org/Convert-UU.ppd
