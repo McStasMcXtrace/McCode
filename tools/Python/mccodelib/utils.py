876bd87f53658c819e810a35dc99fb51a01766e3
jaga-mcstas <jaga@fysik.dtu.dk>
2017-05-04 11:50:32 +0200

    include subfolders of contrib into the overview page

--- tools/Python/mccodelib/utils.py
+++ tools/Python/mccodelib/utils.py
@@ -489,6 +489,11 @@ def get_comp_category(filepath):
     ''' extract first folder name from file path '''
     head = os.path.split(filepath)[0]
     firstdir = os.path.split(head)[1]
+    # handle special case - sub folder of "contrib" folder
+    if firstdir not in ('sources', 'optics', 'samples', 'monitors', 'misc', 'contrib', 'obsolete'):
+        head2 = os.path.split(head)[0]
+        seconddir = os.path.split(head2)[1]
+        return seconddir
     return firstdir
 
 def parse_define_comp(text):
