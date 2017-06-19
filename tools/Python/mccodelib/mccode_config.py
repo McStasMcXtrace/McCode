5481b11a36b5285426bc75c96b76cb5034ae2140
Peter Willendrup <pkwi@fysik.dtu.dk>
2017-06-19 14:51:22 +0200

    2.3 -> 2.4 in default config

--- tools/Python/mccodelib/mccode_config.py
+++ tools/Python/mccodelib/mccode_config.py
@@ -5,8 +5,8 @@ import json
 mcstas/mcxtrace configuration.
 '''
 configuration = {
-    "MCCODE_VERSION": '2.3',
-    "MCCODE_LIB_DIR": '/usr/share/mcstas/2.3/',
+    "MCCODE_VERSION": '2.4',
+    "MCCODE_LIB_DIR": '/usr/share/mcstas/2.4/',
     "MCCODE": 'mcstas',
     "MCRUN": 'mcrun',
     "MCPLOT": 'mcplot-pyqtgraph',
