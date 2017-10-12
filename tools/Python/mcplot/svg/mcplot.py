742cf681f0daa4f079736e21d776dd73a1f31252
Jakob Garde <jaga@fysik.dtu.dk>
2017-10-12 14:48:18 +0200

    json-parametrize 2d plot data

--- tools/Python/mcplot/svg/mcplot.py
+++ tools/Python/mcplot/svg/mcplot.py
@@ -15,6 +15,7 @@ from mccodelib.mcplotloader import McCodeDataLoader, Data1D, Data2D
 from mccodelib.plotgraph import PNSingle
 
 def get_html(template_name, params):
+    '''  '''
     text = open(template_name).read()
     text = text.replace("@PARAMS@", params)
     return text
@@ -34,23 +35,26 @@ def get_json_1d(x, y, yerr, xlabel, ylabel, title):
     
     return json.dumps(params, indent=4)
 
-def fillout_template_2d(template, xmin, xmax, ymin, ymax, image_str, xlabel, ylabel, title):
+def get_json_2d(xmin, xmax, ymin, ymax, image_str, xlabel, ylabel, title):
     '''  '''
-    template = template.replace('@XMIN@', str(xmin))
-    template = template.replace('@XMAX@', str(xmax))
-    template = template.replace('@YMIN@', str(ymin))
-    template = template.replace('@YMAX@', str(ymax))
-
-    template = template.replace('@IMAGE@', image_str)
+    params = {}
+    p = params
     
-    template = template.replace('@WIDTH@', str(500))
-    template = template.replace('@HEIGHT@', str(300))
+    p['w'] = 500
+    p['h'] = 300
     
-    template = template.replace('@XLABEL@', xlabel)
-    template = template.replace('@YLABEL@', ylabel)
-    template = template.replace('@TITLE@', title)
+    p['xmin'] = xmin
+    p['xmax'] = xmax
+    p['ymin'] = ymin
+    p['ymax'] = ymax
     
-    return template
+    p['img2dData'] = image_str
+    
+    p['xlabel'] = xlabel
+    p['ylabel'] = ylabel
+    p['title'] = title
+    
+    return json.dumps(params, indent=4)
 
 def get_cm():
     return np.array([[  0,   0, 143, 255], [  0,   0, 159, 255], [  0,   0, 175, 255], [  0,   0, 191, 255], [  0,   0, 207, 255], [  0,   0, 223, 255], [  0,   0, 239, 255], [  0,   0, 255, 255], [  0,  16, 255, 255], [  0,  32, 255, 255], [  0,  48, 255, 255], [  0,  64, 255, 255], [  0,  80, 255, 255], [  0,  96, 255, 255], [  0, 112, 255, 255], [  0, 128, 255, 255], [  0, 143, 255, 255], [  0, 159, 255, 255], [  0, 175, 255, 255], [  0, 191, 255, 255], [  0, 207, 255, 255], [  0, 223, 255, 255], [  0, 239, 255, 255], [  0, 255, 255, 255], [ 16, 255, 239, 255], [ 32, 255, 223, 255], [ 48, 255, 207, 255], [ 64, 255, 191, 255], [ 80, 255, 175, 255], [ 96, 255, 159, 255], [112, 255, 143, 255], [128, 255, 128, 255], [143, 255, 112, 255], [159, 255,  96, 255], [175, 255,  80, 255], [191, 255,  64, 255], [207, 255,  48, 255], [223, 255,  32, 255], [239, 255,  16, 255], [255, 255,   0, 255], [255, 239,   0, 255], [255, 223,   0, 255], [255, 207,   0, 255], [255, 191,   0, 255], [255, 175,   0, 255], [255, 159,   0, 255], [255, 143,   0, 255], [255, 128,   0, 255], [255, 112,   0, 255], [255,  96,   0, 255], [255,  80,   0, 255], [255,  64,   0, 255], [255,  48,   0, 255], [255,  32,   0, 255], [255,  16,   0, 255], [255,   0,   0, 255], [239,   0,   0, 255], [223,   0,   0, 255], [207,   0,   0, 255], [191,   0,   0, 255], [175,   0,   0, 255], [159,   0,   0, 255], [143,   0,   0, 255], [128,   0,   0, 255]], dtype=np.ubyte)
@@ -115,20 +119,18 @@ def main(args):
             contents = output.getvalue()
             output.close()
             
-            encoded = str(base64.b64encode(contents)).lstrip('b')
-            #print(encoded)
+            encoded = str(base64.b64encode(contents)).lstrip('b').strip("\'")
 
             xmin = data.xlimits[0]
             xmax = data.xlimits[1]
             ymin = data.xlimits[2]
             ymax = data.xlimits[3]
 
-            text = fillout_template_2d(open('template_2d.html').read(), xmin, xmax, ymin, ymax, encoded, data.xlabel, data.ylabel, data.title)
+            json_str = get_json_2d(xmin, xmax, ymin, ymax, encoded, data.xlabel, data.ylabel, data.title)
+            text = get_html('template_2d.html', json_str)
             for l in text.splitlines():
                 print(l)
             print("")
-            
-            #scipy.misc.imsave('outimage.png', img)
             
         else:
             print("can only plot 1D data right now")
