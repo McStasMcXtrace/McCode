cf3b440ede4b9be080ffe1da0d1c138dd17a13a9
Peter Willendrup <pkwi@fysik.dtu.dk>
2018-01-23 16:50:25 +0100

    Add support for scan plots and overviews - via --index=value

--- tools/Python/mcplot/svg/mcplot.py
+++ tools/Python/mcplot/svg/mcplot.py
@@ -13,7 +13,7 @@ import subprocess
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from mccodelib.mcplotloader import McCodeDataLoader, Data1D, Data2D
-from mccodelib.plotgraph import PNSingle
+from mccodelib.plotgraph import PNSingle, PNMultiple
 from mccodelib import mccode_config
 from shutil import copyfile
 
@@ -32,10 +32,11 @@ def path_base_name(path):
     file_name = os.path.basename(path)
     return file_base_name(file_name)
 
-def get_html(template_name, params):
+def get_html(template_name, params, simfile):
     '''  '''
     text = open(os.path.join(os.path.dirname(__file__),template_name)).read()
     text = text.replace("@PARAMS@", params)
+    text = text.replace("@DATAFILE@", simfile)
     return text
 
 def get_json_1d(x, y, yerr, xlabel, ylabel, title):
@@ -91,6 +92,13 @@ def lookup(cm, x):
     #return np.add(cm[f], (xp-f)*(np.subtract(cm[c], cm[f])) ).astype(np.ubyte)
     return cm[np.int(np.round(xp))]
 
+def browse(html_filepath):
+    # open a web-browser in a cross-platform way
+    try:
+        subprocess.Popen('%s %s' % (mccode_config.configuration['BROWSER'], html_filepath), shell=True)
+    except Exception as e:
+        raise Exception('Os-specific open browser: %s' % e.__str__())
+    
 def main(args):
     logging.basicConfig(level=logging.INFO)
     
@@ -104,18 +112,19 @@ def main(args):
     simbase = path_base_name(simfile)
 
     # Store system stdout for later restoring
-    orig_stdout = sys.stdout
+    f = sys.stdout
     html_filepath=''
-    
     # If simfile includes a directory name, dump our js code there 
-    if os.path.isdir(simdir):
-        copyfile(os.path.join(os.path.dirname(__file__),'d3.v4.min.js'), os.path.join(simdir,'d3.v4.min.js'))
-        copyfile(os.path.join(os.path.dirname(__file__),'plotfuncs.js'), os.path.join(simdir,'plotfuncs.js'))
-        html_filepath=os.path.join(simdir,simbase + '.html')
-        f = open(html_filepath, 'w')
-        sys.stdout = f
-             
-        
+    if not(args.nohtml): 
+        if os.path.isdir(simdir):
+            copyfile(os.path.join(os.path.dirname(__file__),'d3.v4.min.js'), os.path.join(simdir,'d3.v4.min.js'))
+            copyfile(os.path.join(os.path.dirname(__file__),'plotfuncs.js'), os.path.join(simdir,'plotfuncs.js'))
+            if not args.index is None:
+                html_filepath=os.path.join(simdir,simbase + '_' + str(args.index) + '.html')
+            else:
+                html_filepath=os.path.join(simdir,simbase + '.html')
+            f = open(html_filepath, 'w')
+            
     # load data
     loader = McCodeDataLoader(simfile=simfile)
     try:
@@ -125,118 +134,126 @@ def main(args):
         quit()
     node = loader.plot_graph
 
+    data=None
+    
     # write data to an html file
     if type(node) is PNSingle:
-        print("\n\n")
+        f.write("\n\n")
         data = node.getdata_idx(0)
-        if type(data) is Data1D:
-            
-            x = data.xvals
-            y = data.yvals
-            yerr = data.y_err_vals
-            
-            try:
-                title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
-            except:
-                title = '%s\n[%s]' % (data.component, data.filename)
-            
-            params_str = get_json_1d(x, y, yerr, data.xlabel, data.ylabel, title)
-            text = get_html('template_1d.html', params_str)
-            for l in text.splitlines():
-                print(l)
-            print("")
-            sys.stdout = orig_stdout 
-            # exit if nobrowse flag has been set
-            if args.nobrowse:
-                return
-    
-            # open a web-browser in a cross-platform way
-            try:
-                subprocess.Popen('%s %s' % (mccode_config.configuration['BROWSER'], html_filepath), shell=True)
-            except Exception as e:
-                raise Exception('Os-specific open browser: %s' % e.__str__())
-                        
-        elif type(data) is Data2D:
-            
-            vals = np.array(data.zvals)
-            dims = np.shape(vals)
-            
-            # create the 2d data as a png given our colormap
-            img = np.zeros((dims[0], dims[1], 4))
-            maxval = np.max(vals)
-            cm = get_cm()
-            for i in range(dims[0]):
-                for j in range(dims[1]):
-                    color = lookup(cm, vals[i,j]/maxval)
-                    img[i,j,:] = color
-            
-            # encode png as base64 string
-            image = scipy.misc.toimage(img)
-            output = io.BytesIO()
-            image.save(output, format="png")
-            contents = output.getvalue()
-            output.close()
-            encoded_2d_data = str(base64.b64encode(contents)).lstrip('b').strip("\'")
-            
-            # crate colormap 1 x 256 image
-            img = np.zeros((256, 1, 4))
-            for i in range(256):
-                color = lookup(cm, i/255)
-                img[255-i, 0] = color
-            
-            # e3ncode cm image
-            cb_img = scipy.misc.toimage(img)
-            output = io.BytesIO()
-            
-            cb_img.save(output, format='png')
-            contents = output.getvalue()
-            output.close()
-            encoded_cb = str(base64.b64encode(contents)).lstrip('b').strip("\'")
-
-            xmin = data.xlimits[0]
-            xmax = data.xlimits[1]
-            ymin = data.xlimits[2]
-            ymax = data.xlimits[3]
-            
-            cb_min = np.min(vals)
-            cb_max = np.max(vals)
-            
-            # generate the title
-            verbose = True
-            try:
-                title = '%s\nI = %s' % (data.component, data.values[0])
-                if verbose:
-                    title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
-            except:
-                title = '%s\n[%s]' % (data.component, data.filename)
-            
-            json_str = get_json_2d(xmin, xmax, ymin, ymax, encoded_2d_data, encoded_cb, cb_min, cb_max, data.xlabel, data.ylabel, title)
-            text = get_html('template_2d.html', json_str)
-            
-            for l in text.splitlines():
-                print(l)
-            print("")
-            sys.stdout = orig_stdout 
-            # exit if nobrowse flag has been set
-            if args.nobrowse:
-                return
-    
-            # open a web-browser in a cross-platform way
-            try:
-                subprocess.Popen('%s %s' % (mccode_config.configuration['BROWSER'], html_filepath), shell=True)
-            except Exception as e:
-                raise Exception('Os-specific open browser: %s' % e.__str__())
-            
+    elif type(node) is PNMultiple:
+        if args.index is None:
+            print("Can only plot a scan file if an index is provided!")
+            return
         else:
-            print("can only plot 1D data right now")
-    else:
+            data = node.getdata_idx(args.index)
+    else: 
         print("can only plot single data files")
 
+    if type(data) is Data1D:
+    
+        x = data.xvals
+        y = data.yvals
+        yerr = data.y_err_vals
+        
+        try:
+            title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
+        except:
+            title = '%s\n[%s]' % (data.component, data.filename)
+            
+        params_str = get_json_1d(x, y, yerr, data.xlabel, data.ylabel, title)
+        text = get_html('template_1d.html', params_str, os.path.basename(data.filename))
+        for l in text.splitlines():
+            f.write(l)
+            f.write("")
+        if not(args.nohtml):
+            f.close()
+                
+        # exit if nobrowse flag has been set
+        if args.nobrowse:
+            return
+        else:
+            browse(html_filepath)
+                        
+    elif type(data) is Data2D:
+        
+        vals = np.array(data.zvals)
+        dims = np.shape(vals)
+        
+        # create the 2d data as a png given our colormap
+        img = np.zeros((dims[0], dims[1], 4))
+        maxval = np.max(vals)
+        cm = get_cm()
+        for i in range(dims[0]):
+            for j in range(dims[1]):
+                color = lookup(cm, vals[i,j]/maxval)
+                img[i,j,:] = color
+            
+        # encode png as base64 string
+        image = scipy.misc.toimage(img)
+        output = io.BytesIO()
+        image.save(output, format="png")
+        contents = output.getvalue()
+        output.close()
+        encoded_2d_data = str(base64.b64encode(contents)).lstrip('b').strip("\'")
+            
+        # crate colormap 1 x 256 image
+        img = np.zeros((256, 1, 4))
+        for i in range(256):
+            color = lookup(cm, i/255)
+            img[255-i, 0] = color
+            
+        # e3ncode cm image
+        cb_img = scipy.misc.toimage(img)
+        output = io.BytesIO()
+        
+        cb_img.save(output, format='png')
+        contents = output.getvalue()
+        output.close()
+        encoded_cb = str(base64.b64encode(contents)).lstrip('b').strip("\'")
+
+        xmin = data.xlimits[0]
+        xmax = data.xlimits[1]
+        ymin = data.xlimits[2]
+        ymax = data.xlimits[3]
+            
+        cb_min = np.min(vals)
+        cb_max = np.max(vals)
+            
+        # generate the title
+        verbose = True
+        try:
+            title = '%s\nI = %s' % (data.component, data.values[0])
+            if verbose:
+                title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
+        except:
+            title = '%s\n[%s]' % (data.component, data.filename)
+            
+        json_str = get_json_2d(xmin, xmax, ymin, ymax, encoded_2d_data, encoded_cb, cb_min, cb_max, data.xlabel, data.ylabel, title)
+        text = get_html('template_2d.html', json_str, os.path.basename(data.filename))
+            
+        for l in text.splitlines():
+            f.write(l)
+        f.write("")
+        if not(args.nohtml):
+            f.close()
+                
+        # exit if nobrowse flag has been set
+        if args.nobrowse:
+            return
+        else:
+            browse(html_filepath)
+    else:
+        print("can only plot 1D/2D data right now - file " + simfile + " is of type " + type(data))
+    
+        
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('simulation', nargs='*', help='file or directory to plot')
+    parser.add_argument('--index', dest='index', type=int, help='Pick nth column from scan file')
     parser.add_argument('--nobrowse', action='store_true', help='do not open a webbrowser viewer')
-
+    parser.add_argument('--nohtml', action='store_true', help='do not save html file')
+    
     args = parser.parse_args()
     
     main(args)
