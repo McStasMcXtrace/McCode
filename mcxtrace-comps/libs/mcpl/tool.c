c394f8467d9eb951232143aef490a7742bc66e2d
Peter Willendrup <pkwi@fysik.dtu.dk>
2018-02-01 14:46:19 +0100

    Also let McXtrace pull directly from mcpl submodule, add mcpl2ssw/ssw2mcpl

--- mcxtrace-comps/libs/mcpl/tool.c
+++ mcxtrace-comps/libs/mcpl/tool.c
@@ -1,16 +1,1 @@
-/////////////////////////////////////////////////////////////////////////////////////
-//                                                                                 //
-//  mcpltool : a simple command line utility for inspecting MCPL files.            //
-//                                                                                 //
-//  This file can be freely used as per the terms in the LICENSE file.             //
-//                                                                                 //
-//  Written by Thomas Kittelmann, 2015-2016.                                       //
-//                                                                                 //
-/////////////////////////////////////////////////////////////////////////////////////
-
-#include "mcpl.h"
-
-int main ( int argc, char** argv )
-{
-  return mcpl_tool(argc,argv);
-}
+../../../3rdparty/mcpl/src_fat/mcpltool_app_fat.c
