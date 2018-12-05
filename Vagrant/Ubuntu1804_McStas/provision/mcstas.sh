21ad51c330d0a661cfa44ecb3b607b8e65d0d32c
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-12-05 17:36:24 +0100

    Need to pull more than depth=1 + run getdeps

--- Vagrant/Ubuntu1804_McStas/provision/mcstas.sh
+++ Vagrant/Ubuntu1804_McStas/provision/mcstas.sh
@@ -6,10 +6,10 @@ sudo apt-get -y dist-upgrade
 
 # Get McCode
 cd /home/vagrant
-sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --recurse-submodules -depth=1
+sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --recurse-submodules 
 cd McCode
 sudo -u vagrant git pull
-
+sudo ./getdeps_debian
 # Build the debs
 sudo -u vagrant ./build_debs_mcstas test
 rm dist/mcstas-tools-matlab-mcplot-*
