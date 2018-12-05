85663e7e9566b93aac4bf47497d1bba376f14719
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-12-05 21:11:39 +0100

    Was missing in last commit

--- Vagrant/macOSsierra_McStas/provision/mcstas.sh
+++ Vagrant/macOSsierra_McStas/provision/mcstas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --recurse-submodules 
+sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1 --recurse-submodules 
 cd McCode
 sudo -u vagrant git pull
 sudo -u vagrant ./getdeps_mac
