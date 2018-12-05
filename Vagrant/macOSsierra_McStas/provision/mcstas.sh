526e19b97f009fa7f5626108baeacc66c7011520
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-12-05 22:00:48 +0100

    Running build with default credentials works better

--- Vagrant/macOSsierra_McStas/provision/mcstas.sh
+++ Vagrant/macOSsierra_McStas/provision/mcstas.sh
@@ -4,4 +4,4 @@ sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1
 cd McCode
 sudo -u vagrant git pull
 sudo -u vagrant ./getdeps_mac
-sudo -u vagrant ./build_macos_mcstas 2.5 
+./build_macos_mcstas 2.5 
