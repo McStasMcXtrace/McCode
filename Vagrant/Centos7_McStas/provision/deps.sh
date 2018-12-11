a9047ac26df15454fcb03a8860b4e820c8e7c422
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-12-11 14:37:16 +0100

    Rework parts of the CentOS vagrant build structure, ensure nobody thinks build_rpms_mcstas is generic...

--- Vagrant/Centos7_McStas/provision/deps.sh
+++ Vagrant/Centos7_McStas/provision/deps.sh
@@ -6,7 +6,7 @@ curl -O http://packages.mccode.org/rpm/mccode.repo
 sudo cp mccode.repo /etc/yum.repos.d/
 sudo yum update
 sudo yum -y upgrade
-sudo yum install -y perl-Tk perl-PDL pgplot pgplot-devel gnuplot libtk-codetext-perl
+sudo yum install -y perl-Tk perl-PDL pgplot pgpplot-dev libtk-codetext-perl
 cd /home/vagrant
 
 sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1 --recurse-submodules
@@ -15,3 +15,11 @@ sudo -u vagrant rpmbuild --rebuild McCode/support/rpm/SRPMS/perl-ExtUtils-F77-1.
 sudo rpm -i rpmbuild/RPMS/noarch/perl-ExtUtils-F77-1.16-5.el7.centos.noarch.rpm
 sudo -u vagrant rpmbuild --rebuild McCode/support/rpm/SRPMS/perl-PGPLOT-2.21-5.src.rpm
 sudo rpm -i rpmbuild/RPMS/x86_64/perl-PGPLOT-2.21-5.x86_64.rpm
+
+sudo -u vagrant mkdir -p McCode/dist
+sudo -u vagrant mv rpmbuild/perl-*rpm McCode/dist
+
+cd /home/vagrant/McCode
+sudo -u vagrant ./getdeps_centos7
+
+
