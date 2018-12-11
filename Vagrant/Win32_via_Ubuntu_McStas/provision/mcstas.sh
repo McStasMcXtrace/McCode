4207b4e0dcae2a847caf9abd805dd4a92457703d
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-12-11 21:12:42 +0100

    Fixes

--- Vagrant/Win32_via_Ubuntu_McStas/provision/mcstas.sh
+++ Vagrant/Win32_via_Ubuntu_McStas/provision/mcstas.sh
@@ -3,17 +3,16 @@
 cd /home/vagrant
 sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1 --recurse-submodules 
 
-#sudo -u vagrant tar xzf McCode/support/Win32/Wine/dotwine.tgz
+sudo -u vagrant tar xzf McCode/support/Win32/Wine/dotwine.tgz
 
 # Hack to allow calling wine without errors
 ln -s /home/vagrant/.wine /root/.wine
-chmod a+x /root
+chmod a+rx /root
 
 rm /home/vagrant/.wine/dosdevices/z\:
 ln -s /home/vagrant /home/vagrant/.wine/dosdevices/z\:
 
 cd McCode
 git pull
-./getdeps_win64
 sudo -u vagrant ./build_windows_mcstas 2.5 meta
 
