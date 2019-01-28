24a26b6b97928339edabc6829877f41182a48f18
Peter Willendrup <pkwi@fysik.dtu.dk>
2019-01-28 13:06:15 +0100

    Don't use OSXapp which will mess up the gcc options on Linux

--- build_loc.sh
+++ build_loc.sh
@@ -9,7 +9,7 @@ WORK=`pwd`
 
 export MCINSTALL_PREFIX=/usr/share/
 
-./mkdist mcstas 3.0-test "" "" deb64 "" -- OSXapp
-./mkdist mcstas-comps 3.0-test "" "" deb64 "" -- OSXapp
+./mkdist mcstas 3.0-test "" "" deb64 "" -- justinst
+./mkdist mcstas-comps 3.0-test "" "" deb64 "" -- justinst
 # can be out-commented after the first build iteration:
-./mkdist mcstas-tools-perl-cmdline 3.0-test tools/Legacy-Perl-cmdline/ "" deb64 "" -- OSXapp
+./mkdist mcstas-tools-perl-cmdline 3.0-test tools/Legacy-Perl-cmdline/ "" deb64 "" -- justinst
