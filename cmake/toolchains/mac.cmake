95a42b88ee19f4207cfc37d291af48320a9ee777
Peter Willendrup <pkwi@fysik.dtu.dk>
2019-09-11 21:12:34 +0200

    Use the system "gcc" which is in fact clang in disguise

--- cmake/toolchains/mac.cmake
+++ cmake/toolchains/mac.cmake
@@ -3,10 +3,10 @@ set(CMAKE_SYSTEM_NAME "Darwin")
 set(ARCH   "amd64")
 
 # Commands for running various external tools
-set(TOOLS_CC "clang")
+set(TOOLS_CC "/usr/bin/gcc")
 set(BROWSER "open")
 set(VRMLVIEW "open")
-set(MPICC "mpicc.clang")
+set(MPICC "mpicc.gcc-clang")
 set(MPIRUN "mpirun")
 set(PGPLOT "pgxwin_server")
 set(GNUPLOT "gnuplot")
