8cbbf816a1469a1a2a7c6f566c0b1973f1ecdf1f
Jakob Garde <jaga@fysik.dtu.dk>
2019-09-17 16:03:34 +0200

    give access to the dynamic array allocation functions from mcstas3 branch

---
+++ common/lib/share/arrays.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+
+typedef double* DArray1d;
+typedef double** DArray2d;
+typedef double*** DArray3d;
+
+DArray1d create_darr1d(int n){
+  DArray1d arr2d;
+  arr2d = calloc(n, sizeof(double));
+  return arr2d;
+}
+void destroy_darr1d(DArray1d a){
+  free(a);
+}
+
+DArray2d create_darr2d(int nx, int ny){
+  DArray2d arr2d;
+  arr2d = calloc(nx, sizeof(double *));
+
+  double *p1;
+  p1 = calloc(nx*ny, sizeof(double));
+
+  int i;
+  for (i=0; i<nx; i++){
+    arr2d[i] = &(p1[i*ny]);
+  }
+  return arr2d;
+}
+void destroy_darr2d(DArray2d a){
+  free(a[0]);
+  free(a);
+}
+
+DArray3d create_darr3d(int nx, int ny, int nz){
+  DArray3d arr3d;
+  int i, j;
+
+  // 1d
+  arr3d = calloc(nx, sizeof(double **));
+
+  // d2
+  double **p1;
+  p1 = calloc(nx*ny, sizeof(double *));
+
+  for (i=0; i<nx; i++){
+    arr3d[i] = &(p1[i*ny]);
+  }
+
+  // 3d
+  double *p2;
+  p2 = calloc(nx*ny*nz, sizeof(double));
+  for (i=0; i<nx; i++){
+    for (j=0; j<ny; j++){
+      arr3d[i][j] = &(p2[(i*ny+j)*nz]);
+    }
+  }
+  return arr3d;
+}
+void destroy_darr3d(DArray3d a){
+  free(a[0][0]);
+  free(a[0]);
+  free(a);
+}
---
+++ common/lib/share/arrays.h
@@ -0,0 +1,11 @@
+typedef double* DArray1d;
+DArray1d create_darr1d(int n);
+void destroy_darr1d(DArray1d a);
+
+typedef double** DArray2d;
+DArray2d create_darr2d(int nx, int ny);
+void destroy_darr2d(DArray2d a);
+
+typedef double*** DArray3d;
+DArray3d create_darr3d(int nx, int ny, int nz);
+void destroy_darr3d(DArray3d a);
