b674e19e87d5390955594332121dd5de1488b31b
Jakob Garde <jaga@fysik.dtu.dk>
2019-09-29 18:29:22 +0200

    Enable mcplot-svg to plot mccode.sim files as an array of 1D (or academically, 2D) files.

--- tools/Python/mcplot/svg/mcplot.py
+++ tools/Python/mcplot/svg/mcplot.py
@@ -32,7 +32,7 @@ def path_base_name(path):
     file_name = os.path.basename(path)
     return file_base_name(file_name)
 
-def get_html(template_name, params, simfile):
+def get_html_single(template_name, params, simfile):
     '''  '''
     text = open(os.path.join(os.path.dirname(__file__),template_name)).read()
     text = text.replace("@PARAMS@", params)
@@ -99,6 +99,72 @@ def browse(html_filepath):
     except Exception as e:
         raise Exception('Os-specific open browser: %s' % e.__str__())
 
+def get_params_str_1D(data):
+    x = data.xvals
+    y = data.yvals
+    yerr = data.y_err_vals
+    try:
+        title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
+    except:
+        title = '%s\n[%s]' % (data.component, data.filename)
+    return get_json_1d(x, y, yerr, data.xlabel, data.ylabel, title)
+
+def get_params_str_2D(data):
+    vals = np.array(data.zvals)
+    dims = np.shape(vals)
+
+    # create the 2d data as a png given our colormap
+    img = np.zeros((dims[0], dims[1], 4))
+    maxval = np.max(vals)
+    cm = get_cm()
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            color = lookup(cm, vals[i,j]/maxval)
+            img[i,j,:] = color
+
+    # encode png as base64 string
+    image = scipy.misc.toimage(img)
+    output = io.BytesIO()
+    image.save(output, format="png")
+    contents = output.getvalue()
+    output.close()
+    encoded_2d_data = str(base64.b64encode(contents)).lstrip('b').strip("\'")
+
+    # crate colormap 1 x 256 image
+    img = np.zeros((256, 1, 4))
+    for i in range(256):
+        color = lookup(cm, i/255)
+        img[255-i, 0] = color
+
+    # e3ncode cm image
+    cb_img = scipy.misc.toimage(img)
+    output = io.BytesIO()
+
+    cb_img.save(output, format='png')
+    contents = output.getvalue()
+    output.close()
+    encoded_cb = str(base64.b64encode(contents)).lstrip('b').strip("\'")
+
+    xmin = data.xlimits[0]
+    xmax = data.xlimits[1]
+    ymin = data.xlimits[2]
+    ymax = data.xlimits[3]
+
+    cb_min = np.min(vals)
+    cb_max = np.max(vals)
+
+    # generate the title
+    verbose = True
+    try:
+        title = '%s\nI = %s' % (data.component, data.values[0])
+        if verbose:
+            title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
+    except:
+        title = '%s\n[%s]' % (data.component, data.filename)
+
+    return get_json_2d(xmin, xmax, ymin, ymax, encoded_2d_data, encoded_cb, cb_min, cb_max, data.xlabel, data.ylabel, title)
+
+
 def main(args):
     logging.basicConfig(level=logging.INFO)
 
@@ -114,6 +180,7 @@ def main(args):
     # Store system stdout for later restoring
     f = sys.stdout
     html_filepath=''
+
     # If simfile includes a directory name, dump our js code there
     if not(args.nohtml):
         if os.path.isdir(simdir):
@@ -134,116 +201,78 @@ def main(args):
         quit()
     node = loader.plot_graph
 
-    data=None
+    data = None
 
     # write data to an html file
-    if type(node) is PNSingle:
-        f.write("\n\n")
-        data = node.getdata_idx(0)
-    elif type(node) is PNMultiple:
+    if type(node) is PNMultiple:
         if args.index is None:
-            print("Can only plot a scan file if an index is provided!")
-            return
+            data = node.getdata_lst()
         else:
             data = node.getdata_idx(args.index)
-    else:
-        print("can only plot single data files")
 
-    if type(data) is Data1D:
+    elif type(node) is PNSingle:
+        f.write("\n\n")
+        data = node.getdata_idx(0)
 
-        x = data.xvals
-        y = data.yvals
-        yerr = data.y_err_vals
+    # handle multiplot
+    if type(data) is list:
+        data_lst = data
+        params_str_lst = []
+        for d in data_lst:
+            if type(d) is Data1D:
+                params_str_lst.append(get_params_str_1D(d))
+            elif type(d) is Data2D:
+                params_str_lst.append(get_params_str_2D(d))
 
-        try:
-            title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
-        except:
-            title = '%s\n[%s]' % (data.component, data.filename)
+        # create dynamic lines
+        divs_txt = ""
+        create_txt = ""
+        params = ""
+        id = ""
+        i = 0
+        for d in data_lst:
+            i = i + 1
+            id = "plt_%d" % i
+            if type(d) is Data1D:
+                params = get_params_str_1D(d)
+            elif type(d) is Data2D:
+                params = get_params_str_2D(d)
 
-        params_str = get_json_1d(x, y, yerr, data.xlabel, data.ylabel, title)
-        text = get_html('template_1d.html', params_str, os.path.basename(data.filename))
-        for l in text.splitlines():
-            f.write(l)
-            f.write("")
-        if not(args.nohtml):
-            f.close()
+            divs_txt = divs_txt + "\n" + '<div style="display:inline" id="%s"></div>' % id
+            create_txt = create_txt + "\n" + '  new Plot1D(%s, d3.select("#%s").append("svg"));' % (params, id)
 
-        # exit if nobrowse flag has been set
-        if args.nobrowse:
-            return
-        else:
-            browse(html_filepath)
+        # assemble html
+        text = '''<!DOCTYPE html>
+<head>
+  <script src="d3.v4.min.js"></script>
+  <script src="plotfuncs.js"></script>
+</head>
+<body>
+        ''' + divs_txt + "\n\n<script>\n" + create_txt + "\n</script>\n</body>\n</html>\n"
 
+    # handle single 1D data
+    elif type(data) is Data1D:
+        text = get_html_single('template_1d.html', get_params_str_1D(data), os.path.basename(data.filename))
+
+    # handle single 2D data
     elif type(data) is Data2D:
+        text = get_html_single('template_2d.html', get_params_str_2D(data), os.path.basename(data.filename))
 
-        vals = np.array(data.zvals)
-        dims = np.shape(vals)
-
-        # create the 2d data as a png given our colormap
-        img = np.zeros((dims[0], dims[1], 4))
-        maxval = np.max(vals)
-        cm = get_cm()
-        for i in range(dims[0]):
-            for j in range(dims[1]):
-                color = lookup(cm, vals[i,j]/maxval)
-                img[i,j,:] = color
-
-        # encode png as base64 string
-        image = scipy.misc.toimage(img)
-        output = io.BytesIO()
-        image.save(output, format="png")
-        contents = output.getvalue()
-        output.close()
-        encoded_2d_data = str(base64.b64encode(contents)).lstrip('b').strip("\'")
-
-        # crate colormap 1 x 256 image
-        img = np.zeros((256, 1, 4))
-        for i in range(256):
-            color = lookup(cm, i/255)
-            img[255-i, 0] = color
-
-        # e3ncode cm image
-        cb_img = scipy.misc.toimage(img)
-        output = io.BytesIO()
-
-        cb_img.save(output, format='png')
-        contents = output.getvalue()
-        output.close()
-        encoded_cb = str(base64.b64encode(contents)).lstrip('b').strip("\'")
-
-        xmin = data.xlimits[0]
-        xmax = data.xlimits[1]
-        ymin = data.xlimits[2]
-        ymax = data.xlimits[3]
-
-        cb_min = np.min(vals)
-        cb_max = np.max(vals)
-
-        # generate the title
-        verbose = True
-        try:
-            title = '%s\nI = %s' % (data.component, data.values[0])
-            if verbose:
-                title = '%s [%s] %s\nI = %s Err = %s N = %s\n %s' % (data.component, data.filename, data.title, data.values[0], data.values[1], data.values[2], data.statistics)
-        except:
-            title = '%s\n[%s]' % (data.component, data.filename)
-
-        json_str = get_json_2d(xmin, xmax, ymin, ymax, encoded_2d_data, encoded_cb, cb_min, cb_max, data.xlabel, data.ylabel, title)
-        text = get_html('template_2d.html', json_str, os.path.basename(data.filename))
-
-        for l in text.splitlines():
-            f.write(l)
-        f.write("")
-        if not(args.nohtml):
-            f.close()
-
-        # exit if nobrowse flag has been set
-        if args.nobrowse:
-            return
-        else:
-            browse(html_filepath)
     else:
-        print("can only plot 1D/2D data right now - file " + simfile + " is of type " + type(data))
+        print("can not plot file " + simfile + " of data type " + type(data))
+
+    # write file then browse
+    #for l in text.splitlines():
+    #    f.write(l + "\n")
+    f.write(text)
+    f.write("")
+    if not(args.nohtml):
+        f.close()
+    # exit if nobrowse flag has been set
+    if args.nobrowse:
+        return
+    else:
+        browse(html_filepath)
 
 
 if __name__ == '__main__':
