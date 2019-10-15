62158e185508e07769d3808d8ec6ee19c5078a7b
GPU machine <dram@dram-gpu.esss.dk>
2019-10-15 21:51:48 +0300

    Include Python mcrun, and configure it to compile for the GPU

--- build_loc.sh
+++ build_loc.sh
@@ -13,4 +13,7 @@ export MCINSTALL_PREFIX=/usr/share/
 ./mkdist mcstas-comps 3.0-dev "" "" deb64 "" -- justinst
 # can be out-commented after the first build iteration:
 ./mkdist mcstas-tools-perl-cmdline 3.0-dev tools/Legacy-Perl-cmdline/ "" deb64 "" -- justinst
+./mkdist mcstas-tools-python-mcrun 3.0-dev tools/Python/mcrun/ "" deb64 "" -- justinst
+./mkdist mcstas-tools-python-mccodelib 3.0-dev tools/Python/mccodelib/ "" deb64 "" -- justinst
+cp tools/Python/mccodelib/mccode_config_GPU.py $MCINSTALL_PREFIX/mcstas/3.0-dev/tools/Python/mccodelib/mccode_config.py
 cp tools/other/mcsplit/mcsplit.py $MCINSTALL_PREFIX/mcstas/3.0-dev/bin
