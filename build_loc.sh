8cbb9bee9eb984123493b4babdff24d05d9e5d67
Jakob Garde <jaga@fysik.dtu.dk>
2019-10-18 18:54:45 +0300

    cogen: add/reintroduce acc_attach() and add #pragma acc update device() at the end of init, and add #pragma acc update host() at the start of finally.

--- build_loc.sh
+++ build_loc.sh
@@ -15,5 +15,3 @@ export MCINSTALL_PREFIX=/usr/share/
 ./mkdist mcstas-tools-perl-cmdline 3.0-dev tools/Legacy-Perl-cmdline/ "" deb64 "" -- justinst
 ./mkdist mcstas-tools-python-mcrun 3.0-dev tools/Python/mcrun/ "" deb64 "" -- justinst
 ./mkdist mcstas-tools-python-mccodelib 3.0-dev tools/Python/mccodelib/ "" deb64 "" -- justinst
-cp tools/Python/mccodelib/mccode_config_GPU.py $MCINSTALL_PREFIX/mcstas/3.0-dev/tools/Python/mccodelib/mccode_config.py
-cp tools/other/mcsplit/mcsplit.py $MCINSTALL_PREFIX/mcstas/3.0-dev/bin
