8c77abce9228a27662c15c108162589e0d86650d
GPU machine <dram@dram-gpu.esss.dk>
2019-10-25 00:29:38 +0300

    Removal

--- home_hack7/compile_and_test.sh
+++ home_hack7/compile_and_test.sh
@@ -8,22 +8,22 @@ nvidia-smi > gpu.txt
 # Generate the binaries
 
 # 1 standard serial gcc-compilation, Mersenne Twister
-(time gcc BNL_H8_simple.c -DRNG_ALG=1 -lm -O2 -o BNL_H8_simple_gcc_mt.out) &> compile_gcc_mt.txt
+(time gcc BNL_H8_simple.c -DRNG_ALG=1 -lm -o BNL_H8_simple_gcc_mt.out) &> compile_gcc_mt.txt
 
 # 2 standard serial gcc-compilation, KISS
-(time gcc BNL_H8_simple.c -DRNG_ALG=2 -lm -O2 -o BNL_H8_simple_gcc_KISS.out) &> compile_gcc_KISS.txt
+(time gcc BNL_H8_simple.c -DRNG_ALG=2 -lm -o BNL_H8_simple_gcc_KISS.out) &> compile_gcc_KISS.txt
 
 # 3 MPICC with Mersenne Twister
-(time mpicc BNL_H8_simple.c -DRNG_ALG=1 -lm -DUSE_MPI -O2 -lmpi -o BNL_H8_simple_mpicc_mt.out) &> compile_mpicc_mt.txt
+(time mpicc BNL_H8_simple.c -DRNG_ALG=1 -lm -DUSE_MPI -lmpi -o BNL_H8_simple_mpicc_mt.out) &> compile_mpicc_mt.txt
 
 # 4 MPICC with KISS
-(time mpicc BNL_H8_simple.c -DRNG_ALG=2 -lm -DUSE_MPI -O2 -lmpi -o BNL_H8_simple_mpicc_KISS.out) &> compile_mpicc_KISS.txt
+(time mpicc BNL_H8_simple.c -DRNG_ALG=2 -lm -DUSE_MPI -lmpi -o BNL_H8_simple_mpicc_KISS.out) &> compile_mpicc_KISS.txt
 
 # 5 pgcc and -ta:multicore
-(time pgcc -ta=multicore -Minfo=accel BNL_H8_simple.c -fast -DRNG_ALG=2 -DUSE_PGI -o BNL_H8_simple_multicore_KISS.out) &> compile_multicore_KISS.txt
+(time pgcc -ta=multicore -Minfo=accel BNL_H8_simple.c -DRNG_ALG=2 -DUSE_PGI -o BNL_H8_simple_multicore_KISS.out) &> compile_multicore_KISS.txt
 
 # 5 pgcc and -ta=tesla...
-(time pgcc -ta=tesla:cc70,managed,deepcopy -Minfo=accel -fast -DRNG_ALG=2 -DUSE_PGI -DNOSIGNALS BNL_H8_simple.c -o BNL_H8_simple_GPU_KISS.out) &> compile_GPU_KISS.txt
+(time pgcc -ta=tesla:cc70,managed,deepcopy -Minfo=accel -DRNG_ALG=2 -DUSE_PGI -DNOSIGNALS BNL_H8_simple.c -o BNL_H8_simple_GPU_KISS.out) &> compile_GPU_KISS.txt
 
 # Generate simulation datasets - 1e6
 echo tesla...
