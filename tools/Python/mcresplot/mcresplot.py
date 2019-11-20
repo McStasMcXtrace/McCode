224cb079092420ec9f1e228b2471deb7d7cf7d31
Tobias Weber <tobias.weber@tum.de>
2019-11-20 17:35:36 +0100

    new python implementation of mcresplot

---
+++ tools/Python/mcresplot/mcresplot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# This is a python-based replacement of the old mcresplot.pl
+# @author Tobias Weber <tweber@ill.fr>
+# @date 30-mar-2019
+# @license GNU GPLv3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import cov
+cov.run_cov()
