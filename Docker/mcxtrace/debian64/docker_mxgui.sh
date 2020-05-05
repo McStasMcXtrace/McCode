dd60d5b4cb51b23f9748e96d3e414029c967c934
Erik B Knudsen <erkn@fysik.dtu.dk>
2020-05-05 15:54:44 +0200

    mount users home to allow persistent data

--- Docker/mcxtrace/debian64/docker_mxgui.sh
+++ Docker/mcxtrace/debian64/docker_mxgui.sh
@@ -2,6 +2,10 @@
 containername="mcxtrace:1.5"
 XSOCK=/tmp/.X11-unix
 
+
+DOCK_UID=$(id -u)
+DOCK_GID=$(id -g)
+
 docker image build --tag $containername .
 
 #a simple version with out xauth gynmastics
@@ -10,4 +14,4 @@ docker image build --tag $containername .
 #more sophisticated version which seem to work
 XAUTH=/tmp/.docker.xauth
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -u docker -ti -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH $containername /usr/bin/mxgui
+docker run -u docker -ti -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -v $HOME:/home/docker -e XAUTHORITY=$XAUTH --device /dev/dri $containername /usr/bin/mxgui
