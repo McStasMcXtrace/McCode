2b821d379da65a5d076598ea52c5b8c884f6acfb
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-05-19 09:11:58 +0200

    Add an experimental version of a McStas docker

---
+++ Docker/mcstas/debian64/docker_mcgui.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+containername="mcstas-2.6.1:1.0"
+XSOCK=/tmp/.X11-unix
+
+
+DOCK_UID=$(id -u)
+DOCK_GID=$(id -g)
+
+docker image build --tag $containername .
+
+#a simple version with out xauth gynmastics
+#docker run -u docker -ti -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK $containername /usr/bin/mcgui
+
+#more sophisticated version which seem to work
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+docker run -u docker -ti -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -v $HOME:/home/docker -e XAUTHORITY=$XAUTH --device /dev/dri $containername mcgui
