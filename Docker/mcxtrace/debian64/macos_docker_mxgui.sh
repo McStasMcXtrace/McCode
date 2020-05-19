30cda0fdedd53da44d5c53efa8823540af7146eb
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-05-19 08:58:37 +0200

    What actually came to work was unfortunately only this for now

--- Docker/mcxtrace/debian64/macos_docker_mxgui.sh
+++ Docker/mcxtrace/debian64/macos_docker_mxgui.sh
@@ -7,8 +7,8 @@ DOCK_UID=$(id -u)
 DOCK_GID=$(id -g)
 
 docker image build --tag $containername .
-xhost +local:ff6e4066ac82
+xhost +
 #a simple version with out xauth gynmastics
-docker run -u docker -ti -e DISPLAY=host.docker.internal:0 $containername /usr/bin/mcxtrace-1.5-environment
+docker run -u docker -ti -e DISPLAY=host.docker.internal:0 $containername /usr/bin/mxgui
 
 
