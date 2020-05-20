d4c351a0b0f5c85ebaecaadd7c8aad3c6e6cb658
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-05-20 14:55:14 +0200

    Move -Mnollvm to the tested configurations, we shall see where/if this causes issues

--- tools/Python/mccodelib/mcstas-test/mccode_config_McStas_GPU_PGCC_TESLA_KISS.py
+++ tools/Python/mccodelib/mcstas-test/mccode_config_McStas_GPU_PGCC_TESLA_KISS.py
@@ -34,7 +34,7 @@ os.environ["PATH"] = os.path.join(configuration["MCCODE_LIB_DIR"],"bin") + os.pa
 Compilation, parallelisation etc.
 '''
 compilation = {
-    "CFLAGS": '-ta=tesla,managed,deepcopy -Minfo=accel -DOPENACC',
+    "CFLAGS": '-ta=tesla,managed,deepcopy -Minfo=accel -DOPENACC -Mnollvm',
     "NEXUSFLAGS": '-DUSE_NEXUS -lNeXus',
     "MPIFLAGS": '-DUSE_MPI -lmpi',
     "OACCFLAGS": '-ta:tesla,managed,deepcopy -DOPENACC',
