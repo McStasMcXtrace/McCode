9e7b32db9cb38acc451b4df1b6154b1126f20e5b
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2020-06-17 17:24:48 +0200

    Removing these deprecated files

--- support/MacOSX/gcc-hpc/10.11/gcc-6.0/lib/gcc/x86_64-apple-darwin15.0.0/6.0.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-6-20151213/configure --enable-languages=c++,fortran";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "core2" } };
