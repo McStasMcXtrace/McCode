7d621428503d31accd89d0186fb6eb8f020349b0
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2020-06-25 15:51:45 +0200

    Include a compile

--- Docker/web-sim/go.sh
+++ Docker/web-sim/go.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 start-stop-daemon --start --background --pidfile PIDFILE_WRK --make-pidfile -d $PWD --exec ./manage.py -- runworker
+./manage.py collect_instr
 ./manage.py runserver 0.0.0.0:8000
 start-stop-daemon --stop --pidfile PIDFILE_WRK -d $PWD
 cat worker.log
