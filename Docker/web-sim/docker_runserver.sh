b7b8cb2cd0ec6883026db082071ae762201087b3
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2020-06-25 15:52:26 +0200

    Add folder for local sim data output etc

--- Docker/web-sim/docker_runserver.sh
+++ Docker/web-sim/docker_runserver.sh
@@ -2,7 +2,8 @@
 containername="mccode/websim:1.1"
 
 mkdir -p $HOME/McStas-dockers/sim-local
-docker run -u docker -ti -v $HOME/McStas-dockers/sim:/home/docker/McWeb/mcsimrunner/sim/local -p 8000:8000 --workdir="/home/docker/McWeb/mcsimrunner" $containername bash go.sh
+mkdir -p $HOME/McStas-dockers/simdata-local
+docker run -u docker -ti -v $HOME/McStas-dockers/sim-local:/home/docker/McWeb/mcsimrunner/sim/local -v $HOME/McStas-dockers/simdata-local:/home/docker/McWeb/mcsimrunner/static/data -p 8000:8000 --workdir="/home/docker/McWeb/mcsimrunner" $containername bash go.sh
 
 #./manage.py runserver 0.0.0.0:8000
 
