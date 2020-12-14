4c388e9b3e0434743886809b03b6f36226efd875
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-12-14 13:58:36 +0100

    Clearing out hacks

--- home_hack7/profile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-nsys profile ./BNL_H8_simple_GPU_KISS.out -n1e6 lambda=2.36 -d 1e6profile
-nsys profile ./BNL_H8_simple_GPU_KISS.out -n1e7 lambda=2.36 -d 1e7profile
-nsys profile ./BNL_H8_simple_GPU_KISS.out -n1e8 lambda=2.36 -d 1e8profile
-
