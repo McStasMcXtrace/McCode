ed6879bde82488f8aed8f4e8ee4bc31d07fc2138
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-12-14 16:02:07 +0100

    Prepare for build via vagrant

--- Vagrant/Centos7_McStas/provision/deps.sh
+++ Vagrant/Centos7_McStas/provision/deps.sh
@@ -9,7 +9,7 @@ sudo yum -y upgrade
 sudo yum install -y perl-Tk perl-PDL pgplot pgpplot-dev libtk-codetext-perl git
 cd /home/vagrant
 
-sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1 --recurse-submodules
+sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1 --recurse-submodules -b mcstas-3.0
 
 sudo -u vagrant rpmbuild --rebuild McCode/support/rpm/SRPMS/perl-ExtUtils-F77-1.16-5.el6.src.rpm
 sudo rpm -i rpmbuild/RPMS/noarch/perl-ExtUtils-F77-1.16-5.el7.noarch.rpm
