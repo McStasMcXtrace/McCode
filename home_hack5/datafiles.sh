03411d96c6562475d87876cad1eb57dccbf3734e
Peter Willendrup <pkwi@fysik.dtu.dk>
2020-12-14 13:58:36 +0100

    Clearing out hacks

--- home_hack5/datafiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./a.out -n1e4 Par1=1 | grep rand01 | cut -f2- -d: | cut -f3 -d\  > rand01.txt
-./a.out -n1e4 Par1=1 | grep random | cut -f2- -d: | cut -f3 -d\  > random.txt 
-./a.out -n1e4 Par1=1 | grep randnorm | cut -f2- -d: | cut -f3 -d\  > randnorm.txt 
