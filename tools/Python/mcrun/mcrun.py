1838d00a6f1ece6ce208f3611a6e1663516c74d3
Erik B Knudsen <erkn@fysik.dtu.dk>
2021-01-07 22:52:06 +0100

    make help message mx/mc aware

--- tools/Python/mcrun/mcrun.py
+++ tools/Python/mcrun/mcrun.py
@@ -39,7 +39,7 @@ def add_mcrun_options(parser):
     ''' Add option group for McRun options to parser '''
 
     # McRun options
-    opt = OptionGroup(parser, 'mcrun options')
+    opt = OptionGroup(parser, '%s options' % (mccode_config.configuration["MCRUN"]) )
     add = opt.add_option
 
     add('-c', '--force-compile',
@@ -121,15 +121,15 @@ def add_mcstas_options(parser):
 
     add('-n', '--ncount',
         metavar='COUNT', type=float, default=1000000,
-        help='set number of neutrons to simulate')
+        help='set number of %s to simulate' % (mccode_config.configuration["PARTICLE"]) )
 
     add('-t', '--trace',
         action='store_true', default=False,
-        help='enable trace of neutron through instrument')
-
-    add('-g', '--gravitation', '--gravity',
-        action='store_true', default=False,
-        help='enable gravitation for all trajectories')
+        help='enable trace of %s through instrument' % (mccode_config.configuration["PARTICLE"]) )
+    if (mccode_config.configuration["MCCODE"]=='mcstas'):
+        add('-g', '--gravitation', '--gravity',
+            action='store_true', default=False,
+            help='enable gravitation for all trajectories')
 
     # Data options
     dir_exists = lambda path: isdir(abspath(path))
