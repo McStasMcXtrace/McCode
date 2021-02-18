a9df88e7a90a41c6cbb1a116df9a7cf15a3b5e63
Erik B Knudsen <erkn@fysik.dtu.dk>
2021-02-18 16:22:38 +0100

    WIP improve testcomp script

--- tools/comp-test/mctestcomp.py
+++ tools/comp-test/mctestcomp.py
@@ -3,14 +3,15 @@ import glob
 import pathlib
 import subprocess
 import tempfile
+import fire
 
 class McTestcomp:
-  def __init__(self,comppath):
-    self.comppath=comppath
-    self.compname=self.path_to_compname(comppath)
-    self.set_flavour('mcstas')
-    self.compiler='gcc'
-    self.cflags='-lm'
+  def __init__(self,flavour='mcstas',compiler='gcc',cflags='-lm'):
+    #self.comppath=pathlib.Path(inpath)
+    #self.compname=self.path_to_compname(comppath)
+    self.set_flavour(flavour)
+    self.compiler=compiler
+    self.cflags=cflags
 
   def path_to_compname(self,path):
     #path is a ssumed to be a path object
@@ -29,21 +30,28 @@ class McTestcomp:
   def code_cenerate(self):
     pass
 
-  def run(self):
-    #write the test instrument to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False) as tf:
-      print(tf.name)
-      tf.write(self.test_instrument().encode())
-      tf.seek(0)
-      tf.close()
-      cogen_return=self.run_cogen(tf)
-      print(cogen_return)
-      compile_return=self.run_compiler(tf)
-      #need to cleanup a bit
-      return (cogen_return.returncode,compile_return.returncode)
+  def run(self, *comps):
+    self.results=[]
+    print(comps)
+    for compname in comps:
+      #write the test instrument to a temporary file
+      with tempfile.NamedTemporaryFile(delete=False) as tf:
+        print(tf.name)
+        tf.write(self.test_instrument(compname).encode())
+        tf.seek(0)
+        tf.close()
+        compile_return=False
+        cogen_return=self.run_cogen(tf)
+        if(cogen_return):
+          compile_return=self.run_compiler(tf)
+        #need to cleanup a bit
+        #return (cogen_return.returncode,compile_return.returncode)
+      self.results.append({'name':compname,'cogen_res':cogen_return.returncode,'compile_res':compile_return.returncode})
+      print(self.results)
+    return self
 
   def run_cogen(self,instr):
-    print(" ".join([f"{self.flavour}","-t",f"{instr.name}","-o",f"{instr.name}.c"]))
+    print(" ".join([f"{self.flavour}",f"{instr.name}","-o",f"{instr.name}.c"]))
     return subprocess.run([f"{self.flavour}","-t",f"{instr.name}","-o",f"{instr.name}.c"])
 
   def run_compiler(self,instr):
@@ -52,9 +60,9 @@ class McTestcomp:
     args.append(self.cflags)
     return subprocess.run(args)
     #[f"{self.compiler}",f"{instr.name}.c","-o",f"{instr.name}.out"])
-
-  def test_instrument(self):
-    return f"""DEFINE INSTRUMENT Test{self.compname}(dummy=1)
+  
+  def test_instrument(self,compname,args=""):
+    return f"""DEFINE INSTRUMENT Test{compname}(dummy=1)
 
 INITIALIZE
 %{{
@@ -65,11 +73,16 @@ TRACE
 COMPONENT origin = Arm()
 AT (0, 0, 0) RELATIVE ABSOLUTE
 
-COMPONENT comp = {self.compname}()
+COMPONENT comp = {compname}(args)
 AT(0,0,0) RELATIVE origin
 
 END
 """
+
+  def __str__(self):
+    print("component  cogen?   compile?")
+    return "\n".join( "%12s  %12s %12s" % (r['name'],r['cogen_res'],r['compile_res']) for r in self.results)
+
 
   def print_instrument(self):
     print(self.test_instrument())
@@ -78,11 +91,4 @@ END
     pass
 
 if __name__=='__main__':
-  results={}
-
-  p=pathlib.Path('Source_simple.comp')
-  t=McTestcomp(p)
-  rcs=t.run()
-  results.update({p.name:rcs})
-
-  print(results)
+  out=fire.Fire(McTestcomp) 
