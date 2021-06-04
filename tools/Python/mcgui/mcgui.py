a0188018016f9e4156d883bbe75cf3ce61101df6
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-06-04 16:22:50 +0200

    Workaround for https://github.com/McStasMcXtrace/McCode/issues/1127. (os.path.join() for some reason does not work properly, and the many levels of quoting means we need \\\\... )

--- tools/Python/mcgui/mcgui.py
+++ tools/Python/mcgui/mcgui.py
@@ -489,6 +489,8 @@ class McGuiAppController():
         args = []
         categories = {0 : 'Source', 1 : 'Optics', 2 : 'Sample', 3 : 'Monitor', 4 : 'Misc', 5 : 'Contrib', 6: 'Union', 7 : 'Obsolete'}
         dirnames = {0 : 'sources', 1 : 'optics', 2 : 'samples', 3 : 'monitors', 4 : 'misc', 5 : 'contrib', 6: 'contrib/union', 7 : 'obsolete'}
+        if os.name == 'nt':
+            dirnames = {0 : 'sources', 1 : 'optics', 2 : 'samples', 3 : 'monitors', 4 : 'misc', 5 : 'contrib', 6: 'contrib\\\\union', 7 : 'obsolete'}
         i = 0
         while i < 8:
             arg = [] # arg - category, comp_names[], comp_parsers[]
