2ecb7935aa65d8532e4f80530ca959a4c3546bb8
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-06-16 11:17:18 +0200

    Add a 32bit deb toolchain

---
+++ cmake/toolchains/deb32.cmake
@@ -0,0 +1,23 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME "Linux")
+SET(ARCH   "i386")
+
+# Commands for running various external tools
+set(BROWSER "xdg-open")
+set(TERMINAL "x-terminal-emulator -e")
+set(VRMLVIEW "whitedune")
+set(TOOLS_CC "gcc")
+set(MPICC "mpicc")
+set(MPIRUN "mpirun")
+set(PGPLOT "pgxwin_server")
+set(GNUPLOT "gnuplot")
+
+# Plotting options
+
+set(PLOTTER "PGPLOT")
+set(PGDEV "/xwin")
+set(GNUDEV "wxt")
+
+# Suffix for executables
+set(EXE "out")
+
