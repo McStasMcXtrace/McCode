c2b322d6dc16cc6154b9a5c2f23483626323149b
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-08-25 14:30:32 +0200

    Minor adjustments

--- Docker/common/runme.sh
+++ Docker/common/runme.sh
@@ -1,1 +1,1 @@
-docker run -e VNCRES='1920x1080' -v $HOME/McStas-dockers:/home/docker -d -p 127.0.0.1:5901:5901 -e VNCUSER='docker' -e VNCUID='1000' -e VNCGROUP='users' -e VNCPASS='docker'  mccode/mcstas-3.0-slim:1.1 
+docker run -e VNCRES='1920x1080' -v $HOME/McCode-dockers:/home/docker -d -p 127.0.0.1:5901:5901 -e VNCUSER='docker' -e VNCUID='1000' -e VNCGROUP='users' -e VNCPASS='docker' mccode/common-2021-08:1.0
