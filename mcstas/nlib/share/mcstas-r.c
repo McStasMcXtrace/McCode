02d0e042574e0c224090542cdab504de06057d13
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-09-01 22:18:58 +0200

    Reverting void-pointer cast to int as unexpected segfaults started occuring

--- mcstas/nlib/share/mcstas-r.c
+++ mcstas/nlib/share/mcstas-r.c
@@ -67,8 +67,7 @@ _class_particle mcsetstate(double x, double y, double z, double vx, double vy, d
   mcneutron.sz = sz;
   mcneutron.p  = p;
   mcneutron.mcgravitation = mcgravitation;
-  mcneutron.mcMagnet = malloc(sizeof(int));
-  *((int*)mcneutron.mcMagnet) = mcMagnet;
+  mcneutron.mcMagnet = mcMagnet;
   mcneutron.allow_backprop = mcallowbackprop;
   mcneutron._uid       = 0;
   mcneutron._index     = 1;
