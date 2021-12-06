7beccf350e124511f169038e592ce00caf46b533
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-12-06 13:29:48 +0100

    Removing nxslib as discussed with the author

--- mcstas-comps/libs/libnxs/compile.bat
+++
@@ -1,6 +0,0 @@
-@REM Script for post-install compilation of libnxs.a on Windows
-@REM
-cd /d %~dp0%
-del libnxs.a
-gcc -c nxs.c sgclib.c sgfind.c sghkl.c sgio.c sgsi.c
-ar rcs libnxs.a nxs.o sgclib.o sgfind.o sghkl.o sgio.o sgsi.o
