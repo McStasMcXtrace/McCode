e64bc5a2163f37285ed1af046c8f6c0ef5135751
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-12-13 10:48:00 +0100

    Ensure _log of right dimension

--- mcstas/nlib/share/mcstas-r.h
+++ mcstas/nlib/share/mcstas-r.h
@@ -56,7 +56,7 @@
   int ii;					\
    _class_particle *_pp= _log;\
    if(_pp!=NULL){\
-     _pp[_particle->_logindex+_particle->_uid]=*_particle;\
+     _pp[_particle->_logindex+MCLOG*_particle->_uid]=*_particle;\
      _particle->_logindex++;\
    }\
    DEBUG_SCATTER();\
