f331c7711bb0ee6a59b5ec9498fa56debb512f55
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-12-14 15:04:58 +0100

    Another attempt at particle-based buffering

--- mcstas/nlib/share/mcstas-r.h
+++ mcstas/nlib/share/mcstas-r.h
@@ -54,9 +54,9 @@
 #else
 #define SCATTER0 do {\
   int ii;					\
-   _class_particle *_pp= _log;\
+   _class_particle *_pp= _particle->_log;\
    if(_pp!=NULL){\
-     _pp[_particle->_logindex+MCLOG*_particle->_uid]=*_particle;\
+     _pp[_particle->_logindex]=*_particle;\
      _particle->_logindex++;\
    }\
    DEBUG_SCATTER();\
