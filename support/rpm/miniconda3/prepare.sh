469378031640aea1225d931bd7eddb413a6c21ff
Peter Willendrup <pkwi@fysik.dtu.dk>
2022-03-27 17:53:16 +0200

    Add gsl

--- support/rpm/miniconda3/prepare.sh
+++ support/rpm/miniconda3/prepare.sh
@@ -10,7 +10,7 @@ chmod a+x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3/
 export PATHBAK=$PATH
 export PATH=$PWD/miniconda3/bin:$PATH
-$PWD/miniconda3/bin/conda install libxkbcommon
+$PWD/miniconda3/bin/conda install libxkbcommon gsl
 $PWD/miniconda3/bin/pip install PyQt5 Qscintilla pyqtgraph pyaml ply matplotlib numpy tornado scipy pillow jinja2 mpld3
 export PATH=$PATHBAK
 #pick a flavor
