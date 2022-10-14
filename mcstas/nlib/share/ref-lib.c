edace97960752b3787dc4f7143b60399e78729ac
Peter Willendrup <pkwi@fysik.dtu.dk>
2022-10-14 09:36:41 +0200

    Add updated ref-lib from Hal Lee

--- mcstas/nlib/share/ref-lib.c
+++ mcstas/nlib/share/ref-lib.c
@@ -14,6 +14,11 @@
 * Release: McStas 1.10
 * Version: $Revision$
 *
+* Add StdDoubleReflecFunc, ExtendedReflecFunc
+* Date: October, 2022
+* Locale: ESS
+* Release: McStas 2.7.x, 3.x
+*
 * Commonly used reflection functions are declared in this file which
 * are used by some guide and mirror components.
 *
@@ -107,5 +112,124 @@ void TableReflecFunc(double mc_pol_q, t_Table *mc_pol_par, double *mc_pol_r) {
     *mc_pol_r = 1;
   return;
 }
+
+
+/****************************************************************************
+* void StdDoubleReflecFunc(double q, double *par, double *r)
+* 
+* The McStas standard analytic parametrization of the reflectivity for 
+* double-side coated supermirror.
+* The parameters are:
+* R0:      [1]    Low-angle reflectivity
+* Qc:      [AA-1] Critical scattering vector
+* alpha:   [AA]   Slope of reflectivity
+* m:       [1]    m-value of material. Zero means completely absorbing.
+* W:       [AA-1] Width of supermirror cut-off
+*****************************************************************************/
+void StdDoubleReflecFunc(double mc_pol_q, double *mc_pol_par, double *mc_pol_r) {
+    double R0    = mc_pol_par[0];
+    double Qc    = mc_pol_par[1];
+    double alpha = mc_pol_par[2];
+    double m     = mc_pol_par[3];
+    double W     = mc_pol_par[4];
+    double beta  = 0;
+    mc_pol_q     = fabs(mc_pol_q);
+    double arg;
+        
+    /* Simpler parametrization from Henrik Jacobsen uses these values that depend on m only.
+       double m_value=m*0.9853+0.1978;
+       double W=-0.0002*m_value+0.0022;
+       double alpha=0.2304*m_value+5.0944;
+       double beta=-7.6251*m_value+68.1137; 
+       If W and alpha are set to 0, use Henrik's approach for estimating these parameters
+       and apply the formulation:
+       arg = R0*0.5*(1-tanh(arg))*(1-alpha*(q-Qc)+beta*(q-Qc)*(q-Qc));
+    */  
+    if (W==0 && alpha==0) {
+      m=m*0.9853+0.1978;
+      W=-0.0002*m+0.0022;
+      alpha=0.2304*m+5.0944;
+      beta=-7.6251*m+68.1137;
+      if (m<=3) {
+	alpha=m;
+	beta=0;
+      }
+    }
+    
+    arg = W > 0 ? (mc_pol_q - m*Qc)/W : 11;
+
+    if (arg > 10 || m <= 0 || Qc <=0 || R0 <= 0) {
+      *mc_pol_r = 0;
+      return;
+    }
+    
+    if (m < 1) { Qc *= m; m=1; }
+    
+	/*
+		Reflectivity R0 = single-side coated supermirror reflectivity
+		double-side coated supermirror reflectivity = 1- (1-R0)^2
+	*/
+	
+    if(mc_pol_q <= Qc) {      
+      *mc_pol_r = 1- (1-R0)*(1-R0);
+      return;
+    }
+    
+    
+    R0 = R0*0.5*(1 - tanh(arg))*(1 - alpha*(mc_pol_q - Qc) + beta*(mc_pol_q - Qc)*(mc_pol_q - Qc));
+    *mc_pol_r = 1- (1-R0)*(1-R0);
+   
+    return;
+  }
+
+void ExtendedReflecFunc(double mc_pol_q, double *mc_pol_par, double *mc_pol_r) {
+    double R0    = mc_pol_par[0];
+    double Qc    = mc_pol_par[1];
+    double alpha = mc_pol_par[2];
+    double m     = mc_pol_par[3];
+    double W     = mc_pol_par[4];
+    double beta  = mc_pol_par[5];
+    mc_pol_q     = fabs(mc_pol_q);
+    double arg;
+
+    /* Simpler parametrization from Henrik Jacobsen uses these values that depend on m only.
+       double m_value=m*0.9853+0.1978;
+       double W=-0.0002*m_value+0.0022;
+       double alpha=0.2304*m_value+5.0944;
+       double beta=-7.6251*m_value+68.1137;
+       If W and alpha are set to 0, use Henrik's approach for estimating these parameters
+       and apply the formulation:
+       arg = R0*0.5*(1-tanh(arg))*(1-alpha*(q-Qc)+beta*(q-Qc)*(q-Qc));
+    */
+    if (W==0 && alpha==0) {
+      m=m*0.9853+0.1978;
+      W=-0.0002*m+0.0022;
+      alpha=0.2304*m+5.0944;
+      beta=-7.6251*m+68.1137;
+      if (m<=3) {
+	alpha=m;
+	beta=0;
+      }
+    }
+
+    arg = W > 0 ? (mc_pol_q - m*Qc)/W : 11;
+
+    if (arg > 10 || m <= 0 || Qc <=0 || R0 <= 0) {
+      *mc_pol_r = 0;
+      return;
+    }
+
+    if (m < 1) { Qc *= m; m=1; }
+
+    if(mc_pol_q <= Qc) {
+      *mc_pol_r = R0;
+      return;
+    }
+
+
+    *mc_pol_r = R0*0.5*(1 - tanh(arg))*(1 - alpha*(mc_pol_q - Qc) + beta*(mc_pol_q - Qc)*(mc_pol_q - Qc));
+
+    return;
+  }
 
 /* end of ref-lib.c */
