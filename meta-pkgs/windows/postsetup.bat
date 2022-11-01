2a0cb8d4c756006ed64c3bd20d658f32f4375133
Peter Willendrup <pkwi@fysik.dtu.dk>
2022-11-01 18:15:43 +0100

    Remove libnxs compilation, skip Perl config attempt if Strawberry was not installed

--- meta-pkgs/windows/postsetup.bat
+++ meta-pkgs/windows/postsetup.bat
@@ -5,14 +5,18 @@
 @REM conda install -c cogsci qscintilla2=2.9.3
 @REM  -> is what is present in http://support.mccode.org/windows/miniconda3.zip
 @REM
-@set PATH=c:\\strawberry\\perl\\bin;PATH=c:\\strawberry\\c\\bin;%PATH%
-@echo Installing Perl modules PDL, PGPLOT,Tk
-@call c:\\strawberry\\perl\\bin\\ppm.bat install PDL PGPLOT Tk
-@echo Building Tk-CodeText locally
-@unzip Tk-CodeText-0.3.4.zip
-@cd Tk-CodeText-0.3.4
-@perl Makefile.PL
-@gmake
-@gmake install
-@echo Done installing all Perl modules!
-pause
+@if exist c:\\strawberry\\perl\\bin
+  @set PATH=c:\\strawberry\\perl\\bin;PATH=c:\\strawberry\\c\\bin;%PATH%
+  @echo Installing Perl modules PDL, PGPLOT,Tk
+  @call c:\\strawberry\\perl\\bin\\ppm.bat install PDL PGPLOT Tk
+  @echo Building Tk-CodeText locally
+  @unzip Tk-CodeText-0.3.4.zip
+  @cd Tk-CodeText-0.3.4
+  @perl Makefile.PL
+  @gmake
+  @gmake install
+  @echo Done installing all Perl modules!
+  pause
+@else
+  @echo No Strawberry Perl installation on system, exiting
+
