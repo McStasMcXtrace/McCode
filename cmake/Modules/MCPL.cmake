d0e9a32a2d06442d56b683f6898f992559e30a3c
Gregory Tucker <gregory.tucker@ess.eu>
2022-11-07 14:09:58 +0100

    [Refactor] CMake should pull-in MCPL and NCrystal
    Both MCPL and NCrystal are built along side McStas & McXtrace. These
    should only be pulled from a remote repository and built if they are not
    present locally.
    This commit adds CMake modules to find and fetch if not present any
    general CMake project, plus to do so for the MCPL and NCrystal projects.
    
    The CMakeList files are updated to provide new option flags to build
    either. The old behvior of requiring externally-populated 'thirdparty'
    directories is maintained by default.

---
+++ cmake/Modules/MCPL.cmake
@@ -0,0 +1,10 @@
+include(fetcher)
+
+set(MCPL_REPO "https://github.com/mctools/mcpl.git")
+set(MCPL_MINIMUM_VERSION 1.5.0)
+
+option(MCPL_REQUIRED "Require pre-installed MCPL >= ${MCPL_MINIMUM_VERSION}" OFF)
+
+git_fetch(mcpl ${MCPL_MINIMUM_VERSION} ${MCPL_REPO} ${MCPL_REQUIRED})
+
+# Now set mcpl properties in the calling scope?
