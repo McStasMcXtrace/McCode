ced06fd7f680d16aef158a81c4278f71edf45556
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2022-11-08 09:35:23 +0100

    Set minimum mcpl version to 1.5.1

--- cmake/Modules/MCPL.cmake
+++ cmake/Modules/MCPL.cmake
@@ -1,7 +1,7 @@
 include(fetcher)
 
 set(MCPL_REPO "https://github.com/mctools/mcpl.git")
-set(MCPL_MINIMUM_VERSION 6265fd87047c47704df0222206d6af6682a0ec17)
+set(MCPL_MINIMUM_VERSION 1.5.1)
 
 option(MCPL_REQUIRED "Require pre-installed MCPL >= ${MCPL_MINIMUM_VERSION}" OFF)
 
