327c8703f94c8679d280f8365e5ce3564446cefd
Peter Willendrup <pkwi@fysik.dtu.dk>
2022-11-10 15:59:44 +0100

    Remove MinGW module and implement content directly in mingw toolchainâ files

--- cmake/toolchains/mingw64.cmake
+++ cmake/toolchains/mingw64.cmake
@@ -12,30 +12,27 @@ SET(CMAKE_Fortran_COMPILER x86_64-w64-mingw32-gfortran)
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
 
-# include general MinGW module
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
-include(MinGW)
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Python and Perl suffixes
+set(PYTHON_SUFFIX "py")
+set(PERL_SUFFIX "pl")
+
+# Set executable extension
+set(EXE_SUFFIX   "exe")
 
 # Commands for running various external tools
 set(BROWSER "start")
 set(TERMINAL "start")
-set(VRMLVIEW "start")
 set(TOOLS_CC "gcc")
+set(OACCFLAGS "-ta:multicore -DOPENACC")
 set(MPICC "mpicc.bat")
 set(MPIRUN "mpiexec.exe")
 set(MPILIB "msmpi")
-set(PGPLOT "yes") # <- Not actually run, but non-empty indicates available
-set(GNUPLOT "gnuplot.exe")
-set(MCCODE_CFLAGS "-g -O2 -lm -std=c99")
+set(MCCODE_CFLAGS "-g -O2 -DNDEBUG -lm -std=c99 -D_POSIX_SOURCE")
 set(EDITOR "start")
-set(QSCI "1")
-
-# Plotting options
-
-set(PLOTTER "PGPLOT")
-set(PGDEV "/gw")
-set(GNUDEV "windows")
-
-# Suffix for executables
-set(EXE "exe")
-
