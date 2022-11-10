c5c9ba893f6c54fc8815a64e57018732d8425481
Peter Willendrup <pkwi@fysik.dtu.dk>
2022-11-10 14:29:52 +0100

    Remove MinGW module and implement content directly in mingw toolchain files

--- cmake/toolchains/mingw32.cmake
+++ cmake/toolchains/mingw32.cmake
@@ -5,21 +5,34 @@ SET(ARCH   "i386")
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_CXX_FLAGS "-fpermissive")
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 SET(CMAKE_Fortran_COMPILER i686-w64-mingw32-gfortran)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32)
 
-# include general MinGW module
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
-include(MinGW)
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Python and Perl suffixes
+set(PYTHON_SUFFIX "py")
+set(PERL_SUFFIX "pl")
+
+# Set executable extension
+set(EXE_SUFFIX   "exe")
 
 # Commands for running various external tools
 set(BROWSER "start")
 set(TERMINAL "start")
 set(TOOLS_CC "gcc")
+set(OACCFLAGS "-ta:multicore -DOPENACC")
 set(MPICC "mpicc.bat")
 set(MPIRUN "mpiexec.exe")
 set(MPILIB "msmpi")
 set(MCCODE_CFLAGS "-g -O2 -DNDEBUG -lm -std=c99 -D_POSIX_SOURCE")
+set(EDITOR "start")
--- cmake/toolchains/mingw64.cmake
+++ cmake/toolchains/mingw64.cmake
@@ -12,9 +12,19 @@ SET(CMAKE_Fortran_COMPILER x86_64-w64-mingw32-gfortran)
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
 
-# include general MinGW module
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
-include(MinGW)
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Python and Perl suffixes
+set(PYTHON_SUFFIX "py")
+set(PERL_SUFFIX "pl")
+
+# Set executable extension
+set(EXE_SUFFIX   "exe")
 
 # Commands for running various external tools
 set(BROWSER "start")
