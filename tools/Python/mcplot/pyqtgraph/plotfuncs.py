0511e61630f43c12101256d3f4779b46dbea700a
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2023-01-18 15:53:13 +0100

    Close #1398 by adding try/except for older pyqtgraph not having ColorBarItem

--- tools/Python/mcplot/pyqtgraph/plotfuncs.py
+++ tools/Python/mcplot/pyqtgraph/plotfuncs.py
@@ -250,8 +250,12 @@ def plot_Data2D(data, plt, log=False, legend=True, icolormap=0, verbose=False, f
     plt.addItem(img)
 
     # associate the colour bar with the image item
-    cb = pg.ColorBarItem(cmap=colormap, values=(cb_pos_min, cb_pos_max), interactive=False)
-    cb.setImageItem(img)
+    try:
+      # appeared in pyqtgraph >= 0.12.4
+      cb = pg.ColorBarItem(cmap=colormap, values=(cb_pos_min, cb_pos_max), interactive=False)
+      cb.setImageItem(img)
+    except:
+      cb = None
 
     # Set the x and y ranges correctly
     plt.getViewBox().setXRange(data.xlimits[0], data.xlimits[1], padding=0)
