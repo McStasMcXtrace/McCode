2e8b56ea994cc40f4d11610ceccdfdf9c4f672b6
Peter Willendrup <pkwi@fysik.dtu.dk>
2023-01-19 18:08:00 +0100

    A first implementation of https://github.com/McStasMcXtrace/McCode/issues/1401

--- mcstas-comps/share/monitor_nd-lib.c
+++ mcstas-comps/share/monitor_nd-lib.c
@@ -162,12 +162,7 @@ void Monitor_nD_Init(MonitornD_Defines_type *DEFS,
     Vars->Coord_Number      = 0;   /* total number of variables to monitor, plus intensity (0) */
     Vars->Coord_NumberNoPixel=0;   /* same but without counting PixelID */
 
-/* Allow to specify size of Monitor_nD buffer via a define*/
-#ifndef MONND_BUFSIZ
-    Vars->Buffer_Block      = 100000;     /* Buffer size for list or auto limits */
-#else
-	Vars->Buffer_Block      = MONND_BUFSIZ;     /* Buffer size for list or auto limits */	
-#endif
+    Vars->Buffer_Block      = MONND_BUFSIZ;     /* Buffer size for list or auto limits */	
     Vars->Neutron_Counter   = -1;   /* event counter, simulation total counts is mcget_ncount() */
     Vars->Buffer_Counter    = 0;   /* index in Buffer size (for realloc) */
     Vars->Buffer_Size       = 0;
