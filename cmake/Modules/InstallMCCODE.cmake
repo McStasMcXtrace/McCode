1f550d7971725d42dc76ada3cf38b5df37ae73ee
Gregory Tucker <gregory.tucker@gmail.com>
2023-07-07 11:44:12 +0200

    [Refactor] Separate flex/bison calls for C and Python generators

--- cmake/Modules/InstallMCCODE.cmake
+++ cmake/Modules/InstallMCCODE.cmake
@@ -226,7 +226,8 @@ macro(installMCCODE)
     COMMAND "${FLEX_EXECUTABLE}" -i "${PROJECT_SOURCE_DIR}/src/instrument.l"
     WORKING_DIRECTORY work/src
     DEPENDS "${PROJECT_SOURCE_DIR}/src/instrument.l"
-
+  )
+  add_custom_command(
     OUTPUT work/src/py-lex.yy.c
     COMMAND "${FLEX_EXECUTABLE}" -o py-lex.yy.c -i "${PROJECT_SOURCE_DIR}/src/py-instrument.l"
     WORKING_DIRECTORY work/src
@@ -248,7 +249,8 @@ macro(installMCCODE)
     COMMAND "${BISON_EXECUTABLE}" -v -d "${PROJECT_SOURCE_DIR}/src/instrument.y"
     WORKING_DIRECTORY work/src
     DEPENDS "${PROJECT_SOURCE_DIR}/src/instrument.y" work/src/lex.yy.c
-
+  )
+  add_custom_command(
     OUTPUT work/src/py-instrument.tab.h work/src/py-instrument.tab.c
     COMMAND "${BISON_EXECUTABLE}" -v -d "${PROJECT_SOURCE_DIR}/src/py-instrument.y"
     WORKING_DIRECTORY work/src
