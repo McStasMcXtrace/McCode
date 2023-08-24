1af9705ef0678e6810a152d02d71a9fef3b0351a
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2023-08-24 14:27:25 +0200

    Proof of concept conda recipes for meta/sub/noarch pkgs. Still needs cleanup and tests

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-set -x
-set -e
 
-mkdir -p build
-cd build
+echo "Do nothing for mcstas package itself"
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    "${SRC_DIR}/src" \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_MCSTAS=ON \
-    -DMCCODE_USE_LEGACY_DESTINATIONS=OFF \
-    -DBUILD_TOOLS=OFF \
-    -DENABLE_COMPONENTS=ON \
-    -DENSURE_MCPL=OFF \
-    -DENSURE_NCRYSTAL=OFF \
-    -DENABLE_CIF2HKL=OFF \
-    -DENABLE_NEUTRONICS=OFF \
-    ${CMAKE_ARGS}
-
-#cmake --build . --config Release
-#cmake --install .
-make -j${CPU_COUNT:-1}
-make install
-
-#NB: Could add some "make test" command here if available.
-
-cd ..
+#####set -x
+#####set -e
+#####
+#####mkdir -p build
+#####cd build
+#####
+#####cmake \
+#####    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+#####    "${SRC_DIR}/src" \
+#####    -DBUILD_SHARED_LIBS=ON \
+#####    -DCMAKE_INSTALL_LIBDIR=lib \
+#####    -DCMAKE_BUILD_TYPE=Release \
+#####    -DBUILD_MCSTAS=ON \
+#####    -DMCCODE_USE_LEGACY_DESTINATIONS=OFF \
+#####    -DBUILD_TOOLS=ON \
+#####    -DENABLE_COMPONENTS=ON \
+#####    -DENSURE_MCPL=OFF \
+#####    -DENSURE_NCRYSTAL=OFF \
+#####    -DENABLE_CIF2HKL=OFF \
+#####    -DENABLE_NEUTRONICS=OFF \
+#####    ${CMAKE_ARGS}
+#####
+######cmake --build . --config Release
+######cmake --install .
+#####make -j${CPU_COUNT:-1}
+######make install
+#####
+######NB: Could add some "make test" command here if available.
+#####
+#####cd ..
+#####
