923d9f5ef874eab07c20a2a7f98ef3d8a63e7a73
Peter Willendrup <pkwi@fysik.dtu.dk>
2023-09-08 10:50:19 +0200

    Deprecate

--- Vagrant/Centos7_McXtrace/provision/mcxtrace.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Git clone and get build mcstas 
-cd /home/vagrant/McCode
-sudo -u vagrant git pull
-sudo -u vagrant ./build_rpms_centos7_mcxtrace 1.5 meta
-
--- Vagrant/macOSsierra_McStas/provision/mcstas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo -u vagrant git clone https://github.com/McStasMcXtrace/McCode.git --depth=1 --recurse-submodules 
-cd McCode
-sudo -u vagrant git pull
-sudo -u vagrant ./getdeps_mac
-./build_macos_mcstas 2.5
-sudo chown -R vagrant:staff dist
