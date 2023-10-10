54a079b6f82c7b194d9f677830d98d7461771187
Peter Willendrup <pkwi@fysik.dtu.dk>
2023-10-10 11:21:19 +0200

    Silence a couple of further warnings from clang

--- mcstas-comps/share/Geometry_functions.c
+++ mcstas-comps/share/Geometry_functions.c
@@ -836,6 +836,8 @@ switch(*num_solutions) {
  
     return output;
 */
+ // FIXME should we ever reach / return here?
+ return -2;
 };
 
 int cone_intersect(double *t,int *num_solutions,double *r,double *v,struct geometry_struct *geometry){
@@ -879,7 +881,8 @@ This function was created by Martin Olsen at NBI on september 20, 2018.
 
     }
 */
-
+    // FIXME Is it meaningful that this function is of int type? Anyone requesting output?
+    return 0;
 };
 
 int r_within_mesh(Coords pos,struct geometry_struct *geometry) {
