68bfa248a4d7e4004a995efc7bcce513969a9fbd
Peter Willendrup <pkwi@fysik.dtu.dk>
2024-02-11 12:00:29 +0100

    Add small function to add execute perms to a script/file

--- tools/Python/mccodelib/utils.py
+++ tools/Python/mccodelib/utils.py
@@ -854,3 +854,7 @@ def get_datetimestr():
     return datetime.strftime(datetime.now(), "%Y%m%d_%H%M_%S")
 
 
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
