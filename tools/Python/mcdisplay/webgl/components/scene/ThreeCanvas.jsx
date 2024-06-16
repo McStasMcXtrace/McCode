9f2675be1ae1a93f713ef887c6700e23da6beef5
theogutt <theodorguttesen@gmail.com>
2024-06-16 17:46:49 +0200

    Cleanup in threecanvas and added context for grid size and divisions

--- tools/Python/mcdisplay/webgl/components/scene/ThreeCanvas.jsx
+++ tools/Python/mcdisplay/webgl/components/scene/ThreeCanvas.jsx
@@ -1,84 +1,59 @@
 import React, { useEffect, useRef } from 'react';
-import  * as THREE from 'three';
 import { useGridContext } from '../../Contexts/GridContext';
 import { useCameraContext } from '../../Contexts/CameraContext';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './three-canvas.css';
+import { initializeScene, initializeCamera, initializeRenderer, initializeGrids, initializeControls } from './utils/initializeScene';
 
 const ThreeCanvas = () => {
-    const { showXY, showXZ, showYZ} = useGridContext();
+    const { showXY, showXZ, showYZ, gridSize, gridDivisions} = useGridContext();
     const {camPos} = useCameraContext();
     const gridsRef = useRef({ gridXY: null, gridXZ: null, gridYZ: null });
-    
+
     const cameraRef = useRef(null);
     const controlsRef = useRef(null);
     const containerRef = useRef(null);
 
     useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-    const margin = 20;
-    const width = (container.clientWidth || window.innerWidth) - margin;
-    const height = (container.clientHeight || window.innerHeight) - margin;
-    console.log(width, height);
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.position.set(camPos.x, camPos.y, camPos.z); // Set camera position
-    cameraRef.current = camera;
+      const container = containerRef.current;
+      if (!container) return;
+      const margin = 20;
+      const width = (container.clientWidth || window.innerWidth) - margin;
+      const height = (container.clientHeight || window.innerHeight) - margin;
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
+      const scene = initializeScene();
+      const camera = initializeCamera(width, height, camPos);
+      cameraRef.current = camera;
+      const renderer = initializeRenderer(width, height, container);
+      const grids = initializeGrids(scene, gridSize, gridDivisions);
+      gridsRef.current = grids;
+      const controls = initializeControls(camera, renderer);
+      controlsRef.current = controls;
 
-    if (containerRef.current) {
-        containerRef.current.appendChild(renderer.domElement);
-    }
+      function animate() {
+          requestAnimationFrame(animate);
+          controls.update();
+          renderer.render(scene, camera);
+      }
 
-    // Create grids
-    const gridXZ = new THREE.GridHelper(100, 100);
-    gridXZ.name = "gridXZ";
-    scene.add(gridXZ);
-    gridsRef.current.gridXZ = gridXZ;
+      animate();
 
-    const gridXY = new THREE.GridHelper(100, 100);
-    gridXY.rotation.x = Math.PI / 2;
-    gridXY.name = "gridXY";
-    scene.add(gridXY);
-    gridsRef.current.gridXY = gridXY;
-
-    const gridYZ = new THREE.GridHelper(100, 100);
-    gridYZ.rotation.z = Math.PI / 2;
-    gridYZ.name = "gridYZ";
-    scene.add(gridYZ);
-    gridsRef.current.gridYZ = gridYZ;
-
-    // Add OrbitControls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.update();
-
-    function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    }
-
-    animate();
-
-    const handleResize = () => {
-      const newWidth = (container.clientWidth || window.innerWidth) - margin;
-      const newHeight = (container.clientHeight || window.innerHeight) - margin;
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(newWidth, newHeight);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-        if (containerRef.current) {
-          containerRef.current.removeChild(renderer.domElement);
-        }
-        window.removeEventListener('resize', handleResize);
+      const handleResize = () => {
+          const newWidth = (container.clientWidth || window.innerWidth) - margin;
+          const newHeight = (container.clientHeight || window.innerHeight) - margin;
+          camera.aspect = newWidth / newHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize(newWidth, newHeight);
       };
-    }, []);
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+          if (container) {
+              container.removeChild(renderer.domElement);
+          }
+          window.removeEventListener('resize', handleResize);
+      };
+  }, [camPos, gridSize, gridDivisions]);
 
     useEffect(() => {
       if (gridsRef.current.gridXY) {
