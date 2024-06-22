948752602c9b53535573f0a61ee0973627c4ea1c
Theodor Guttesen <theodorguttesen@gmail.com>
2024-06-22 12:50:41 +0200

    app context: for now just used for loading indicator

---
+++ tools/Python/mcdisplay/webgl/Contexts/AppContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, ReactNode, useState } from "react";
+
+type AppContextType = {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const AppContext = createContext<AppContextType>({
+  loading: true,
+  setLoading: () => {},
+});
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [loading, _setLoading] = useState<boolean>(true);
+  const setLoading = (value: React.SetStateAction<boolean>) => {
+    console.log("setLoading called with value:", value);
+    _setLoading(value);
+  };
+  return (
+    <AppContext.Provider value={{ loading, setLoading }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => useContext(AppContext);
