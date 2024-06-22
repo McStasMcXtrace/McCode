ad7d9f73c578267ca4882b7e25edd1a3538bafb9
Theodor Guttesen <theodorguttesen@gmail.com>
2024-06-22 12:47:53 +0200

    Loading indicator

---
+++ tools/Python/mcdisplay/webgl/components/loading-indicator/LoadingIndicator.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../common.css";
+import "./loading-indicator.css";
+import { Watch } from "react-loader-spinner";
+
+const LoadingIndicator = () => {
+  console.log("LoadingIndicator called");
+  return (
+    <div id="loading-indicator" className="">
+      <Watch
+        visible={true}
+        height="100"
+        width="100"
+        radius="48"
+        color="#1a73e8"
+        ariaLabel="watch-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+};
+
+export default LoadingIndicator;
