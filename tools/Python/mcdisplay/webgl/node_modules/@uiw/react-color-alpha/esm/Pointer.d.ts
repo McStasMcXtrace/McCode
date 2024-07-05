9e2a2d61bdad8e38204000ab6f598e40ab5cc123
Peter Willendrup <pkwi@fysik.dtu.dk>
2024-07-05 23:06:11 +0200

    Clear out in-repo copy of node_modules

--- tools/Python/mcdisplay/webgl/node_modules/@uiw/react-color-alpha/esm/Pointer.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export interface PointerProps extends React.HTMLAttributes<HTMLDivElement> {
-    prefixCls?: string;
-    left?: string;
-    top?: string;
-    fillProps?: React.HTMLAttributes<HTMLDivElement>;
-}
-export declare const Pointer: ({ className, prefixCls, left, top, style, fillProps, ...reset }: PointerProps) => JSX.Element;
