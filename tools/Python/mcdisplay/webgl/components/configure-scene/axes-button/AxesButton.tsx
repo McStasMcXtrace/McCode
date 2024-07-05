7289b83f2a579c991aa66a9f245002cf674b1ad2
Theodor Guttesen <theodorguttesen@gmail.com>
2024-07-05 15:27:35 +0200

    simplified axes button

--- tools/Python/mcdisplay/webgl/components/configure-scene/axes-button/AxesButton.tsx
+++ tools/Python/mcdisplay/webgl/components/configure-scene/axes-button/AxesButton.tsx
@@ -6,14 +6,15 @@ import { useGridContext } from "../../../Contexts/GridContext";
 const AxesButton = () => {
   const { showAxes, toggleAxes } = useGridContext();
   return (
-    <div id="axes-button" className="">
-      <button onClick={toggleAxes} className="row">
-        {showAxes ? "Hide Axes  " : "Show Axes  "}
-        <p id="x"> X</p>
-        <p id="y"> Y</p>
-        <p id="z"> Z</p>
-      </button>
-    </div>
+    <button
+      id="axes-button"
+      onClick={toggleAxes}
+      className={`row ${showAxes ? "active" : ""}`}
+    >
+      <p id="x"> X</p>
+      <p id="y"> Y</p>
+      <p id="z"> Z</p>
+    </button>
   );
 };
 
