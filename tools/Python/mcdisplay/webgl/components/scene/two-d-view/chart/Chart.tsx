22338908ae0ca964f3076efaf6f31ceb37684eca
Theodor Guttesen <theodorguttesen@gmail.com>
2024-07-18 14:03:27 +0200

    configuring chart from plotly

--- tools/Python/mcdisplay/webgl/components/scene/two-d-view/chart/Chart.tsx
+++ tools/Python/mcdisplay/webgl/components/scene/two-d-view/chart/Chart.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../../../common.css";
+import Plot from "react-plotly.js";
 import "./chart.css";
 
 interface ChartProps {
@@ -12,7 +13,47 @@ const Chart: React.FC<ChartProps> = ({
   xAxisLabel,
   yAxisLabel,
 }) => {
-  return <div id="chart" className=""></div>;
+  const layout = {
+    title: chartTitle,
+    xaxis: {
+      title: xAxisLabel,
+      showgrid: true,
+      zeroline: false,
+      showline: true,
+      range: [0, 100], // Set a range from data later
+    },
+    yaxis: {
+      title: {
+        text: yAxisLabel,
+        standoff: 10, // Adjust this value to move the title
+      },
+      showgrid: true,
+      zeroline: false,
+      showline: true,
+      range: [-50, 50], // Set a range from data later
+    },
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: "rgba(0,0,0,0)",
+    autosize: true,
+    margin: {
+      l: 30, // left margin
+      r: 15, // right margin
+      b: 15, // bottom margin
+      t: 15, // top margin
+      pad: 0, // padding
+    },
+  };
+
+  return (
+    <div id="chart" className="">
+      <Plot
+        data={[]}
+        layout={layout}
+        useResizeHandler
+        style={{ width: "100%", height: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default Chart;
