cb94191f1cea7b1eb796d9e7b5cfcb4b71124627
Theodor Guttesen <theodorguttesen@gmail.com>
2024-08-29 12:08:42 +0200

    import rays

--- tools/Python/mcdisplay/webgl/App.tsx
+++ tools/Python/mcdisplay/webgl/App.tsx
@@ -6,10 +6,13 @@ import LoadingIndicator from "./components/loading-indicator/LoadingIndicator";
 import { useAppContext } from "./Contexts/AppContext";
 import { initializeInstrument } from "./data-utils/initInstrument";
 import { useInstrumentContext } from "./Contexts/InstrumentContext";
+import { useRaysContext } from "./Contexts/RaysContext";
+import { initializeRays } from "./data-utils/initRays";
 
 export default function App() {
   const { loading, setLoading } = useAppContext();
   const { instrument, setInstrument } = useInstrumentContext();
+  const { rays, setRays } = useRaysContext();
 
   const dropHandler = () => (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -18,8 +21,15 @@ export default function App() {
     const file = e.dataTransfer.files[0];
     const reader = new FileReader();
     reader.onload = (e) => {
-      const newInstrument = JSON.parse(e.target.result as string);
-      setInstrument(initializeInstrument(newInstrument));
+      const data = JSON.parse(e.target.result as string);
+      //only instrument json contains the key "cmd"
+      if (data.hasOwnProperty("cmd")) {
+        setInstrument(initializeInstrument(data));
+      }
+      //only rays json contains the key "numrays"
+      else if (data.hasOwnProperty("numrays")) {
+        setRays(initializeRays(data));
+      }
       setLoading(false);
     };
     reader.readAsText(file);
