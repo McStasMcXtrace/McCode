e45940c373d1fc99a70af0e04e19351d96e46780
Theodor Guttesen <theodorguttesen@gmail.com>
2024-08-29 10:35:20 +0200

    import json

--- tools/Python/mcdisplay/webgl/App.tsx
+++ tools/Python/mcdisplay/webgl/App.tsx
@@ -4,11 +4,40 @@ import ConfigureSceneMenu from "./components/configure-scene/ConfigureSceneMenu"
 import "./common.css";
 import LoadingIndicator from "./components/loading-indicator/LoadingIndicator";
 import { useAppContext } from "./Contexts/AppContext";
+import { initializeInstrument } from "./data-utils/initInstrument";
+import { useInstrumentContext } from "./Contexts/InstrumentContext";
 
 export default function App() {
   const { loading, setLoading } = useAppContext();
+  const { instrument, setInstrument } = useInstrumentContext();
+
+  const dropHandler = () => (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(e.dataTransfer.files);
+    const file = e.dataTransfer.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const data = JSON.parse(e.target.result as string);
+      const newInstrument = instrument;
+      newInstrument.components = data;
+      setInstrument(initializeInstrument(newInstrument));
+      setLoading(false);
+    };
+    reader.readAsText(file);
+  };
+
+  const dragOverHandler = () => (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <div id="app" className="column">
+    <div
+      id="app"
+      onDrop={dropHandler()}
+      onDragOver={dragOverHandler()}
+      className="column"
+    >
       {loading ? <LoadingIndicator /> : null}
       <ConfigureSceneMenu />
       <ThreeCanvas />
